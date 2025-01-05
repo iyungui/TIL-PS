40bca082eafe3b7396ff50aba8ede42bc391cd76
이융의 <121233350+iyungui@users.noreply.github.com>
2025-01-05 13:30:48 +0900

    [Bronze I] Title: ROT13, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/11655. ROT13/ROT13.cc
+++ 백준/Bronze/11655. ROT13/ROT13.cc
@@ -2,44 +2,20 @@
 
 using namespace std;
 
-string str, ret;
-
-void toPW() {
-    for(int a : str) {
-        // 대문자
-        if(a >= 65 && a <= (65 + 26)) {
-            int next_a = a + 13;
-            if(next_a >= 65 + 26) {
-                // 초과 값
-                int temp = next_a - (65 + 26);
-                next_a = 65 + temp;
-            }
-            ret += next_a;
-        }
-        // 소문자
-        else if(a >= 97 && a <= (97 + 26)) {
-            int next_a = a + 13;
-            if(next_a >= 97 + 26) {
-                // 초과 값
-                int temp = next_a - (97 + 26);
-                next_a = 97 + temp;
-            }
-            ret += next_a;
-          
-        // 공백
-        } else {
-            ret += a;
-        }
-    }
-}
+string str;
 
 int main() {
-    
     getline(cin, str);
-    
-    toPW();
-    
-    cout << ret;
+    for(int i = 0; i < (int)str.size(); i++) {
+        if(str[i] >= 65 && str[i] < 97) {
+            if(str[i] + 13 > 90) str[i] = str[i] + 13 - 26;
+            else str[i] += 13;
+        } else if(str[i] >= 97 && str[i] <= 122) {
+            if(str[i] + 13 > 122) str[i] = str[i] + 13 - 26;
+            else str[i] += 13;
+        }
+        cout << str[i];
+    }
     
     return 0;
 }
