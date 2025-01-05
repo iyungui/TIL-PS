98df5316b44b8944491d6e8d63c753cac347fb29
이융의 <121233350+iyungui@users.noreply.github.com>
2025-01-06 00:34:23 +0900

    [Silver III] Title: 팰린드롬 만들기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.cc
+++ 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.cc
@@ -1,50 +1,30 @@
+
 #include <bits/stdc++.h>
+
 using namespace std;
 
-string a, ret;
-int n, mid;
-int cnt[26];
-
+string s, ret;
+int cnt[200], flag;
+char mid;
 int main() {
-    cin >> a;
-    
-    n = (int)a.size();
-    
-    // cnt
-    for(char c : a) cnt[c - 'A']++;
-    
-    
-    // isPelindrome
-    int odd = 0;
-    for(int i = 0; i < 26; i++) {
-        if(cnt[i] % 2 == 1) {
-            odd++;
-            mid = i + 'A';
+    cin >> s;
+    for(char a : s) cnt[a]++;
+    for(int i = 'Z'; i >= 'A'; i--) {
+        if(cnt[i]) {
+            if(cnt[i] & 1) {
+                mid = (char)(i);
+                flag++;
+                cnt[i]--;
+            }
+            if(flag == 2) break;
+            for(int j = 0; j < cnt[i]; j += 2) {
+                ret = char(i) + ret;
+                ret += char(i);
+            }
         }
     }
-    
-    if(odd >= 2) {
-        cout << "I'm Sorry Hansoo";
-        return 0;
-    }
-    
-    // 여기까지 오면 펠린드롬 가능
-    for(int i = 0; i < 26; i++) {
-        for(int j = 0; j < cnt[i] / 2; j++) {
-            ret += (char)(i + 'A');
-        }
-    }
-    
-    if(odd == 1) ret += mid;
-    
-    // 나머지 짝수 cnt (사전역순 배치)
-    for(int i = 25; i >= 0; i--) {
-        for(int j = 0; j < cnt[i] / 2; j++) {
-            ret += (char)(i + 'A');
-        }
-    }
-    
-    cout << ret;
-    
+    if(mid)ret.insert(ret.begin() + ret.size() / 2, mid);
+    if(flag == 2) cout << "I'm Sorry Hansoo\n";
+    else cout << ret << '\n';
     return 0;
 }
