598dff608d79ad2702c900fec296f03efde05ee3
이융의 <121233350+iyungui@users.noreply.github.com>
2025-01-05 11:37:04 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.cc
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.cc
@@ -2,36 +2,14 @@
 
 using namespace std;
 
-string str;
-stack<char> s;
-int n;
-int mid, start;
-
-bool pelindrome() {
-    for(int i = 0; i <= mid; i++) {
-        s.push(str[i]);
-    }
-    
-    for(int i = start; i < n; i++) {
-        if(s.top() == str[i]) {
-            s.pop();
-        }
-    }
-    
-    if(s.empty()) return true;
-    return false;
-}
+string str, temp;
 
 int main() {
     cin >> str;
-    n = (int)str.size();
-    mid = n / 2 - 1;
+    temp = str;
+    reverse(temp.begin(), temp.end());
     
-    if(n % 2 == 0) start = mid + 1;
-    else start = mid + 2;
-    
-    if(pelindrome()) cout << 1;
+    if(temp == str) cout << 1;
     else cout << 0;
-    
     return 0;
 }
