477be72bc9be9a24d32eaeaeb2b55b05f5f6b0d0
이융의 <121233350+iyungui@users.noreply.github.com>
2025-01-06 09:27:01 +0900

    [Silver III] Title: 한국이 그리울 땐 서버에 접속하지, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/9996. 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.cc
+++ 백준/Silver/9996. 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.cc
@@ -2,24 +2,26 @@
 
 using namespace std;
 
-string p, s, pre, suf;
 int n;
+string p, a, pre, suf, ret;
 
 int main() {
     cin >> n;
     cin >> p;
-    
-    int pos = (int)p.find('*'); // 무조건 * 있음
+    int pos = (int)p.find('*');
     pre = p.substr(0, pos);
     suf = p.substr(pos + 1);
     
     for(int i = 0; i < n; i++) {
-        cin >> s;
-        if(pre.size() + suf.size() > s.size()) cout << "NE\n";
+        cin >> a;
+        if((int)pre.size() + (int)suf.size() > (int)a.size()) {
+            ret += "NE\n";
+        }
         else {
-            if(s.substr(0, pre.size()) == pre && s.substr(s.size() - suf.size()) == suf) cout << "DA\n";
-            else cout << "NE\n";
+            if(pre == a.substr(0, (int)pre.size()) && suf == a.substr((int)a.size() - (int)suf.size())) ret += "DA\n";
+            else ret +="NE\n";
         }
     }
+    cout << ret;
     return 0;
 }
