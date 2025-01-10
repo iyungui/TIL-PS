dad2bed426da44be30922bb5889e643237ce1320
이융의 <121233350+iyungui@users.noreply.github.com>
2025-01-10 22:37:33 +0900

    [Silver V] Title: 비밀번호 발음하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/4659. 비밀번호 발음하기/비밀번호 발음하기.cc
+++ 백준/Silver/4659. 비밀번호 발음하기/비밀번호 발음하기.cc
@@ -2,38 +2,33 @@
 using namespace std;
 
 string s, ret;
+int lcnt, vcnt; // 연속 모음, 연속 자음 cnt
 
-bool isVowel(char c) {
+bool isVowel(int c) {
     return (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
 }
 
 bool solve() {
-    // 1. 모음이 하나 이상 있어야 한다
-    int vowelCnt = 0;
-    for(int i = 0; i < (int)s.size(); i++) {
-        if(isVowel(s[i])) vowelCnt++;
-    }
-    if(vowelCnt == 0) return false;
+    lcnt = vcnt = 0;
+    bool is_include_v = 0;
+    int prev = -1;
     
-    // 2. 모음이나 자음이 3개 연속으로 오면 안된다
-    for(int i = 0; i < (int)s.size() - 2; i++) {
-        bool v1 = isVowel(s[i]);
-        bool v2 = isVowel(s[i + 1]);
-        bool v3 = isVowel(s[i + 2]);
-        
-        // 모음 3개 연속
-        if(v1 && v2 && v3) return false;
-        // 자음 3개 연속
-        if(!v1 && !v2 && !v3) return false;
-    }
-    
-    // 3. 같은 글자가 연속으로 두번 오면 안됨 (ee, oo 제외)
-    for(int i = 0; i < (int)s.size() - 1; i++) {
-        if(s[i] == s[i + 1]) {
-            if(s[i] != 'e' && s[i] != 'o') return false;
+    for(int i = 0; i < s.size(); i++) {
+        int idx = s[i];
+        if(isVowel(idx)) {
+            lcnt++;
+            vcnt = 0;
+            is_include_v = 1;
         }
+        else {
+            vcnt++;
+            lcnt = 0;
+        }
+        if(vcnt == 3 || lcnt == 3) return false;
+        if(i >= 1 && (prev == idx) && (idx != 'e' && idx != 'o')) return false;
+        prev = idx;
     }
-    
+    if(!is_include_v) return false;
     return true;
 }
 
