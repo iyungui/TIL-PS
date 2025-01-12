1332b6b8d5b99aa1775547e80731ce0085deefff
이융의 <121233350+iyungui@users.noreply.github.com>
2025-01-12 14:27:53 +0900

    [Gold IV] Title: 연구소, Time: 44 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.cc
+++ 백준/Gold/14502. 연구소/연구소.cc
@@ -1,8 +1,9 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
-int a[10][10], visited[10][10], n, m, ret;
-vector<pair<int, int>> wallList, virusList;
+int n, m, ret, cnt, a[10][10], visited[10][10];
+vector<pair<int, int>> virusList, wallList;
 const int dy[] = {-1, 0, 1, 0};
 const int dx[] = {0, 1, 0, -1};
 
@@ -12,42 +13,48 @@ bool InRange(int y, int x) {
 
 void dfs(int y, int x) {
     visited[y][x] = 1;
+    
+    int ny, nx;
     for(int i = 0; i < 4; i++) {
-        int ny = y + dy[i];
-        int nx = x + dx[i];
+        ny = y + dy[i];
+        nx = x + dx[i];
         
         if(InRange(ny, nx) && a[ny][nx] == 0 && visited[ny][nx] == 0) {
             dfs(ny, nx);
         }
     }
+    
     return;
 }
 
 int solve() {
-    int cnt = 0;
     fill(&visited[0][0], &visited[0][0] + 10 * 10, 0);
+    
+    // 바이러스 퍼뜨리기
     for(pair<int, int> b : virusList) {
         dfs(b.first, b.second);
     }
+    
+    
+    // 안전영역 개수 구하기
+    cnt = 0;
     
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < m; j++) {
             if(a[i][j] == 0 && visited[i][j] == 0) cnt++;
         }
     }
+    
     return cnt;
 }
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL); cout.tie(NULL);
     cin >> n >> m;
-    
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < m; j++) {
             cin >> a[i][j];
             if(a[i][j] == 0) wallList.push_back({i, j});
-            else if(a[i][j] == 2) virusList.push_back({i, j});
+            if(a[i][j] == 2) virusList.push_back({i, j});
         }
     }
     
