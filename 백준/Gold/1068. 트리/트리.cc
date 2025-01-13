2c9aae0878a526021fd4df20404e584f3bde8fda
이융의 <121233350+iyungui@users.noreply.github.com>
2025-01-13 09:07:45 +0900

    [Gold V] Title: 트리, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1068. 트리/트리.cc
+++ 백준/Gold/1068. 트리/트리.cc
@@ -2,28 +2,25 @@
 
 using namespace std;
 
-int n, temp, r, root;
+int n, root, r, temp, ret;
 vector<int> adj[54];
 
 int dfs(int here) {
-    int ret = 0;
+    int cnt = 0;
     int child = 0;
-    
     for(int there : adj[here]) {
-        if(there == r) continue;
-        ret += dfs(there);
+        if(r == there) continue;
+        cnt += dfs(there);
         child++;
     }
     if(child == 0) return 1;
-    return ret;
+    return cnt;
 }
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
-    
     cin >> n;
-    
     for(int i = 0; i < n; i++) {
         cin >> temp;
         if(temp == -1) root = i;
@@ -34,7 +31,7 @@ int main() {
         cout << 0 << '\n';
         return 0;
     }
-    
     cout << dfs(root) << '\n';
+    
     return 0;
 }
