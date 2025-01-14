2f10dc72d6bfe7ba1ce73e5254bf00725faa3539
이융의 <121233350+iyungui@users.noreply.github.com>
2025-01-14 22:39:26 +0900

    [Silver IV] Title: 주몽, Time: 76 ms, Memory: 2080 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.cc
+++ 백준/Silver/1940. 주몽/주몽.cc
@@ -1,29 +1,22 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-int n, m, temp, sum, ret;
-vector<int> v;
+int n, m, a[15004], ret;
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
-    
     cin >> n >> m;
+    for(int i = 0; i < n; i++) cin >> a[i];
     
-    for(int i = 0; i < n; i++) {
-        cin >> temp;
-        v.push_back(temp);
-    }
+    // n C 2
     
     for(int i = 0; i < n; i++) {
         for(int j = i + 1; j < n; j++) {
-            sum = v[i] + v[j];
-            if(sum == m) {
-                ret++;
-            }
+            if(a[i] + a[j] == m) ret++;
         }
     }
-    cout << ret;
+    
+    cout << ret << '\n';
     return 0;
 }
