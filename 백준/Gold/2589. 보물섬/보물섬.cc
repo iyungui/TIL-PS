11cffb0dae91dedfa4b7bd8e45ad9bf141ff6a2f
이융의 <121233350+iyungui@users.noreply.github.com>
2025-01-15 22:16:35 +0900

    [Gold V] Title: 보물섬, Time: 116 ms, Memory: 2040 KB -BaekjoonHub

--- 백준/Gold/2589. 보물섬/보물섬.cc
+++ 백준/Gold/2589. 보물섬/보물섬.cc
@@ -2,8 +2,9 @@
 
 using namespace std;
 
-int n, m, visited[54][54], mx;
+int n, m, ret;
 char a[54][54];
+int visited[54][54];
 const int dy[] = {-1, 0, 1, 0};
 const int dx[] = {0, 1, 0, -1};
 
@@ -11,27 +12,27 @@ bool InRange(int y, int x) {
     return y >= 0 && y < n && x >= 0 && x < m;
 }
 
-void bfs(int y, int x) {
-    memset(visited, 0, sizeof(visited));
+int bfs(int y, int x) {
+    
     visited[y][x] = 1;
     queue<pair<int, int>> q;
     q.push({y, x});
     
     while(q.size()) {
         tie(y, x) = q.front(); q.pop();
+        
         for(int i = 0; i < 4; i++) {
             int ny = y + dy[i];
             int nx = x + dx[i];
             
-            if(InRange(ny, nx) && a[ny][nx] != 'W' && visited[ny][nx] == 0) {
+            if(InRange(ny, nx) && visited[ny][nx] == 0 && a[ny][nx] == 'L') {
                 visited[ny][nx] = visited[y][x] + 1;
-                mx = max(mx, visited[ny][nx]);
                 q.push({ny, nx});
             }
         }
     }
     
-    return;
+    return visited[y][x] - 1;
 }
 
 int main() {
@@ -44,10 +45,13 @@ int main() {
     
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < m; j++) {
-            if(a[i][j] == 'L') bfs(i, j);
+            if(a[i][j] == 'L') {
+                memset(visited, 0, sizeof(visited));
+                ret = max(ret, bfs(i, j));
+            }
         }
     }
     
-    cout << mx - 1 << '\n';
+    cout << ret << '\n';
     return 0;
 }
