b04f6f3f17d7e6641c430351abe79f6fa2c3881b
이융의 <121233350+iyungui@users.noreply.github.com>
2025-01-19 02:03:58 +0900

    [Gold III] Title: 불!, Time: 40 ms, Memory: 11016 KB -BaekjoonHub

--- 백준/Gold/4179. 불！/불！.cc
+++ 백준/Gold/4179. 불！/불！.cc
@@ -1,23 +1,19 @@
 #include <bits/stdc++.h>
-using namespace std;
 
+using namespace std;
+int n, m, sx, sy, y, x, ret;
 char a[1004][1004];
-int n, m, sx, sy, ret, y, x;
-const int dy[] = {-1, 0, 1, 0};
-const int dx[] = {0, 1, 0, -1};
 int fire_check[1004][1004], person_check[1004][1004];
 queue<pair<int, int>> q;
 
-bool InRange(int y, int x) {
-    return y >= 0 && y < n && x >= 0 && x < m;
-}
-
+const int dy[] = {-1, 0, 1, 0};
+const int dx[] = {0, 1, 0, -1};
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
     cin >> n >> m;
     fill(&fire_check[0][0], &fire_check[0][0] + 1004 * 1004, INT_MAX);
-
+    
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < m; j++) {
             cin >> a[i][j];
@@ -29,17 +25,19 @@ int main() {
             }
         }
     }
-    
-    while(q.size()) {
+    while(!q.empty()) {
         tie(y, x) = q.front(); q.pop();
+        
         for(int i = 0; i < 4; i++) {
             int ny = y + dy[i];
             int nx = x + dx[i];
             
-            if(InRange(ny, nx) && a[ny][nx] != '#' && fire_check[ny][nx] == INT_MAX) {
-                fire_check[ny][nx] = fire_check[y][x] + 1;
-                q.push({ny, nx});
-            }
+            if(ny >= n || ny < 0 || nx >= m || nx < 0) continue;
+            
+            if(a[ny][nx] == '#') continue;
+            if(fire_check[ny][nx] != INT_MAX) continue;
+            fire_check[ny][nx] = fire_check[y][x] + 1;
+            q.push({ny, nx});
         }
     }
     
@@ -47,24 +45,21 @@ int main() {
     q.push({sy, sx});
     
     while(q.size()) {
-        tie(y, x) = q.front(); q.pop();
-        
-        if(x == m - 1 || x == 0 || y == 0 || y == n - 1) {
-            ret = person_check[y][x];
-            break;
+        tie(y, x) = q.front(), q.pop();
+        if(x == m - 1 || x == 0 || y == n - 1 || y == 0) {
+            ret = person_check[y][x]; break;
         }
         
         for(int i = 0; i < 4; i++) {
-            int ny = y + dy[i];
-            int nx = x + dx[i];
-            if(InRange(ny, nx) && a[ny][nx] != '#' && !person_check[ny][nx] && (fire_check[ny][nx] > person_check[y][x] + 1)) {
-                person_check[ny][nx] = person_check[y][x] + 1;
-                q.push({ny, nx});
-            }
+            int ny = y + dy[i], nx = x + dx[i];
+            if(ny >= n || ny < 0 || nx >= m || nx < 0 || a[ny][nx] == '#') continue;
+            if(person_check[ny][nx]) continue;
+            if(fire_check[ny][nx] <= person_check[y][x] + 1) continue;
+            person_check[ny][nx] = person_check[y][x] + 1;
+            q.push({ny, nx});
         }
     }
-    
-    if(ret != 0) cout << ret << '\n';
+    if(ret) cout << ret << '\n';
     else cout << "IMPOSSIBLE \n";
 
     return 0;
