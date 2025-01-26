90263f0cd6411767f6281a2571be81da10c5153a
이융의 <121233350+iyungui@users.noreply.github.com>
2025-01-26 21:12:16 +0900

    [Gold IV] Title: 다이어트, Time: 8 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Gold/19942. 다이어트/다이어트.cc
+++ 백준/Gold/19942. 다이어트/다이어트.cc
@@ -1,75 +1,54 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+typedef long long ll;
+int n, mp, mf, ms, mv;
+int b, c, d, e, ret = INT_MAX, sum;
 
-int n;
-int min_price = INT_MAX;
-int mp,mf,ms,mv;
-vector<int> num;
-struct Point {
-    int a, b, c, d, e;
-};
+struct A {
+    int mp, mf, ms, mv, cost;
+} a[16];
 
-vector<Point> points;
+map<int, vector<vector<int>>> ret_v;
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
+    
     cin >> n;
     cin >> mp >> mf >> ms >> mv;
     for(int i = 0; i < n; i++) {
-        int a, b, c, d, e;
-        cin >> a >> b >> c >> d >> e;
-        points.push_back({a, b, c, d, e});
+        cin >> a[i].mp >> a[i].mf >> a[i].ms >> a[i].mv >> a[i].cost;
     }
     
-//    for(int i = 0; i < n; i++) {
-//        cout << points[i].a << " " << points[i].b << " " << points[i].c << " " << points[i].d << " " << points[i].e << '\n';
-//    }
-    
-    for(int i = 0; i < (1 << n); i++) {
-        vector<Point> temp;
-        vector<int> selected;
+    for(int i = 1; i < (1 << n); i++) {
+        b = c = d = e = sum = 0;
+        vector<int> v;
         for(int j = 0; j < n; j++) {
             if(i & (1 << j)) {
-                temp.push_back(points[j]);
-                selected.push_back(j + 1);
+                v.push_back(j + 1);
+                b += a[j].mp;
+                c += a[j].mf;
+                d += a[j].ms;
+                e += a[j].mv;
+                sum += a[j].cost;
             }
         }
-        int sumP = 0, sumF = 0, sumS = 0, sumV = 0, sumC = 0;
-        for(Point p : temp) {
-            sumP += p.a;
-            sumF += p.b;
-            sumS += p.c;
-            sumV += p.d;
-            sumC += p.e;
-        }
-        
-        // mp,mf,ms,mv
-        if(mp <= sumP && mf <= sumF && ms <= sumS && mv <= sumV) {
-            if(min_price > sumC) {
-                min_price = sumC;
-                num.clear();
-                num = selected;
-            }
-            else if(min_price == sumC) {
-                vector<int> temp = num;
-                sort(selected.begin(), selected.end());
-                sort(temp.begin(), temp.end());
-                if(selected < temp) {
-                    num = selected;
-                }
+        if(b >= mp && c >= mf && d >= ms && e >= mv) {
+            if(ret >= sum) {
+                ret = sum;
+                ret_v[ret].push_back(v);
             }
         }
     }
-    
-    if(min_price == INT_MAX) {
-        cout << -1 << '\n';
-        return 0;
+    if(ret == INT_MAX) cout << -1 << '\n';
+    else {
+        sort(ret_v[ret].begin(), ret_v[ret].end());
+        cout << ret << '\n';
+        for(int a : ret_v[ret][0]) {
+            cout << a << " ";
+        }
     }
-    cout << min_price << '\n';
     
-    sort(num.begin(), num.end());
-    for(int i : num) cout << i << " ";
     return 0;
 }
