7c8088818b91c92f880796881f8ebf2a9661ceed
이융의 <121233350+iyungui@users.noreply.github.com>
2025-01-28 11:28:31 +0900

    [Gold IV] Title: 다이어트, Time: 8 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/19942. 다이어트/다이어트.cc
+++ 백준/Gold/19942. 다이어트/다이어트.cc
@@ -1,54 +1,61 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-typedef long long ll;
-int n, mp, mf, ms, mv;
-int b, c, d, e, ret = INT_MAX, sum;
+
+int n, ret = INT_MAX;   // ret : 최소 비용
+int mp, mf, ms, mv, sumP, sumF, sumS, sumV, sumC;
+vector<int> num;    // 최소 비용 식재료 번호
 
 struct A {
-    int mp, mf, ms, mv, cost;
+    int p, f, s, v, cost;
 } a[16];
 
-map<int, vector<vector<int>>> ret_v;
-
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL); cout.tie(NULL);
-    
     cin >> n;
     cin >> mp >> mf >> ms >> mv;
     for(int i = 0; i < n; i++) {
-        cin >> a[i].mp >> a[i].mf >> a[i].ms >> a[i].mv >> a[i].cost;
+        int p, f, s, v, c;
+        cin >> p >> f >> s >> v >> c;
+        a[i] = {p, f, s, v, c};
     }
     
-    for(int i = 1; i < (1 << n); i++) {
-        b = c = d = e = sum = 0;
-        vector<int> v;
+    // solution
+    for(int i = 0; i < (1 << n); i++) {
+        sumP = sumF = sumS = sumV = sumC = 0;
+        vector<int> selected;
         for(int j = 0; j < n; j++) {
             if(i & (1 << j)) {
-                v.push_back(j + 1);
-                b += a[j].mp;
-                c += a[j].mf;
-                d += a[j].ms;
-                e += a[j].mv;
-                sum += a[j].cost;
+                sumP += a[j].p;
+                sumF += a[j].f;
+                sumS += a[j].s;
+                sumV += a[j].v;
+                sumC += a[j].cost;
+                selected.push_back(j + 1);
             }
         }
-        if(b >= mp && c >= mf && d >= ms && e >= mv) {
-            if(ret >= sum) {
-                ret = sum;
-                ret_v[ret].push_back(v);
+        if(sumP >= mp && sumF >= mf && sumS >= ms && sumV >= mv) {
+            if(ret > sumC) {
+                ret = sumC;
+                num.clear();
+                num = selected;
+            } else if(ret == sumC) {
+                sort(num.begin(), num.end());
+                sort(selected.begin(), selected.end());
+                if(num > selected) {
+                    num = selected;
+                }
             }
-        }
-    }
-    if(ret == INT_MAX) cout << -1 << '\n';
-    else {
-        sort(ret_v[ret].begin(), ret_v[ret].end());
-        cout << ret << '\n';
-        for(int a : ret_v[ret][0]) {
-            cout << a << " ";
         }
     }
     
+    
+    if(ret == INT_MAX) {
+        cout << -1 << '\n';
+    } else {
+        cout << ret << '\n';
+        sort(num.begin(), num.end());
+        for(int i : num) cout << i << " ";
+        cout << '\n';
+    }
     return 0;
 }
