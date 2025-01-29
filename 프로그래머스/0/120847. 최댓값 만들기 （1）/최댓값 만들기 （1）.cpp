e64bc47684e737c3eb037e2a744d8d1825063965
이융의 <121233350+iyungui@users.noreply.github.com>
2025-01-29 19:09:04 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.02 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.cpp
+++ 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 int solution(vector<int> numbers) {
+    /* 조합을 이용한 풀이
     int ret = 0;
     for(int i = 0; i < numbers.size(); i++) {
         for(int j = i + 1; j < numbers.size(); j++) {
@@ -10,4 +11,10 @@ int solution(vector<int> numbers) {
         }
     }
     return ret;
+    
+    */
+    
+    // sort (내림차순) 활용 풀이
+    sort(numbers.begin(), numbers.end(), greater<int>());
+    return numbers[0] * numbers[1];
 }
