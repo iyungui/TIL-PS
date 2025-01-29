d04959c6d1523ea10cc4ca8443eeb3359594ff18
이융의 <121233350+iyungui@users.noreply.github.com>
2025-01-29 15:51:02 +0900

    [level 0] Title: 양꼬치, Time: 0.01 ms, Memory: 4.12 MB -BaekjoonHub

--- 프로그래머스/0/120830. 양꼬치/양꼬치.cpp
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.cpp
@@ -4,7 +4,5 @@
 using namespace std;
 
 int solution(int n, int k) {
-    int b = n / 10;
-    int answer = (12000 * n) + (2000 * (k - b));
-    return answer;
+    return ((12000 * n) + (k - (n / 10)) * 2000);
 }
