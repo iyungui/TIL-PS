5370a75f515e2b86d4c8ad864ca52d5c8bc6cb4b
이융의 <121233350+iyungui@users.noreply.github.com>
2025-01-29 19:27:20 +0900

    [level 0] Title: 세균 증식, Time: 0.02 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/0/120910. 세균 증식/세균 증식.cpp
+++ 프로그래머스/0/120910. 세균 증식/세균 증식.cpp
@@ -3,5 +3,10 @@
 using namespace std;
 
 int solution(int n, int t) {
-    return n << t;
+    // 풀이 1: 일반적인 방법
+    // while(t--) n *= 2;
+    // return n;
+    
+    // 풀이 2: pow 활용
+    return n * pow(2, t);
 }
