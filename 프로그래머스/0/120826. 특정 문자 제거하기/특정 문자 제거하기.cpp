add21ab13155506d8abeedba6a6b897d7b26c70b
이융의 <121233350+iyungui@users.noreply.github.com>
2025-01-29 20:09:21 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.cpp
+++ 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.cpp
@@ -3,9 +3,14 @@
 using namespace std;
 
 string solution(string my_string, string letter) {
-    while(my_string.find(letter) != string::npos) {
-        my_string.erase(my_string.find(letter), letter.size());
-    }
+//     // 내 코드 (letter 길이가 2이상이어도 대응됨)
+//     while(my_string.find(letter) != string::npos) {
+//         my_string.erase(my_string.find(letter), letter.size());
+//     }
     
+//     return my_string;
+    
+    // 최적 코드
+    my_string.erase(remove(my_string.begin(), my_string.end(), letter[0]), my_string.end());
     return my_string;
 }
