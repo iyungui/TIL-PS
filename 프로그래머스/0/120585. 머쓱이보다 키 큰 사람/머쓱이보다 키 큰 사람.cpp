be841058a4dc983293d3daa17c6d42d558e1d4b9
이융의 <121233350+iyungui@users.noreply.github.com>
2025-01-29 15:54:20 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.01 ms, Memory: 4.17 MB -BaekjoonHub

--- 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.cpp
+++ 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int solution(vector<int> array, int height) {
     int answer = 0;
-    for(int i : array) {
+    for(const auto i : array) {
         if(i > height) answer++;
     }
     return answer;
