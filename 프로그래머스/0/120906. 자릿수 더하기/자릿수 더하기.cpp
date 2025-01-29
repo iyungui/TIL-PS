f19c46d234d0f80b7e63e052f8a771a676df8e22
이융의 <121233350+iyungui@users.noreply.github.com>
2025-01-29 19:45:41 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.02 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.cpp
+++ 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.cpp
@@ -1,12 +1,13 @@
-#include <bits/stdc++.h>
+#include <string>
+#include <vector>
 
 using namespace std;
 
 int solution(int n) {
     int answer = 0;
-    string str = to_string(n);
-    for(int i = 0; i < str.size(); i++) {
-        answer += (int)str[i] - '0';
+    while(n > 0) {
+        answer += n % 10;
+        n /= 10;
     }
     return answer;
 }
