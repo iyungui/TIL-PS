8a8b23c1ff443a3e179ffe4b6ce5ba34c1fb24db
이융의 <121233350+iyungui@users.noreply.github.com>
2025-01-30 03:44:13 +0900

    [level 0] Title: 삼각형의 완성조건 (1), Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.cpp
+++ 프로그래머스/0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.cpp
@@ -3,19 +3,6 @@
 using namespace std;
 
 int solution(vector<int> sides) {
-    int answer = 0;
-    int mx = 0;
-    int sum = 0, cnt = 0;
-    for(int i : sides) mx = max(mx, i);
-    for(int i : sides) {
-        if(i == mx) continue;
-        sum += i;
-        cnt++;
-    }
-    if(cnt == 0) sum += (mx * 3);
-    if(cnt == 1) sum += mx;
-    if(sum > mx) answer = 1;
-    else answer = 2;
-    
-    return answer;
+    sort(sides.begin(), sides.end(), greater<int>());
+    return ((sides[0] < sides[1] + sides[2]) ? 1 : 2);
 }
