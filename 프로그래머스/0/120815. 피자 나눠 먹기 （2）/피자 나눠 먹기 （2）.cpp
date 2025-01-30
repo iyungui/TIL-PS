6d5099bf2b6f612dae1395121d97b11aa8ba0276
이융의 <121233350+iyungui@users.noreply.github.com>
2025-01-30 15:27:55 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.cpp
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.cpp
@@ -3,13 +3,24 @@
 
 using namespace std;
 
+int gcd(int a, int b) {
+    if(a == 0) return b;
+    return gcd(b % a, a);
+}
+int lcm(int a, int b) {
+    return (a * b) / gcd(a, b);
+}
+
 int solution(int n) {
-    int answer = 0;
-    for(int i = 1; i <= n; i++) {
-        if((i * 6) % n == 0) {
-            answer = i;
-            break;
-        }
-    }
-    return answer;
+    // int answer = 0;
+    // for(int i = 1; i <= n; i++) {
+    //     if((i * 6) % n == 0) {
+    //         answer = i;
+    //         break;
+    //     }
+    // }
+    // return answer;
+    
+    // 최대공약수 gcd 사용
+    return lcm(6, n) / 6;
 }
