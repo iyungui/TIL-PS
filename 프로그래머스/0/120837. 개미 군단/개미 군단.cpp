f797c8180fa17ca5e2d02c2859473e107f76c9c0
이융의 <121233350+iyungui@users.noreply.github.com>
2025-01-30 11:03:20 +0900

    [level 0] Title: 개미 군단, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/0/120837. 개미 군단/개미 군단.cpp
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.cpp
@@ -4,12 +4,5 @@
 using namespace std;
 
 int solution(int hp) {
-    int answer = 0;
-    int a[3] = {5, 3, 1};
-    int i = 0;
-    while(hp > 0 && i < 3) {
-        if(hp >= a[i]) hp -= a[i], answer++;
-        else i++;
-    }
-    return answer;
+    return (hp / 5) + (hp % 5 / 3) + (hp % 5 % 3);
 }
