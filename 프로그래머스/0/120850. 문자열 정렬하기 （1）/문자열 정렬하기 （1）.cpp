af536e5ed30c6b93b53dcd69a513eb1f31aa5ff6
이융의 <121233350+iyungui@users.noreply.github.com>
2025-01-30 14:39:17 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.01 ms, Memory: 3.75 MB -BaekjoonHub

---
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<int> solution(string my_string) {
+    vector<int> answer;
+    for(char c : my_string) {
+        if(c >= '0' && c <= '9') answer.push_back(c - '0');
+    }
+    sort(answer.begin(), answer.end());
+    return answer;
+}
