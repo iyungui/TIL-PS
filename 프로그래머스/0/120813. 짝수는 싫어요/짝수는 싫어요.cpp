ff5af56d6e60ce1a54f633b2497fd71b50598764
이융의 <121233350+iyungui@users.noreply.github.com>
2025-01-30 10:26:38 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.02 ms, Memory: 3.68 MB -BaekjoonHub

--- 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.cpp
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.cpp
@@ -5,11 +5,8 @@ using namespace std;
 
 vector<int> solution(int n) {
     vector<int> answer;
-    int i = 1;
-    while(i <= n) {
-        if(i % 2 == 1) answer.push_back(i);
-        i++;
+    for(int i = 1; i <= n; i+=2) {
+        answer.push_back(i);
     }
-    
     return answer;
 }
