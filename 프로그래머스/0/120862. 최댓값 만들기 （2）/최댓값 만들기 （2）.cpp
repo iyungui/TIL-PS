4f612150e2cc7a23bdb85a1a8c12a77ad157fbe8
이융의 <121233350+iyungui@users.noreply.github.com>
2025-01-30 14:22:24 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.01 ms, Memory: 4.27 MB -BaekjoonHub

--- 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.cpp
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.cpp
@@ -3,11 +3,17 @@
 using namespace std;
 
 int solution(vector<int> numbers) {
-    int answer = INT_MIN;
-    for(int i = 0; i < numbers.size(); i++) {
-        for(int j = i + 1; j < numbers.size(); j++) {
-            answer = max(answer, numbers[i] * numbers[j]);
-        }
-    }
-    return answer;
+    // 조합을 이용한 풀이 O(n ^ 2)
+    // int answer = INT_MIN;
+    // for(int i = 0; i < numbers.size(); i++) {
+    //     for(int j = i + 1; j < numbers.size(); j++) {
+    //         answer = max(answer, numbers[i] * numbers[j]);
+    //     }
+    // }
+    // return answer;
+    
+    // sort 사용 O(logn)
+    sort(numbers.begin(), numbers.end());
+    int n = numbers.size();
+    return max(numbers[n - 1] * numbers[n - 2], numbers[0] * numbers[1]);
 }
