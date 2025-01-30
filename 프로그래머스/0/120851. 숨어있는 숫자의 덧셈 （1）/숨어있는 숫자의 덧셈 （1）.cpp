f226ec612e33a38d86ab86ef9d78bd606c7b7b29
이융의 <121233350+iyungui@users.noreply.github.com>
2025-01-30 14:12:53 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.cpp
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.cpp
@@ -5,8 +5,8 @@ using namespace std;
 
 int solution(string my_string) {
     int answer = 0;
-    for(char c : my_string) {
-        if(c < 65) answer += (c - '0');
+    for(char i : my_string) {
+        if(i >= '0' && i <= '9') answer += (i - '0');
     }
     return answer;
 }
