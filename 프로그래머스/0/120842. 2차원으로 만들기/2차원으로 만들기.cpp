f35abef1ef992c5dab01a2dad18fd4c07aca5105
이융의 <121233350+iyungui@users.noreply.github.com>
2025-02-02 00:22:10 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.05 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.cpp
+++ 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.cpp
@@ -1,14 +1,12 @@
-#include <vector>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 vector<vector<int>> solution(vector<int> num_list, int n) {
-    int size = num_list.size() / n;  // 몇 개의 행이 필요한지 계산
-    vector<vector<int>> answer(size, vector<int>(n));
-    
+    int s = (int)num_list.size() / n;
+    vector<vector<int>> answer(s, vector<int>(n));
     for(int i = 0; i < num_list.size(); i++) {
-        answer[i/n][i%n] = num_list[i];
+        answer[i / n][i % n] = num_list[i];
     }
-    
     return answer;
 }
