6a498ca48e891daba8999bf66ced4e15a7a79b23
이융의 <121233350+iyungui@users.noreply.github.com>
2025-02-01 12:41:19 +0900

    [level 0] Title: 중복된 문자 제거, Time: 0.01 ms, Memory: 3.66 MB -BaekjoonHub

--- 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.cpp
+++ 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.cpp
@@ -4,13 +4,23 @@
 using namespace std;
 
 string solution(string my_string) {
-    string answer = "";
-    bool visited[128] = {false};
+    // visited 이용 방법
     
+//     string answer = "";
+//     bool visited[128] = {false};
+    
+//     for(char c : my_string) {
+//         if(!visited[c]) {
+//             answer += c;
+//             visited[c] = true;
+//         }
+//     }
+    
+    // string.find 이용 방법 (추천)
+    string answer = "";
     for(char c : my_string) {
-        if(!visited[c]) {
+        if(answer.find(c) == string::npos) {
             answer += c;
-            visited[c] = true;
         }
     }
     return answer;
