7e4d6fcef92d6b6ad7ddf3b2293b267c73553fa0
이융의 <121233350+iyungui@users.noreply.github.com>
2025-02-02 18:17:08 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.cpp
+++ 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.cpp
@@ -7,9 +7,7 @@ using namespace std;
 vector<int> solution(vector<int> emergency) {
     vector<int> answer(emergency.size());
     vector<int> temp = emergency;
-    
     sort(temp.begin(), temp.end(), greater<int>());
-    
     for(int i = 0; i < emergency.size(); i++) {
         auto it = find(temp.begin(), temp.end(), emergency[i]);
         answer[i] = it - temp.begin() + 1;
