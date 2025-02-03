74002d41f2d635bb4c77c1d8dd6e1380d02e6f33
이융의 <121233350+iyungui@users.noreply.github.com>
2025-02-03 23:33:26 +0900

    [Gold V] Title: AC, Time: 88 ms, Memory: 3020 KB -BaekjoonHub

--- 백준/Gold/5430. AC/AC.cc
+++ 백준/Gold/5430. AC/AC.cc
@@ -2,52 +2,48 @@
 
 using namespace std;
 
-int T, N, x;
-string P, order;
+int t, n;
+string p, s;
 
 int main() {
-    cin >> T;
-    for(int t = 0; t < T; t++) {
-        deque<int> D;
-        cin >> P >> N >> order;
-        x = 0;
-        //"[1,2,3,4]" -> [1, 2, 3, 4]
-        for(char c : order) {
-            if(c == '[' || c == ']') continue;
-            
-            // 숫자가 나오면 현재 수 * 10 을 한 뒤에 더함.
-            if(c >= '0' && c <= '9') x = x * 10 + c - '0';
-            // 아닐 경우 현재 수를 덱에 넣음
-            else {
-                if(x > 0) D.push_back(x);
-                x = 0;
+    cin >> t;
+    while(t--) {
+        cin >> p >> n >> s;
+        deque<int> d;
+        int cur = 0;
+        for(char c : s) {
+            if(c == ']' || c == '[') continue;
+            else if(c >= '0' && c <= '9') {
+                cur = cur * 10 + c - '0';
+            } else {
+                if(cur > 0) d.push_back(cur);
+                cur = 0;
             }
         }
-        if(x > 0) D.push_back(x);
+        if(cur > 0) d.push_back(cur);
         
-        // 초기에는 에러 없음, 뒤집히지 않은 상태.
         bool error = false, rev = false;
-        for(char a : P) {
-            if(a == 'R') rev = !rev;
-            else {
-                // 비어 있는데, 제거하려 하면 에러
-                if(D.empty()) {
+        
+        for(char a : p) {
+            if(a == 'R') {
+                rev = !rev;
+            } else {
+                if(d.empty()) {
                     error = true;
                     break;
                 }
-                if(rev) D.pop_back();
-                else D.pop_front();
+                if(rev) d.pop_back();
+                else d.pop_front();
             }
         }
-        
         if(error) {
             cout << "error" << '\n';
         } else {
+            if(rev) reverse(d.begin(), d.end());
             cout << "[";
-            if(rev) reverse(D.begin(), D.end());
-            for(int i = 0; i < D.size(); i++) {
-                cout << D[i];
-                if(i < D.size() - 1) cout << ",";
+            for(int i = 0; i < d.size(); i++) {
+                cout << d[i];
+                if(i < d.size() - 1) cout << ",";
             }
             cout << "]\n";
         }
