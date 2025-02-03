5588a9517203c6d1f7c455fc1704b7e584f6fba8
이융의 <121233350+iyungui@users.noreply.github.com>
2025-02-03 14:15:42 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 0.02 ms, Memory: 4.18 MB -BaekjoonHub

---
+++ 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string solution(string s) {
+    string answer = "";
+    map<char, int> mp;
+    for(char c : s) {
+        mp[c]++;
+    }
+    for(auto it : mp) {
+        if(it.second == 1) {
+            answer += it.first;
+        }
+    }
+    return answer;
+}
