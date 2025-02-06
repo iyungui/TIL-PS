dc3356ab1ee4c1b5fca4b90cf335dfef2389c6af
이융의 <121233350+iyungui@users.noreply.github.com>
2025-02-06 16:59:17 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 188 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, a, b;
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    cin >> n;
+    while(n--) {
+        cin >> a >> b;
+        cout << a + b << '\n';
+    }
+    return 0;
+}
