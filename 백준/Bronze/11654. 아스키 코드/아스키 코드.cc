fb9529fa4218e4c84d981ee8cbf101634266d48a
이융의 <121233350+iyungui@users.noreply.github.com>
2025-02-07 14:18:25 +0900

    [Bronze V] Title: 아스키 코드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+char a;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    cin >> a;
+    cout << (int)a << '\n';
+    return 0;
+}
