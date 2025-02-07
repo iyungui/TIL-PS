4d41072735c5fa1a37b38554f743d2f019152123
이융의 <121233350+iyungui@users.noreply.github.com>
2025-02-07 14:06:23 +0900

    [Bronze V] Title: 문자와 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string s;
+int n;
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    cin >> s >> n;
+    cout << s[n - 1] << '\n';
+    return 0;
+}
