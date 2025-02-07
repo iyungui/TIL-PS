86ccd6249b317c43fec86429c50f7d003c5dc3ae
이융의 <121233350+iyungui@users.noreply.github.com>
2025-02-07 09:31:09 +0900

    [Bronze III] Title: 최소, 최대, Time: 96 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.cc
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.cc
@@ -1,15 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, a[1000004], mn = INT_MAX, mx = INT_MIN;
+int n, temp, mn = INT_MAX, mx = INT_MIN;
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
     cin >> n;
     for(int i = 0; i < n; i++) {
-        cin >> a[i];
-        mn = min(mn, a[i]);
-        mx = max(mx, a[i]);
+        cin >> temp;
+        mn = min(mn, temp);
+        mx = max(mx, temp);
     }
     cout << mn << " " << mx << '\n';
     return 0;
