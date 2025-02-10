9467f0dcb1e4e1b1202219fb54ec4bfbf152edd2
이융의 <121233350+iyungui@users.noreply.github.com>
2025-02-11 00:04:45 +0900

    [level 0] Title: 배열의 원소만큼 추가하기, Time: 0.12 ms, Memory: 16.8 MB -BaekjoonHub

--- 프로그래머스/0/181861. 배열의 원소만큼 추가하기/배열의 원소만큼 추가하기.swift
+++ 프로그래머스/0/181861. 배열의 원소만큼 추가하기/배열의 원소만큼 추가하기.swift
@@ -1,5 +1,5 @@
 import Foundation
 
 func solution(_ arr:[Int]) -> [Int] {
-    return arr.flatMap { Array(repeatElement($0, count: $0)) }
+    return arr.flatMap { Array(repeating: $0, count: $0) }
 }
