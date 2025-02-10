f7575fa322ff9bad925f55b645dccefe0cdd4e91
이융의 <121233350+iyungui@users.noreply.github.com>
2025-02-10 15:45:12 +0900

    [level 0] Title: 배열의 길이에 따라 다른 연산하기, Time: 0.55 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/0/181854. 배열의 길이에 따라 다른 연산하기/배열의 길이에 따라 다른 연산하기.swift
+++ 프로그래머스/0/181854. 배열의 길이에 따라 다른 연산하기/배열의 길이에 따라 다른 연산하기.swift
@@ -7,14 +7,27 @@ func solution(_ arr:[Int], _ n:Int) -> [Int] {
 //         return (isOddLength && index % 2 == 0) || (!isOddLength && index % 2 == 1) ? value + n : value
 //     }
     
-//     S2 (명령형)
-    var result = arr
-    let isOddLength = arr.count % 2 == 1
-    
-    for i in arr.indices {
-        if (isOddLength && i % 2 == 0) || (!isOddLength && i % 2 == 1) {
-            result[i] += n
+    // S1 클로저 축약
+    return arr.enumerated().map {
+        if arr.count % 2 == 1 { // arr 길이가 홀수 라면
+            // 짝수 인덱스 위치 값에 +n
+            return $0.offset % 2 == 0 ? $0.element + n : $0.element
+        } else {    // arr 길이가 짝수라면
+            // 홀수 인덱스 위치 값에 +n
+            return $0.offset % 2 == 1 ? $0.element + n : $0.element
         }
     }
-    return result
+
+    
+//     S2 (명령형)
+//     var result = arr
+//     let isOddLength = arr.count % 2 == 1
+    
+//     for i in arr.indices {
+//         if (isOddLength && i % 2 == 0) || (!isOddLength && i % 2 == 1) {
+//             result[i] += n
+//         }
+//     }
+//     return result
+    
 }
