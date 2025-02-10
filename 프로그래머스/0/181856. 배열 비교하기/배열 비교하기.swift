37bab8e925658b756858991b5735d9f8bb5c9975
이융의 <121233350+iyungui@users.noreply.github.com>
2025-02-10 16:13:05 +0900

    [level 0] Title: 배열 비교하기, Time: 0.07 ms, Memory: 16.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+func solution(_ arr1:[Int], _ arr2:[Int]) -> Int {
+    if(arr1.count != arr2.count) {
+        return arr1.count > arr2.count ? 1 : -1
+    }
+    
+    let sum1: Int = arr1.reduce(0, +)
+    let sum2: Int = arr2.reduce(0, +)
+    if(sum1 == sum2) { return 0 }
+    return sum1 > sum2 ? 1 : -1
+}
