1602d0e0794bb5237158daa3bb28435eafe11308
이융의 <121233350+iyungui@users.noreply.github.com>
2025-02-10 11:08:43 +0900

    [level 0] Title: 문자열 곱하기, Time: 0.02 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.swift
+++ 프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.swift
@@ -1,9 +1,5 @@
 import Foundation
 
 func solution(_ my_string:String, _ k:Int) -> String {
-    var answer : String = ""
-    for i in 0..<k {
-        answer += my_string
-    }
-    return answer
+    return String(repeating: my_string, count: k)
 }
