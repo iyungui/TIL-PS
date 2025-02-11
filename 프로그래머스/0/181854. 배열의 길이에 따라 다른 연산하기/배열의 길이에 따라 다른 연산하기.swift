e2ede75885de8948c7c0010c1ee6c90e5e11f9d8
이융의 <121233350+iyungui@users.noreply.github.com>
2025-02-11 22:02:50 +0900

    [level 0] Title: 배열의 길이에 따라 다른 연산하기, Time: 0.16 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/0/181854. 배열의 길이에 따라 다른 연산하기/배열의 길이에 따라 다른 연산하기.swift
+++ 프로그래머스/0/181854. 배열의 길이에 따라 다른 연산하기/배열의 길이에 따라 다른 연산하기.swift
@@ -1,11 +1,11 @@
 import Foundation
 
 func solution(_ arr:[Int], _ n:Int) -> [Int] {
-    return arr.enumerated().map {   // offset, element
-        if arr.count % 2 == 1 {
-            return $0.offset % 2 == 0 ? $0.element + n : $0.element
-        } else {
-            return $0.offset % 2 == 1 ? $0.element + n : $0.element
-        }
+    var ret: [Int] = arr
+    let startIndex: Int = arr.count % 2 == 1 ? 0 : 1
+    for i in stride(from: startIndex, to: arr.count, by: 2) {
+        ret[i] += n
     }
+    
+    return ret
 }
