dfe2808ee86cfa8005edd21fb96e17469d751a22
이융의 <121233350+iyungui@users.noreply.github.com>
2025-02-11 11:09:18 +0900

    [level 0] Title: 공배수, Time: 0.01 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/0/181936. 공배수/공배수.swift
+++ 프로그래머스/0/181936. 공배수/공배수.swift
@@ -1,6 +1,5 @@
 import Foundation
 
 func solution(_ number:Int, _ n:Int, _ m:Int) -> Int {
-    if(number % n == 0 && number % m == 0) {return 1}
-    return 0
+    number % n == 0 && number % m == 0 ? 1 : 0
 }
