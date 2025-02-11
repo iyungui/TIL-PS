6b0df70f5f8bc1a774e30aff8736ac52e21557a4
이융의 <121233350+iyungui@users.noreply.github.com>
2025-02-11 22:25:13 +0900

    [level 0] Title: 문자열 바꿔서 찾기, Time: 0.04 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.swift
+++ 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.swift
@@ -1,11 +1,6 @@
 import Foundation
 
 func solution(_ myString:String, _ pat:String) -> Int {
-    // let temp = myString
-    //     .replacingOccurrences(of: "A", with: "X")  // 임시로 X 사용
-    //     .replacingOccurrences(of: "B", with: "A")
-    //     .replacingOccurrences(of: "X", with: "B")
-    // return temp.contains(pat) ? 1 : 0
-    let changed = String(myString.map { $0 == "A" ? "B" : "A" })
-    return changed.contains(pat) ? 1 : 0
+    let temp: String = String(myString.map { $0 == "A" ? "B" : "A" })
+    return temp.contains(pat) ? 1 : 0
 }
