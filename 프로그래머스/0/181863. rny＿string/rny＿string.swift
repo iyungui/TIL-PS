58b72ea5eaf39005ffa9f0799c1456b25686736b
이융의 <121233350+iyungui@users.noreply.github.com>
2025-02-11 11:49:31 +0900

    [level 0] Title: rny_string, Time: 0.09 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/0/181863. rny＿string/rny＿string.swift
+++ 프로그래머스/0/181863. rny＿string/rny＿string.swift
@@ -1,13 +1,5 @@
 import Foundation
 
 func solution(_ rny_string:String) -> String {
-    var ret: String = ""
-    for i in rny_string {
-        if(i == "m") {
-            ret += "rn"
-        } else {
-            ret.append(i)
-        }
-    }
-    return ret
+    return rny_string.map { $0 == "m" ? "rn" : String($0) }.joined()
 }
