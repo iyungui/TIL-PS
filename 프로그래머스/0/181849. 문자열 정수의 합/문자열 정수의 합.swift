dae18ddc587a5a111676905e559bdb2d18d2d006
이융의 <121233350+iyungui@users.noreply.github.com>
2025-02-11 21:47:37 +0900

    [level 0] Title: 문자열 정수의 합, Time: 0.09 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/0/181849. 문자열 정수의 합/문자열 정수의 합.swift
+++ 프로그래머스/0/181849. 문자열 정수의 합/문자열 정수의 합.swift
@@ -1,21 +1,5 @@
 import Foundation
 
 func solution(_ num_str:String) -> Int {
-    var sum = 0
-    for char in num_str {
-        sum += Int(String(char))!
-    }
-    return sum
-    // S1
-    
-    // var sum = 0
-    // for char in num_str {
-    //     sum += Int(String(char))!
-    // }
-    // return sum
-    
-    // S2
-    //     return num_str.map { Int(String($0))! }.reduce(0, +)
-    // return num_str.map { Int(String($0))! }.reduce(0, +)
-    
+    num_str.map { Int(String($0))! }.reduce(0, +)
 }
