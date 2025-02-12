b65dfb9d7831fb0e6fb9023c043fe33f293f4f61
이융의 <121233350+iyungui@users.noreply.github.com>
2025-02-12 12:18:06 +0900

    [level 0] Title: 이어 붙인 수, Time: 0.14 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.swift
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.swift
@@ -1,14 +1,18 @@
 import Foundation
 
 func solution(_ num_list:[Int]) -> Int {
-    var odd: String = ""
-    var even: String = ""
-    for num in num_list {
-        if(num % 2 == 0) {
-            even.append(String(num))
-        } else {
-            odd.append(String(num))
-        }
-    }
-    return Int(odd)! + Int(even)!
+    // var odd: String = ""
+    // var even: String = ""
+    // for num in num_list {
+    //     if(num % 2 == 0) {
+    //         even.append(String(num))
+    //     } else {
+    //         odd.append(String(num))
+    //     }
+    // }
+    // return Int(odd)! + Int(even)!
+    
+    let odd = Int(num_list.filter { $0 % 2 == 1 }.map(String.init).joined())!
+    let even = Int(num_list.filter { $0 % 2 == 0 }.map(String.init).joined())!
+    return odd + even
 }
