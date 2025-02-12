8dda59882f638e0052192279163be1415de3b872
이융의 <121233350+iyungui@users.noreply.github.com>
2025-02-12 11:38:29 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 1, Time: 109.26 ms, Memory: 215 MB -BaekjoonHub

--- 프로그래머스/0/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.swift
+++ 프로그래머스/0/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.swift
@@ -1,9 +1,21 @@
 import Foundation
 
 func solution(_ arr:[Int]) -> [Int] {
+    /* 오답
     return arr.map { 
-        if ($0 >= 50 && $0 % 2 == 0) { return $0 / 2 }
-        else if ($0 < 50 && $0 % 2 == 1) { return $0 * 2 }
-        return $0
-    }
+        if ($0 >= 50 && $0 % 2 == 0) { $0 /= 2 }  // 오류: $0 직접 수정 불가
+        else if ($0 < 50 && $0 % 2 == 1) { $0 *= 2}  // 오류: $0 직접 수정 불가
+        // 기본 리턴값 누락
+    }   
+    */
+    
+    // S1 (오답 수정)       // best
+    // return arr.map { 
+    //     if ($0 >= 50 && $0 % 2 == 0) { return $0 / 2 }
+    //     else if ($0 < 50 && $0 % 2 == 1) { return $0 * 2 }
+    //     return $0
+    // }
+    
+    // S2 삼항 연산자 사용
+    return arr.map { n in n >= 50 && n % 2 == 0 ? n / 2 : n < 50 && n % 2 == 1 ? n * 2 : n }
 }
