5ea10e84932deed34baa5709cd2ac3104c91c1b3
이융의 <121233350+iyungui@users.noreply.github.com>
2025-02-12 11:44:40 +0900

    [level 0] Title: n보다 커질 때까지 더하기, Time: 0.03 ms, Memory: 16.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.swift
@@ -0,0 +1,31 @@
+import Foundation
+
+func solution(_ numbers:[Int], _ n:Int) -> Int {
+    // 오답
+    // var sum : Int = 0
+    // numbers.map {
+    //     sum += $0
+    //     if(sum > n) {
+    //         break
+    //     }
+    // }
+    // return sum
+    
+    var sum = 0
+    for num in numbers {
+        sum += num
+        if sum > n {
+            break  // for 문 안에서는 break 사용 가능
+        }
+    }
+    return sum
+}
+/*
+map을 사용하면 안 되는 이유:
+
+map은 변환을 위한 함수 (각 요소를 다른 것으로 변환)
+중간에 중단할 수 없음 (break 안됨)
+부수 효과(side effect)를 위한 용도가 아님
+
+이 문제는 단순 순회가 목적이므로 for 문이나 forEach가 더 적절합니다.
+*/
