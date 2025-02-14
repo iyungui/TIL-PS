4439538972b4ad0afe46a6fd4ab1a8d0b100b60e
이융의 <121233350+iyungui@users.noreply.github.com>
2025-02-14 18:20:11 +0900

    [level 0] Title: 5명씩, Time: 0.67 ms, Memory: 16.1 MB -BaekjoonHub

--- 프로그래머스/0/181886. 5명씩/5명씩.swift
+++ 프로그래머스/0/181886. 5명씩/5명씩.swift
@@ -1,13 +1,5 @@
 import Foundation
 
 func solution(_ names:[String]) -> [String] {
-    var result: [String] = []
-    
-
-    for i in 0..<names.count {
-        if i % 5 == 0 {
-            result.append(names[i])
-        }
-    }
-    return result
+    return stride(from: 0, to: names.count, by: 5).map { names[$0] }
 }
