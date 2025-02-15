bfa5e83ae0451e4b153a823d891b77c24322708e
이융의 <121233350+iyungui@users.noreply.github.com>
2025-02-15 18:08:38 +0900

    [level 0] Title: 가까운 1 찾기, Time: 0.08 ms, Memory: 18.2 MB -BaekjoonHub

--- 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.swift
+++ 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.swift
@@ -1,10 +1,5 @@
 import Foundation
 
 func solution(_ arr:[Int], _ idx:Int) -> Int {
-    for i in idx..<arr.count {
-        if arr[i] == 1 {
-            return i
-        }
-    }
-    return -1
+    return arr[idx...].firstIndex(of: 1) ?? -1
 }
