3be25f10ca1b2278d9dab56561ee32a871f7ef86
이융의 <121233350+iyungui@users.noreply.github.com>
2025-02-15 17:22:36 +0900

    [level 0] Title: n 번째 원소까지, Time: 0.02 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/0/181889. n 번째 원소까지/n 번째 원소까지.swift
+++ 프로그래머스/0/181889. n 번째 원소까지/n 번째 원소까지.swift
@@ -1,5 +1,7 @@
 import Foundation
 
 func solution(_ num_list:[Int], _ n:Int) -> [Int] {
-    return num_list[0..<n].map { $0 }
+    // return num_list[0..<n].map { $0 }
+    // return Array(num_list.prefix(n))     // 왜 Array 로 묶어야 하는지?
+    return Array(num_list[0..<n])
 }
