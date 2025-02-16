5ad3f1c938f2fd329eac0fcccabc4025faed247f
이융의 <121233350+iyungui@users.noreply.github.com>
2025-02-16 22:30:15 +0900

    [level 0] Title: 글자 이어 붙여 문자열 만들기, Time: 0.54 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/0/181915. 글자 이어 붙여 문자열 만들기/글자 이어 붙여 문자열 만들기.swift
+++ 프로그래머스/0/181915. 글자 이어 붙여 문자열 만들기/글자 이어 붙여 문자열 만들기.swift
@@ -1,6 +1,9 @@
 import Foundation
 
 func solution(_ my_string:String, _ index_list:[Int]) -> String {
-    let strArr = Array(my_string)
-    return index_list.map { String(strArr[$0]) }.joined()
+    // let strArr = Array(my_string)    // [Character]
+    // return index_list.map { String(strArr[$0]) }.joined()
+    
+    let strArr = my_string.map { String($0) }   // [String]
+    return index_list.reduce("") { $0 + strArr[$1] }
 }
