695278e388572941d6f2d6e330bb83c14fd5c1cf
이융의 <121233350+iyungui@users.noreply.github.com>
2025-02-16 18:01:15 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 0.03 ms, Memory: 16.1 MB -BaekjoonHub

--- 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.swift
+++ 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.swift
@@ -1,5 +1,6 @@
 import Foundation
 
 func solution(_ my_string:String, _ is_prefix:String) -> Int {
-    return my_string.hasPrefix(is_prefix) ? 1 : 0
+    // return my_string.hasPrefix(is_prefix) ? 1 : 0
+    is_prefix == my_string.prefix(is_prefix.count) ? 1 : 0
 }
