da6baa0a238b75b1cb39e7f2a3e2ac8fd0a18688
이융의 <121233350+iyungui@users.noreply.github.com>
2025-02-19 22:57:11 +0900

    [level 0] Title: 수 조작하기 1, Time: 22.76 ms, Memory: 16.7 MB -BaekjoonHub

--- 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.swift
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.swift
@@ -1,17 +1,6 @@
 import Foundation
 
 func solution(_ n:Int, _ control:String) -> Int {
-    var answer = n
-    control.map { str in
-        if(str == "w") {
-            answer += 1
-        } else if(str == "s") {
-            answer -= 1
-        } else if(str == "d") {
-            answer += 10
-        } else if(str == "a") {
-            answer -= 10
-        }
-    }
-    return answer
+    let op: [Character: Int] = ["w": 1, "s": -1, "d": 10, "a": -10]
+    return n + control.reduce(0) { $0 + op[$1]! }
 }
