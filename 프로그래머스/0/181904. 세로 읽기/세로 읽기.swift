d6c17b7b16a90cb9d9c3e9a32fc6835b01203772
이융의 <121233350+iyungui@users.noreply.github.com>
2025-02-21 19:09:30 +0900

    [level 0] Title: 세로 읽기, Time: 0.51 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/0/181904. 세로 읽기/세로 읽기.swift
+++ 프로그래머스/0/181904. 세로 읽기/세로 읽기.swift
@@ -1,7 +1,11 @@
 import Foundation
 
 func solution(_ my_string:String, _ m:Int, _ c:Int) -> String {
-    return stride(from: c - 1, to: my_string.count, by: m)
-        .map { String(my_string[my_string.index(my_string.startIndex, offsetBy: $0)]) }.joined()
+    // return stride(from: c - 1, to: my_string.count, by: m)
+    //     .map { String(my_string[my_string.index(my_string.startIndex, offsetBy: $0)]) }.joined()
         
+    let arr = Array(my_string)
+    return stride(from: c - 1, to: arr.count, by: m)
+    .map { String(arr[$0]) }
+    .joined()
 }
