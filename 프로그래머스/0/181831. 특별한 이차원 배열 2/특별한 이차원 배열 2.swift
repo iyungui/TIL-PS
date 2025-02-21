a1365372f7593df88ca2da91462ff6a7206b631d
이융의 <121233350+iyungui@users.noreply.github.com>
2025-02-21 13:45:00 +0900

    [level 0] Title: 특별한 이차원 배열 2, Time: 0.21 ms, Memory: 17 MB -BaekjoonHub

--- 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.swift
+++ 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.swift
@@ -3,10 +3,8 @@ import Foundation
 func solution(_ arr:[[Int]]) -> Int {
     let n: Int = arr.count
     for i in 0..<n {
-        for j in 0..<n {
-            if(arr[i][j] != arr[j][i]) {
-                return 0
-            }
+        for j in (i+1)..<n where arr[i][j] != arr[j][i] {
+            return 0
         }
     }
     return 1
