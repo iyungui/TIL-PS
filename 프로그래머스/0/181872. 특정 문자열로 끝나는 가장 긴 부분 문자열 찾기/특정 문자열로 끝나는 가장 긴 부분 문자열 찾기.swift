4900f70fff332b8b82d998f6f3bed16258d4cfad
이융의 <121233350+iyungui@users.noreply.github.com>
2025-02-23 23:56:34 +0900

    [level 0] Title: 특정 문자열로 끝나는 가장 긴 부분 문자열 찾기, Time: 0.18 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/0/181872. 특정 문자열로 끝나는 가장 긴 부분 문자열 찾기/특정 문자열로 끝나는 가장 긴 부분 문자열 찾기.swift
+++ 프로그래머스/0/181872. 특정 문자열로 끝나는 가장 긴 부분 문자열 찾기/특정 문자열로 끝나는 가장 긴 부분 문자열 찾기.swift
@@ -1,6 +1,14 @@
 import Foundation
 
 func solution(_ myString:String, _ pat:String) -> String {
-    let parts = myString.components(separatedBy: pat)
-    return parts[0..<parts.count - 1].joined(separator: pat) + pat
+    let strArr = Array(myString)
+    let patArr = Array(pat)
+    
+    for i in (0...strArr.count - patArr.count).reversed() {
+        let currentSlice = Array(strArr[i..<i+patArr.count])
+        if currentSlice == patArr {
+            return String(strArr[...(i+patArr.count-1)])
+        }
+    }
+    return ""
 }
