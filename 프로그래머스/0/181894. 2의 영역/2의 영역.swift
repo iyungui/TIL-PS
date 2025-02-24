2fe421dbe108de04dd3dd21e9b9824187ab2e419
이융의 <121233350+iyungui@users.noreply.github.com>
2025-02-24 19:46:57 +0900

    [level 0] Title: 2의 영역, Time: 8.52 ms, Memory: 22.4 MB -BaekjoonHub

--- 프로그래머스/0/181894. 2의 영역/2의 영역.swift
+++ 프로그래머스/0/181894. 2의 영역/2의 영역.swift
@@ -1,11 +1,17 @@
 import Foundation
 
 func solution(_ arr:[Int]) -> [Int] {
-    let indices = arr.enumerated().filter { $0.element == 2 }.map { $0.offset }
-    guard !indices.isEmpty else {
-        return [-1]
+    // let indices = arr.enumerated().filter { $0.element == 2 }.map { $0.offset }
+    // guard !indices.isEmpty else {
+    //     return [-1]
+    // }
+    // let first = indices[0]
+    // let last = indices[indices.count - 1]
+    // return Array(arr[first...last])
+    
+    if let s = arr.firstIndex(of: 2), let e = arr.lastIndex(of: 2) {
+        return Array(arr[s...e])
+    } else {
+        return [-1]   
     }
-    let first = indices[0]
-    let last = indices[indices.count - 1]
-    return Array(arr[first...last])
 }
