51b8f408ea921dbf43f0d73d9c0619ff1b50784e
이융의 <121233350+iyungui@users.noreply.github.com>
2025-02-24 17:59:40 +0900

    [level 0] Title: 문자열 묶기, Time: 46.50 ms, Memory: 23.7 MB -BaekjoonHub

--- 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.swift
+++ 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.swift
@@ -1,9 +1,11 @@
 import Foundation
 
 func solution(_ strArr:[String]) -> Int {
-    var cnt = Array(repeating: 0, count: 31) 
-    strArr.forEach {
-        cnt[$0.count] += 1
-    }
-    return cnt.max()!
+    // var cnt = Array(repeating: 0, count: 31) 
+    // strArr.forEach {
+    //     cnt[$0.count] += 1
+    // }
+    // return cnt.max()!
+    let groups = Dictionary(grouping: strArr) { $0.count }  // count 문자열 길이가 키가 됨
+    return groups.values.map { $0.count }.max() ?? 0
 }
