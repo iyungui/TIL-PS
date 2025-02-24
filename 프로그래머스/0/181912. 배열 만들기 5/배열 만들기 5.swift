27ba72fc88029e348da098f816dd842d992c73a5
이융의 <121233350+iyungui@users.noreply.github.com>
2025-02-24 15:58:43 +0900

    [level 0] Title: 배열 만들기 5, Time: 20.61 ms, Memory: 18.9 MB -BaekjoonHub

--- 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.swift
+++ 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.swift
@@ -1,5 +1,11 @@
 import Foundation
 
 func solution(_ intStrs:[String], _ k:Int, _ s:Int, _ l:Int) -> [Int] {
-    return intStrs.map { Int($0.prefix(s + l).suffix(l))! }.filter { $0 > k }
+    // return intStrs.map { Int($0.prefix(s + l).suffix(l))! }.filter { $0 > k }
+    
+    return intStrs.map {
+        let startIdx = $0.index($0.startIndex, offsetBy: s)
+        let endIdx = $0.index(startIdx, offsetBy: l)
+        return Int(String($0[startIdx..<endIdx]))!
+    }.filter { $0 > k }
 }
