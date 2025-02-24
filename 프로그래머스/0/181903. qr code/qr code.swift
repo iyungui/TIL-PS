7210ee38afeaf014ba5ce715fadc869ea9f98d4b
이융의 <121233350+iyungui@users.noreply.github.com>
2025-02-25 00:41:50 +0900

    [level 0] Title: qr code, Time: 0.46 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/0/181903. qr code/qr code.swift
+++ 프로그래머스/0/181903. qr code/qr code.swift
@@ -1,12 +1,5 @@
 import Foundation
 
 func solution(_ q:Int, _ r:Int, _ code:String) -> String {
-    var result: String = ""
-    let code = Array(code)
-    for (offset, element) in code.enumerated() {
-        if(offset % q == r) {
-            result += String(element)
-        }
-    }
-    return result
+    return code.enumerated().filter { $0.offset % q == r }.map { String($0.element) }.joined()
 }
