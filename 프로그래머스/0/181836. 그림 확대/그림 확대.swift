efdbafd018436ac954689aa19db52f2c2b57b812
이융의 <121233350+iyungui@users.noreply.github.com>
2025-02-25 21:21:07 +0900

    [level 0] Title: 그림 확대, Time: 0.15 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/0/181836. 그림 확대/그림 확대.swift
+++ 프로그래머스/0/181836. 그림 확대/그림 확대.swift
@@ -12,4 +12,19 @@ func solution(_ picture:[String], _ k:Int) -> [String] {
         }
     }
     return result
+    // S1
+    // var result: [String] = []
+    // for row in picture {
+    //     var str: String = ""
+    //     for i in row {
+    //         str += String(repeating: i, count: k)
+    //     }
+    //     for _ in 0..<k {
+    //         result.append(str)
+    //     }
+    // }
+    // return result
+    
+    // S2
+    // return picture.flatMap { Array(repeating: $0.map { String(repeating: $0, count: k) }.joined(), count: k) }
 }
