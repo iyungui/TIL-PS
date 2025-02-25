e370c31f924089cfe0f9cde7f126da1eaad5199f
이융의 <121233350+iyungui@users.noreply.github.com>
2025-02-25 11:16:19 +0900

    [level 0] Title: 문자 개수 세기, Time: 0.34 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.swift
+++ 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.swift
@@ -2,7 +2,17 @@ import Foundation
 
 func solution(_ my_string:String) -> [Int] {
     var result = Array(repeating: 0, count: 52)
-    
+    my_string.forEach { c in
+        if c.isUppercase {
+            result[Int(c.asciiValue! - Character("A").asciiValue!)] += 1
+        } else {
+            result[Int(c.asciiValue! - Character("a").asciiValue!) + 26] += 1
+        }
+    }
+    return result
+}
+
+/*
     for c in my_string {
         if let asciiValue = c.asciiValue {
             if asciiValue >= 97 {   // 소문자
@@ -14,5 +24,4 @@ func solution(_ my_string:String) -> [Int] {
             }
         }
     }
-    return result
-}
+*/
