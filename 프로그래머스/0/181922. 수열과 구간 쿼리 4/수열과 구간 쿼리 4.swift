a8779fd38903a2deea875bb9077c5632b9b4262e
이융의 <121233350+iyungui@users.noreply.github.com>
2025-02-25 10:24:56 +0900

    [level 0] Title: 수열과 구간 쿼리 4, Time: 100.77 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.swift
+++ 프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.swift
@@ -4,11 +4,7 @@ func solution(_ arr:[Int], _ queries:[[Int]]) -> [Int] {
     var arr = arr
     queries.forEach { num in
         let (s, e, k) = (num[0], num[1], num[2])
-        for i in s...e {
-            if i % k == 0 {
-                arr[i] += 1
-            }
-        }
+        (s...e).forEach { if $0 % k == 0 { arr[$0] += 1 } }
     }
     return arr
 }
