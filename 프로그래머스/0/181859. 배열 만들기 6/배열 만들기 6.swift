f53f2d62b9ec9a0817b47990fad0c80259dd1fd2
이융의 <121233350+iyungui@users.noreply.github.com>
2025-02-25 10:46:01 +0900

    [level 0] Title: 배열 만들기 6, Time: 264.13 ms, Memory: 57.9 MB -BaekjoonHub

--- 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.swift
+++ 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.swift
@@ -1,15 +1,12 @@
 import Foundation
 
 func solution(_ arr:[Int]) -> [Int] {
-    var i = 0
-    var stk: [Int] = []
-    while i < arr.count {
-        if stk.isEmpty || stk[stk.count - 1] != arr[i] {
-            stk.append(arr[i])
-            i += 1
-        } else if stk[stk.count - 1] == arr[i] {
-            stk.remove(at: stk.count - 1)
-            i += 1
+    var stk = [Int]()
+    for num in arr {
+        if stk.isEmpty || stk.last! != num {
+            stk.append(num)
+        } else {
+            stk.popLast()
         }
     }
     return stk.isEmpty ? [-1] : stk
