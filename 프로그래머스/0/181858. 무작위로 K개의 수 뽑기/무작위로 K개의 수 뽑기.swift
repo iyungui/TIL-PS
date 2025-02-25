3372a866e4916fc44aa605a75dc18b95d7e70804
이융의 <121233350+iyungui@users.noreply.github.com>
2025-02-25 19:05:35 +0900

    [level 0] Title: 무작위로 K개의 수 뽑기, Time: 1.56 ms, Memory: 17.5 MB -BaekjoonHub

--- 프로그래머스/0/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.swift
+++ 프로그래머스/0/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.swift
@@ -3,7 +3,6 @@ import Foundation
 func solution(_ arr:[Int], _ k:Int) -> [Int] {
     var result: [Int] = []
     var seen: Set<Int> = []
-    
     for num in arr {
         if seen.contains(num) {
             continue
@@ -16,7 +15,7 @@ func solution(_ arr:[Int], _ k:Int) -> [Int] {
         }
     }
     while result.count < k {
-        result.append(-1) 
+        result.append(-1)
     }
     return result
 }
