ba5b2e0d5df7627932da411d73435de85be19134
이융의 <121233350+iyungui@users.noreply.github.com>
2025-03-01 22:20:39 +0900

    [level 0] Title: 부분 문자열 이어 붙여 문자열 만들기, Time: 0.47 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/0/181911. 부분 문자열 이어 붙여 문자열 만들기/부분 문자열 이어 붙여 문자열 만들기.swift
+++ 프로그래머스/0/181911. 부분 문자열 이어 붙여 문자열 만들기/부분 문자열 이어 붙여 문자열 만들기.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 func solution(_ my_strings:[String], _ parts:[[Int]]) -> String {
-    return zip(my_strings, parts).map {
-        String($0.0.prefix($0.1[1] + 1).suffix($0.1[1] - $0.1[0] + 1))
+    return zip(my_strings, parts).map { (str, part) in
+        String(str.prefix(part[1] + 1).suffix(part[1] - part[0] + 1))
     }.joined()
 }
