e2b127dee20b3a3892e91b0b3f5fdb7e7f588351
이융의 <121233350+iyungui@users.noreply.github.com>
2025-03-01 17:01:32 +0900

    [level 0] Title: n보다 커질 때까지 더하기, Time: 0.03 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/0/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.swift
+++ 프로그래머스/0/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.swift
@@ -1,53 +1,21 @@
 import Foundation
 
 func solution(_ numbers:[Int], _ n:Int) -> Int {
-    // 오답
-    // var sum : Int = 0
-    // numbers.map {
-    //     sum += $0
-    //     if(sum > n) {
-    //         break
-    //     }
+    // var sum = 0
+    // numbers.forEach {
+    //     if sum <= n { sum += $0 }
     // }
     // return sum
     
-    // S1 for + if
-    // var sum = 0
-    // for num in numbers {
+    // for num in numbers where sum <= n {
     //     sum += num
-    //     if sum > n { break }
     // }
     // return sum
     
-    // S2 for + where
-    // var sum = 0
-    // for num in numbers where sum <= n { sum += num }
-    // return sum
-
-    // S3 forEach
-    // var sum = 0
-    // numbers.forEach { (num: Int) -> Void in
-    //     if(sum <= n) {
-    //         sum += num
-    //     } 
+    // return numbers.reduce(0) {
+    //     if $0 <= n { return $0 + $1 }
+    //     return $0
     // }
-    // return sum
     
-    // S4 reduce (축약)   // best
     return numbers.reduce(0) { $0 > n ? $0 : $0 + $1 }
-    
-    // S4 reduce (풀코드) 
-    // return numbers.reduce(0, { (result: Int, element: Int) -> Int in
-    //     result > n ? result : result + element
-    // })
-    
 }
-/*
-map을 사용하면 안 되는 이유:
-
-map은 변환을 위한 함수 (각 요소를 다른 것으로 변환)
-중간에 중단할 수 없음 (break 안됨)
-부수 효과(side effect)를 위한 용도가 아님
-
-이 문제는 단순 순회가 목적이므로 for 문이나 forEach가 더 적절합니다.
-*/
