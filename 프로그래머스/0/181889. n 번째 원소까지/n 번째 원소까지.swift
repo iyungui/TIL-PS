68efb1e3cc788c36e59233dbf04eab1510183751
이융의 <121233350+iyungui@users.noreply.github.com>
2025-03-01 17:34:18 +0900

    [level 0] Title: n 번째 원소까지, Time: 0.03 ms, Memory: 16.1 MB -BaekjoonHub

--- 프로그래머스/0/181889. n 번째 원소까지/n 번째 원소까지.swift
+++ 프로그래머스/0/181889. n 번째 원소까지/n 번째 원소까지.swift
@@ -1,10 +1,6 @@
 import Foundation
 
 func solution(_ num_list:[Int], _ n:Int) -> [Int] {
-    // return num_list[0..<n].map { $0 }
-    
-    return Array(num_list.prefix(n))     
-    // 왜 Array 로 묶어야 하는지? => prefix의 반환타입은 ArraySlice<Int> 형임. 이것은 원본 배열을 "참조"하는 것으로, 원본 배열이 변경되면 같이 변경됨. 보통 완전하게 사용하기 위해, Array로 형변환 하여 사용함.
-    
     // return Array(num_list[0..<n])
+    return Array(num_list.prefix(n))
 }
