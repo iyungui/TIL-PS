d89332aa56fb4bbed9c30f5815546e1917c2da7d
이융의 <121233350+iyungui@users.noreply.github.com>
2025-03-04 14:44:26 +0900

    [level 0] Title: 수 조작하기 2, Time: 61.04 ms, Memory: 20.8 MB -BaekjoonHub

--- 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.swift
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.swift
@@ -2,5 +2,5 @@ import Foundation
 
 func solution(_ numLog:[Int]) -> String {
     let diff: [Int: String] = [1: "w", -1: "s", 10: "d", -10: "a"]
-    return zip(numLog, numLog.dropFirst()).map { diff[($1 - $0)]! }.joined()
+    return zip(numLog, numLog.dropFirst()).map { diff[$1 - $0]! }.joined()
 }
