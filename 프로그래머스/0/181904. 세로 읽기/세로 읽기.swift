2f21887e6330b23ccdf4f42d3c22e2ee40e6d987
이융의 <121233350+iyungui@users.noreply.github.com>
2025-03-08 16:33:23 +0900

    [level 0] Title: 세로 읽기, Time: 0.51 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/0/181904. 세로 읽기/세로 읽기.swift
+++ 프로그래머스/0/181904. 세로 읽기/세로 읽기.swift
@@ -1,20 +1,7 @@
 import Foundation
 
 func solution(_ my_string:String, _ m:Int, _ c:Int) -> String {
-    
-    // S1: stride - String 그대로 (String.index 타입 사용)
-    // return stride(from: c - 1, to: my_string.count, by: m)
-    //     .map { String(my_string[my_string.index(my_string.startIndex, offsetBy: $0)]) }.joined()
-        
-    // S2: stride - String 을 [Character]로 형변환 후 사용
-    // let arr = Array(my_string)
-    // return stride(from: c - 1, to: arr.count, by: m)
-    // .map { String(arr[$0]) }
-    // .joined()
-    
-    // 문자열을 m개씩 나누어서 접근
-    return my_string.enumerated()
-        .filter { $0.offset % m == c - 1 }
-        .map { String($0.element) }
-        .joined()
+    my_string.enumerated().filter {
+        $0.offset % m == c - 1
+    }.map { String($0.element) }.joined()
 }
