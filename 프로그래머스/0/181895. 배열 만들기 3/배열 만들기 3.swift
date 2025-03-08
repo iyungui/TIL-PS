104c8c5e6a19739c3f5c5a41b19daf40f0f028e2
이융의 <121233350+iyungui@users.noreply.github.com>
2025-03-08 16:23:48 +0900

    [level 0] Title: 배열 만들기 3, Time: 0.28 ms, Memory: 26.1 MB -BaekjoonHub

--- 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.swift
+++ 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.swift
@@ -1,5 +1,7 @@
 import Foundation
 
 func solution(_ arr:[Int], _ intervals:[[Int]]) -> [Int] {
-    intervals.flatMap { arr[$0[0]...$0[1]] }
+    return intervals.flatMap {
+        arr[$0[0]...$0[1]]
+    }
 }
