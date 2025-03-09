6c24fb752c1c9c44fcda22c14504a8902c550a0e
이융의 <121233350+iyungui@users.noreply.github.com>
2025-03-09 11:43:39 +0900

    [level 0] Title: A 강조하기, Time: 0.02 ms, Memory: 16 MB -BaekjoonHub

--- 프로그래머스/0/181874. A 강조하기/A 강조하기.swift
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.swift
@@ -1,6 +1,15 @@
 import Foundation
 
 func solution(_ myString:String) -> String {
-    // myString.map { $0.lowercased() == "a" ? "A" : $0.lowercased() }.joined()
-    myString.lowercased().replacingOccurrences(of: "a", with: "A")
+    var result: String = ""
+    myString.forEach {
+        if $0 == "a" {
+            result.append("A")
+        } else if $0 >= "B" && $0 <= "Z" {  // A가 아닌 대문자 알파벳
+            result.append($0.lowercased())
+        } else {    // A
+            result.append(String($0))
+        }
+    }
+    return result
 }
