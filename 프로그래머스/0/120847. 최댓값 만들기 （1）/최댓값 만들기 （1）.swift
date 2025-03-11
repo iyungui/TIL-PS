fa295324468a4d448f146238df66fdf432e86b67
이융의 <121233350+iyungui@users.noreply.github.com>
2025-03-11 11:37:53 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.49 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.swift
+++ 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.swift
@@ -1,11 +1,14 @@
 import Foundation
 
 func solution(_ numbers:[Int]) -> Int {
-    var result = 0
-    for i in (0..<numbers.count) {
-        for j in (i + 1..<numbers.count) {
-            result = max(result, numbers[i] * numbers[j])
-        }
-    }
-    return result
+    // var result = 0
+    // for i in (0..<numbers.count) {
+    //     for j in (i + 1..<numbers.count) {
+    //         result = max(result, numbers[i] * numbers[j])
+    //     }
+    // }
+    // return result
+    
+    let a = numbers.sorted()
+    return a[a.count - 1] * a[a.count - 2]
 }
