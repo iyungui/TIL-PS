790cd1e24c2376c9ed6e1870613a2211c6ae3d4e
이융의 <121233350+iyungui@users.noreply.github.com>
2025-03-11 11:27:02 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.07 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.swift
+++ 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.swift
@@ -1,10 +1,12 @@
 import Foundation
 
 func solution(_ n:Int) -> Int {
-    var n = n, sum = 0
-    while(n > 0) {
-        sum += n % 10
-        n /= 10
-    }
-    return sum
+    // var n = n, sum = 0
+    // while(n > 0) {
+    //     sum += n % 10
+    //     n /= 10
+    // }
+    // return sum
+    
+    return String(n).map { Int(String($0))! }.reduce(0, +)
 }
