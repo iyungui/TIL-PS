2dccbb4d6dd23b0b3561010cb99270abd4fe8ea2
이융의 <121233350+iyungui@users.noreply.github.com>
2025-03-11 10:28:31 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.06 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.swift
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.swift
@@ -1,9 +1,6 @@
 import Foundation
 
 func solution(_ num_list:[Int]) -> [Int] {
-    var odd = 0, even = 0
-    num_list.forEach {
-        $0 % 2 == 0 ? (even += 1) : (odd += 1)
-    }
-    return [even, odd]
+    return [num_list.filter {$0 % 2 == 0}.count, 
+            num_list.filter {$0 % 2 == 1}.count]
 }
