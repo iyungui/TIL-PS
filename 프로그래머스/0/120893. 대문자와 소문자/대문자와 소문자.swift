be1caad44bd9e56ff947ee88e7555ea02a0a1eaf
이융의 <121233350+iyungui@users.noreply.github.com>
2025-03-12 14:48:14 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.40 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.swift
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.swift
@@ -2,10 +2,6 @@ import Foundation
 
 func solution(_ my_string:String) -> String {
     return my_string.map {
-        if $0 >= "A" && $0 <= "Z" {
-            return $0.lowercased()
-        } else {
-            return $0.uppercased()
-        }
+        $0.isLowercase ? $0.uppercased() : $0.lowercased()
     }.joined()
 }
