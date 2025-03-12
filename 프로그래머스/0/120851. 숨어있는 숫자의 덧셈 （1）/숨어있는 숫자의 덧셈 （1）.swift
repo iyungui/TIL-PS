a7dc2e8fde04dc0deeeab546158875b5beb3c89c
이융의 <121233350+iyungui@users.noreply.github.com>
2025-03-12 14:39:56 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.09 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.swift
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.swift
@@ -3,7 +3,9 @@ import Foundation
 func solution(_ my_string:String) -> Int {
     my_string
         // .filter { $0 >= "0" && $0 <= "9" }
-        .filter { $0.isNumber }
-        .map { Int(String($0))! }
+        // .filter { $0.isNumber }
+        // .map { Int(String($0))! }
+    
+        .compactMap { Int(String($0)) }
         .reduce(0, +)
 }
