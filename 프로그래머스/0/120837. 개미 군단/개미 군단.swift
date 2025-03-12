541d01428a9e8bfb2d1d103af746c55e90029301
이융의 <121233350+iyungui@users.noreply.github.com>
2025-03-12 14:10:08 +0900

    [level 0] Title: 개미 군단, Time: 0.04 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/0/120837. 개미 군단/개미 군단.swift
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.swift
@@ -1,5 +1,13 @@
 import Foundation
 
 func solution(_ hp:Int) -> Int {
-    return (hp / 5) + (hp % 5 / 3) + (hp % 5 % 3)
+    // return (hp / 5) + (hp % 5 / 3) + (hp % 5 % 3)
+    
+    var result = 0
+    var hp = hp
+    for i in [5, 3, 1] {
+        result += hp / i
+        hp %= i
+    }
+    return result
 }
