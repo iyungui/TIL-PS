727c77572c059b9bc5245d8e29b67cdd52af6819
이융의 <121233350+iyungui@users.noreply.github.com>
2025-03-13 09:00:16 +0900

    [level 0] Title: 암호 해독, Time: 1.61 ms, Memory: 15.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120892. 암호 해독/암호 해독.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+func solution(_ cipher:String, _ code:Int) -> String {
+    cipher
+        .enumerated()
+        .filter { ($0.offset + 1) % code == 0 }
+        .map { String($0.element) }
+        .joined()
+}
