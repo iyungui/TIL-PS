abef0fe79ef6a74be765aa5a93d7d8dd8a723d4f
이융의 <121233350+iyungui@users.noreply.github.com>
2025-03-13 19:00:37 +0900

    [level 0] Title: 369게임, Time: 0.07 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/0/120891. 369게임/369게임.swift
+++ 프로그래머스/0/120891. 369게임/369게임.swift
@@ -2,6 +2,6 @@ import Foundation
 
 func solution(_ order:Int) -> Int {
     return String(order)
-    .filter { String($0) == "3" || String($0) == "6" || String($0) == "9" }
+    .filter { $0 == "3" || $0 == "6" || $0 == "9" }
     .count
 }
