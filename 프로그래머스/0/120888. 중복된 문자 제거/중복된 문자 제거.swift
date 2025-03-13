98c73b0a3c657735cc425284e8271c6e7f5c3368
이융의 <121233350+iyungui@users.noreply.github.com>
2025-03-13 19:35:32 +0900

    [level 0] Title: 중복된 문자 제거, Time: 0.16 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.swift
+++ 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.swift
@@ -1,16 +1,11 @@
 import Foundation
 
-extension Array where Element: Equatable {
-    var unique: [Element] {
-        var uniqueValues: [Element] = []
-        forEach { item in
-            guard !uniqueValues.contains(item) else { return }
-            uniqueValues.append(item)
-        }
-        return uniqueValues
-    }
-}
-
 func solution(_ my_string:String) -> String {
-    Array(my_string).unique.map { String($0) }.joined()
+    var result = ""
+    for c in my_string {
+        if !result.contains(c) {
+            result += String(c)
+        }
+    }
+    return result
 }
