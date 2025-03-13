7643e511c829c700d3e03f8d959e90dda1bf8d70
이융의 <121233350+iyungui@users.noreply.github.com>
2025-03-13 09:22:26 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 0.36 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.swift
+++ 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.swift
@@ -1,5 +1,6 @@
 import Foundation
 
 func solution(_ my_string:String) -> String {
-    my_string.lowercased().sorted().map {String($0)}.joined()
+    // return my_string.lowercased().sorted().map {String($0)}.joined()
+    return my_string.map { $0.lowercased() }.sorted().joined()
 }
