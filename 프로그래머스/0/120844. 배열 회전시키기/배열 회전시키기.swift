65ed179119104ad6d2169c8070396d779780f8e4
이융의 <121233350+iyungui@users.noreply.github.com>
2025-03-13 18:55:42 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.03 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.swift
+++ 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.swift
@@ -1,13 +1,24 @@
 import Foundation
 
 func solution(_ numbers:[Int], _ direction:String) -> [Int] {
+    // S1
     var result = numbers
     if direction == "right" {
         let lastElement = result.removeLast()
-        result.insert(lastElement, at: 0)
+        result.insert(lastElement, at: 0)  // [lastElement] + result
     } else {
         let firstElement = result.removeFirst()
         result.append(firstElement)
     }
+    
     return result
+    
+    // S2
+    // if direction == "right" {
+    //     return [numbers.last!] + numbers.dropLast()
+    // } else {
+    //     return Array(numbers.dropFirst()) + [numbers.first!]
+    // }
+    
+    
 }
