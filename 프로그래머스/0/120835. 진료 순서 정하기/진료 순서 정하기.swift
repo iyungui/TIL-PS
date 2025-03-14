8dfb2168a33dcdce8cc1f3fd9244943d1c707992
이융의 <121233350+iyungui@users.noreply.github.com>
2025-03-14 23:51:49 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.23 ms, Memory: 16.1 MB -BaekjoonHub

--- 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.swift
+++ 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.swift
@@ -1,15 +1,34 @@
 import Foundation
 
 func solution(_ emergency:[Int]) -> [Int] {
-    var result: [Int] = []
+//     var result: [Int] = []
+//     let sortedEmergency = emergency.sorted(by: >)
+//     for i in emergency {
+//         for (offset, element) in sortedEmergency.enumerated() {
+//             if element == i {
+//                 result.append(offset + 1)
+//             }
+//         }
+//     }
+
+//     return result
     let sortedEmergency = emergency.sorted(by: >)
-    for i in emergency {
-        for (offset, element) in sortedEmergency.enumerated() {
-            if element == i {
-                result.append(offset + 1)
-            }
-        }
+    let rankDict = Dictionary(uniqueKeysWithValues: sortedEmergency.enumerated().map { (offset, element) in 
+        (element, offset + 1)
+    })
+    
+    return emergency.map { rankDict[$0]! }
+}
+
+/*
+
+func solution(_ emergency:[Int]) -> [Int] {
+    var answer: [Int] = []
+
+    for tmp in emergency {
+        answer.append(emergency.filter { $0 > tmp }.count + 1)
     }
 
-    return result
+    return answer
 }
+*/
