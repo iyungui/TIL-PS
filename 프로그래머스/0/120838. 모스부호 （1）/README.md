d1ef542c162efd2459bfd090a2da5238e4ebdea8
이융의 <121233350+iyungui@users.noreply.github.com>
2025-03-14 19:19:40 +0900

    [level 0] Title: 모스부호 (1), Time: 0.08 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/0/120838. 모스부호 （1）/README.md
+++ 프로그래머스/0/120838. 모스부호 （1）/README.md
@@ -1,10 +1,10 @@
 # [level 0] 모스부호 (1) - 120838 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120838) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120838?language=swift) 
 
 ### 성능 요약
 
-메모리: 3.66 MB, 시간: 0.03 ms
+메모리: 16.4 MB, 시간: 0.08 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 01일 12:48:35
+2025년 03월 14일 19:19:37
 
 ### 문제 설명
 
---
+++ 프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.swift
@@ -0,0 +1,42 @@
+import Foundation
+
+func solution(_ letter:String) -> String {
+    let morse: [String: String] = [
+        ".-": "a",
+        "-...": "b",
+        "-.-.": "c",
+        "-..": "d",
+        ".": "e",
+        "..-.": "f",
+        "--.": "g",
+        "....": "h",
+        "..": "i",
+        ".---": "j",
+        "-.-": "k",
+        ".-..": "l",
+        "--": "m",
+        "-.": "n",
+        "---": "o",
+        ".--.": "p",
+        "--.-": "q",
+        ".-.": "r",
+        "...": "s",
+        "-": "t",
+        "..-": "u",
+        "...-": "v",
+        ".--": "w",
+        "-..-": "x",
+        "-.--": "y",
+        "--..": "z"
+    ]
+    
+    let morseLetters = letter.split(separator: " ")
+    var result = ""
+    for morseCode in morseLetters {
+        if let c = morse[String(morseCode)] {
+            result += c
+        }
+    }
+    
+    return result
+}
