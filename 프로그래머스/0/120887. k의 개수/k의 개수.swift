45b41ccb938153f90ffff3fc1e806793cb36e047
이융의 <121233350+iyungui@users.noreply.github.com>
2025-03-14 23:08:11 +0900

    [level 0] Title: k의 개수, Time: 161.40 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/0/120887. k의 개수/k의 개수.swift
+++ 프로그래머스/0/120887. k의 개수/k의 개수.swift
@@ -1,13 +1,16 @@
 import Foundation
 
 func solution(_ i:Int, _ j:Int, _ k:Int) -> Int {
-    let a = stride(from: i, through: j, by: 1)
-        .filter { String($0).contains(String(k)) }
-        .map { String($0) }
-        .joined()
-    var sum = 0
-    a.forEach {
-        if k == Int(String($0))! { sum += 1}
-    }
-    return sum
+    // let a = stride(from: i, through: j, by: 1)
+    //     .filter { String($0).contains(String(k)) }
+    //     .map { String($0) }
+    //     .joined()
+    // var sum = 0
+    // a.forEach {
+    //     if k == Int(String($0))! { sum += 1}
+    // }
+    // return sum
+    return (i...j).map {
+        String($0).filter { String($0) == String(k) }.count
+    }.reduce(0, +)
 }
