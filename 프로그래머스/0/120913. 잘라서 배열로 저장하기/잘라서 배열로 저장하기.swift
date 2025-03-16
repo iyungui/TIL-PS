6d01dff3bec2a184c19b96ebb76b9213f76af12e
이융의 <121233350+iyungui@users.noreply.github.com>
2025-03-16 17:04:29 +0900

    [level 0] Title: 잘라서 배열로 저장하기, Time: 0.15 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.swift
+++ 프로그래머스/0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.swift
@@ -4,7 +4,9 @@ func solution(_ my_str:String, _ n:Int) -> [String] {
     var result: [String] = []
     for i in stride(from: 0, to: my_str.count, by: n) {
         let startIdx = my_str.index(my_str.startIndex, offsetBy: i)
-        let endIdx = my_str.index(startIdx, offsetBy: min(n, my_str.count - i))
+        // let endIdx = my_str.index(startIdx, offsetBy: min(n, my_str.count - i))
+        let endIdx = my_str.index(startIdx, offsetBy: n, limitedBy: my_str.endIndex) ?? my_str.endIndex
+
         let substr = my_str[startIdx..<endIdx]
         result.append(String(substr))
     }
