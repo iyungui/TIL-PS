8cc4ca8a8452761003bab9eebf421a8d89fd3543
이융의 <121233350+iyungui@users.noreply.github.com>
2025-03-16 15:52:49 +0900

    [level 0] Title: 소인수분해, Time: 0.19 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/0/120852. 소인수분해/소인수분해.swift
+++ 프로그래머스/0/120852. 소인수분해/소인수분해.swift
@@ -3,12 +3,15 @@ import Foundation
 func solution(_ n:Int) -> [Int] {
     var factors = Set<Int>()
     var n = n
-    for i in (2...n) {
+    var i = 2
+    while i * i <= n {
         while n % i == 0 {
+            print(n)
             factors.insert(i)
             n /= i
         }
+        i += 1
     }
-    // if n > 1 { factors.insert(n) }
+    if n > 1 { factors.insert(n) }
     return factors.sorted()
 }
