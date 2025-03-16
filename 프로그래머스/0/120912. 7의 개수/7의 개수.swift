d9ab79f1b3ea9bbf6b0c61feaed11778e4ac5594
이융의 <121233350+iyungui@users.noreply.github.com>
2025-03-16 14:17:55 +0900

    [level 0] Title: 7의 개수, Time: 0.12 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/0/120912. 7의 개수/7의 개수.swift
+++ 프로그래머스/0/120912. 7의 개수/7의 개수.swift
@@ -1,14 +1,5 @@
 import Foundation
 
 func solution(_ array:[Int]) -> Int {
-    var sum = 0
-    let a = array.map { String($0) }
-    a
-    .forEach {
-        $0.forEach {
-            if String($0) == "7" { sum += 1 }
-        }
-    }
-    
-    return sum
+    array.map { String($0) }.joined().filter { $0 == "7" }.count
 }
