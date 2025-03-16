fe88e62dcbe01fc8b1c079c1f5269a5c3614dd47
이융의 <121233350+iyungui@users.noreply.github.com>
2025-03-16 10:56:56 +0900

    [level 0] Title: 가까운 수, Time: 0.08 ms, Memory: 16.1 MB -BaekjoonHub

--- 프로그래머스/0/120890. 가까운 수/README.md
+++ 프로그래머스/0/120890. 가까운 수/README.md
@@ -1,10 +1,10 @@
 # [level 0] 가까운 수 - 120890 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120890?language=swift) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120890?language=swift#) 
 
 ### 성능 요약
 
-메모리: 16.5 MB, 시간: 0.04 ms
+메모리: 16.1 MB, 시간: 0.08 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 16일 10:39:43
+2025년 03월 16일 10:56:54
 
 ### 문제 설명
 
--- 프로그래머스/0/120890. 가까운 수/가까운 수.swift
+++ 프로그래머스/0/120890. 가까운 수/가까운 수.swift
@@ -1,14 +1,15 @@
 import Foundation
 
 func solution(_ array:[Int], _ n:Int) -> Int {
-    var diff = Int.max, answer = 0
-    array.forEach {
-        if diff > abs($0 - n) {
-            diff = abs($0 - n)
-            answer = $0
-        } else if diff == abs($0 - n) && answer > $0 {
-            answer = $0
-        }
-    }
-    return answer
+    return array.min(by: { (abs($0 - n), $0) < (abs($1 - n), $1) })!
+    // var diff = Int.max, answer = 0
+    // array.forEach {
+    //     if diff > abs($0 - n) {
+    //         diff = abs($0 - n)
+    //         answer = $0
+    //     } else if diff == abs($0 - n) && answer > $0 {
+    //         answer = $0
+    //     }
+    // }
+    // return answer
 }
