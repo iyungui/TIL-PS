511dfebae82c56955f0b1fd25e52db50709a3e29
이융의 <121233350+iyungui@users.noreply.github.com>
2025-03-16 11:38:13 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 0.30 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.swift
+++ 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.swift
@@ -6,10 +6,11 @@ func solution(_ s:String) -> String {
     for c in s {
         counts[c, default: 0] += 1
     }
-    for c in counts {
-        if c.value == 1 {
-            answer += String(c.key)
-        }
-    }
-    return String(answer.sorted())
+    return counts.filter { $0.value == 1 }.keys.sorted().reduce("") { $0 + String($1) }
+    // for c in counts {
+    //     if c.value == 1 {
+    //         answer += String(c.key)
+    //     }
+    // }
+    // return String(answer.sorted())
 }
