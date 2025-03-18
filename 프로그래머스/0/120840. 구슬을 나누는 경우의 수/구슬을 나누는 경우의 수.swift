2da91e890dab78220eb1a24b4043250cf8500e37
이융의 <121233350+iyungui@users.noreply.github.com>
2025-03-18 12:08:40 +0900

    [level 0] Title: 구슬을 나누는 경우의 수, Time: 0.14 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.swift
+++ 프로그래머스/0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.swift
@@ -1,12 +1,25 @@
 import Foundation
 
+// func solution(_ balls:Int, _ share:Int) -> Int {
+//     let min = balls - share < share ? balls - share : share
+//     var result: Double = 1
+    
+//     for i in 0..<min {
+//         result *= Double(balls - i) / Double(i + 1)
+//     }
+    
+//     return Int(round(result))
+// }
+
+
+
 func solution(_ balls:Int, _ share:Int) -> Int {
-    let min = balls - share < share ? balls - share : share
-    var result: Double = 1
-    
-    for i in 0..<min {
-        result *= Double(balls - i) / Double(i + 1)
-    }
-    
-    return Int(round(result))
+    var result = (balls - share + 1...balls)
+        .map { Double($0) }
+        .reduce(1.0, *)
+
+    (1...share)
+        .map { result /= Double($0) }
+
+    return Int(result)
 }
