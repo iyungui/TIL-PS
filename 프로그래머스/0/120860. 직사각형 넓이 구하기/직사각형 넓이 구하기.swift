61882621efdaf76fcf1e7c64d19ab8960eddb0b3
이융의 <121233350+iyungui@users.noreply.github.com>
2025-03-19 00:13:26 +0900

    [level 0] Title: 직사각형 넓이 구하기, Time: 0.10 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.swift
+++ 프로그래머스/0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.swift
@@ -1,9 +1,13 @@
 import Foundation
 
 func solution(_ dots:[[Int]]) -> Int {
-    let xValues = dots.map { $0[0] }
-    let yValues = dots.map { $0[1] }
-    let width = xValues.max()! - xValues.min()!
-    let height = yValues.max()! - yValues.min()!
-    return width * height
+    // let xValues = dots.map { $0[0] }
+    // let yValues = dots.map { $0[1] }
+    // let width = xValues.max()! - xValues.min()!
+    // let height = yValues.max()! - yValues.min()!
+    // return width * height
+    
+    // tuple sort (x와 y 정렬)
+    let dots = dots.sorted(by: { ($0[0], $0[1]) < ($1[0], $1[1]) })
+    return (dots.last![0] - dots.first![0]) * (dots.last![1] - dots.first![1])
 }
