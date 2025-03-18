158b99c4b1a41967a499f1575f5e76f6e1acd80b
이융의 <121233350+iyungui@users.noreply.github.com>
2025-03-18 23:37:05 +0900

    [level 0] Title: 종이 자르기, Time: 0.01 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/0/120922. 종이 자르기/종이 자르기.swift
+++ 프로그래머스/0/120922. 종이 자르기/종이 자르기.swift
@@ -4,11 +4,6 @@ func solution(_ M:Int, _ N:Int) -> Int {
     // 한쪽을 1로 만들기까지 count:      2->1 (1)
     // 한쪽 * (다른한쪽 - 1)     :    2(한쪽) * 4(다른한쪽 * 1) = 8
     // 1 + 8 -> 9
-    var m = M, answer = 0
-    while m > 1 {
-        answer += 1
-        m -= 1
-    }
-    answer += (M * (N - 1))
-    return answer
+    // return (M - 1) + (M * (N - 1))
+    return M * N - 1 
 }
