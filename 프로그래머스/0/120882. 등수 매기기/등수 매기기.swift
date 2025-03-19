3c2d4bbed7a1d94532c3e2d1ad5f7acdae5677f2
이융의 <121233350+iyungui@users.noreply.github.com>
2025-03-19 14:02:46 +0900

    [level 0] Title: 등수 매기기, Time: 0.21 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/0/120882. 등수 매기기/등수 매기기.swift
+++ 프로그래머스/0/120882. 등수 매기기/등수 매기기.swift
@@ -1,10 +1,9 @@
 import Foundation
 
 func solution(_ score:[[Int]]) -> [Int] {
-    var answer: [Int] = []
     let total = score.map { $0.reduce(0, +) }
-    for tmp in total {
-        answer.append(total.filter { $0 > tmp }.count + 1)
+    let sortedTotal = total.sorted(by: >)
+    return total.map {
+        (sortedTotal.firstIndex(of: $0) ?? 0) + 1
     }
-    return answer
 }
