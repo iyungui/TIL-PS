3fdb3e27d20f35fb4778cbe00b92272f5422a182
이융의 <121233350+iyungui@users.noreply.github.com>
2025-03-19 23:03:05 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.01 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.swift
+++ 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.swift
@@ -7,33 +7,20 @@ func gcd(_ a: Int, _ b: Int) -> Int {
     return gcd(b, a % b)
 }
 
-func isPrimeNumber(_ n: Int) -> Bool {
-    if n < 2 { return false }
-    for i in 2..<n {
-        if n % i == 0 { return false }
-    }
-    return true
-}
-
 func solution(_ a:Int, _ b:Int) -> Int {
-    var a = a, b = b
-    // 1. 기약분수로 만들기
-    var divisor = gcd(a, b)
-    while divisor != 1 {
-        a /= divisor
-        b /= divisor
-        divisor = gcd(a, b)
-    }
-    print(a, b)
+    // 기약분수의 분모 구하기
+    var denominator = b / gcd(a, b)
     
-    // 2. 소인수 찾기
-    if b == 1 { return 1 }
-    for i in (2...b) {
-        if b % i == 0 && isPrimeNumber(i) {
-            if i == 2 || i == 5 { continue }
-            return 2
-        }
+    // 분모가 2^n * 5^m 형태로 되는지 확인
+    // 2로 나눌 수 있을 때까지 나누기
+    while denominator % 2 == 0 {
+        denominator /= 2
     }
     
-    return 1
+    // 5로 나눌 수 있을 때까지 나누기
+    while denominator % 5 == 0 {
+        denominator /= 5
+    }
+    
+    return denominator == 1 ? 1 : 2
 }
