4546bba319bbf8e91ad9c4a1b64864dc3e1848cc
이융의 <121233350+iyungui@users.noreply.github.com>
2025-03-19 15:02:27 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.25 ms, Memory: 16.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+func solution(_ n:Int) -> Int {
+    // 3의 배수이거나 3이 들어간 숫자는 이전 값에서 +1을 한번 더 해야 함
+    var prev: Int = 0
+    for i in (1...n) {
+        prev += 1
+        while prev % 3 == 0 || String(prev).contains("3") {
+            prev += 1
+        }
+        print("i: \(i), prev: \(prev)")
+    }
+    while prev % 3 == 0 || String(prev).contains("3") {
+        prev += 1
+    }
+    return prev
+}
