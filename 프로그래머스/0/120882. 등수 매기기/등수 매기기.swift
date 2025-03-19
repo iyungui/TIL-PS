6f041b764caa9fc8a5535071eaebd34e52b2ea39
이융의 <121233350+iyungui@users.noreply.github.com>
2025-03-19 13:55:10 +0900

    [level 0] Title: 등수 매기기, Time: 0.08 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/0/120882. 등수 매기기/등수 매기기.swift
+++ 프로그래머스/0/120882. 등수 매기기/등수 매기기.swift
@@ -1,18 +1,10 @@
 import Foundation
 
 func solution(_ score:[[Int]]) -> [Int] {
-    let avg: [Double] = score.map { Double($0[0] + $0[1]) / 2.0 }
-    var result: [Int] = Array(repeating: 0, count: avg.count)
-    
-    for i in (0..<avg.count) {
-        var rank: Int = 1
-        for j in (0..<avg.count) {
-            if avg[i] < avg[j] {
-                rank += 1
-            }
-        }
-        result[i] = rank
+    var answer: [Int] = []
+    let total = score.map { $0.reduce(0, +) }
+    for tmp in total {
+        answer.append(total.filter { $0 > tmp }.count + 1)
     }
-    
-    return result
+    return answer
 }
