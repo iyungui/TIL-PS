e51ca30d2597f28bda528adb2ef4f65c707a53ca
이융의 <121233350+iyungui@users.noreply.github.com>
2025-03-20 21:53:29 +0900

    [level 0] Title: 특이한 정렬, Time: 0.31 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.swift
+++ 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.swift
@@ -1,8 +1,11 @@
 import Foundation
 
 func solution(_ numlist:[Int], _ n:Int) -> [Int] {
-    return numlist
-            .map { (abs($0 - n), $0) }
-            .sorted { ($0.0, $1.1) < ($1.0, $0.1) }
-            .map { $0.1 }
+    let diffList = numlist.map { (abs($0 - n), $0) }    // (거리, 값)
+    
+    // (1)거리 기준으로 오름차순,  같으면 값 기준으로 내림차순
+    let sortedList = diffList.sorted { ($0.0, $1.1) < ($1.0, $0.1) }
+    
+    // 튜플에서 값만 출력
+    return sortedList.map { $0.1 }
 }
