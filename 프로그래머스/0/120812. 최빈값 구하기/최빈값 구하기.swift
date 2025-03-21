c4aa4f130ca3a63251a0d84e3a816f17b98a7ceb
이융의 <121233350+iyungui@users.noreply.github.com>
2025-03-21 19:12:10 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.76 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.swift
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.swift
@@ -1,7 +1,10 @@
 import Foundation
 func solution(_ array:[Int]) -> Int {
     let group = Dictionary.init(grouping: array) { $0 }
+    print(group)
     let max = group.map { $0.1.count }.max()
+    print(max)
     let maxGroup = group.filter { $0.1.count == max }
+    print(maxGroup)
     return maxGroup.count > 1 ? -1 : maxGroup.keys.first!
 }
