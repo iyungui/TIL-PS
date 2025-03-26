020bf54c855cca31b4f37918566142807e515ff5
이융의 <121233350+iyungui@users.noreply.github.com>
2025-03-26 21:48:46 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 0.08 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.swift
+++ 프로그래머스/0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.swift
@@ -5,6 +5,10 @@ func solution(_ lines:[[Int]]) -> Int {
     let mn = lines.map { $0[0] }.min()!
     
     // Calculate the total range and offset
+    // For example the mn value is -1 and mx value is 3, and the offset is 1 because the offset is abs(-1) = 1
+    // So, the range is (-1, 0, 1, 2). (count is 4)
+    // if range is mx + offset = 3 + 1 = 4. 
+    
     let offset = abs(mn)
     let range = mx + offset
     
