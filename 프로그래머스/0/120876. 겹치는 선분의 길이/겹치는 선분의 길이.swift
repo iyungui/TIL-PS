b2db113ca7829721ad7c7df90fafff980d34f0c3
이융의 <121233350+iyungui@users.noreply.github.com>
2025-03-26 22:11:56 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 0.55 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.swift
+++ 프로그래머스/0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.swift
@@ -1,30 +1,14 @@
 import Foundation
 
 func solution(_ lines:[[Int]]) -> Int {
-    let mx = lines.map { $0[1] }.max()!
-    let mn = lines.map { $0[0] }.min()!
     
-    // Calculate the total range and offset
-    // For example the mn value is -1 and mx value is 3, and the offset is 1 because the offset is abs(-1) = 1
-    // So, the range is (-1, 0, 1, 2). (count is 4)
-    // if range is mx + offset = 3 + 1 = 4. 
+    var dict = [Int: Int]() // (range: count)
     
-    let offset = abs(mn)
-    let range = mx + offset
-    
-    var checked = Array(repeating: 0, count: range + 1)
-    lines.forEach {
-        for i in $0[0] + offset..<$0[1] + offset {
-            checked[i] += 1
+    for line in lines {
+        for i in line[0]..<line[1] {
+            dict[i, default: 0] += 1
         }
     }
-    
-    var count: Int = 0
-    for i in 0..<checked.count {
-        if checked[i] > 1 {
-            count += 1
-        }
-    }
-    
-    return count
+    print(dict)
+    return dict.values.filter { $0 > 1 }.count
 }
