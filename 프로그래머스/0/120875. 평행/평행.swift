8ebfd4549b4b1d72316d27a331339f3ac022febf
이융의 <121233350+iyungui@users.noreply.github.com>
2025-03-27 15:18:47 +0900

    [level 0] Title: 평행, Time: 0.06 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/0/120875. 평행/평행.swift
+++ 프로그래머스/0/120875. 평행/평행.swift
@@ -1,27 +1,27 @@
 import Foundation
 
-// 직선의 기울기 구하기
-func getSlope(_ point1: [Int], _ point2: [Int]) -> Double {
-    let x1 = Double(point1[0]), x2 = Double(point2[0])
-    let y1 = Double(point1[1]), y2 = Double(point2[1])
-    
-    return (y1 - y2) / (x1 - x2)
-}
-
 func solution(_ dots:[[Int]]) -> Int {
-    let combi = [
+    let cases = [
         ((0, 1), (2, 3)),
         ((0, 2), (1, 3)),
         ((0, 3), (1, 2))
     ]
     
-    for ((a, b), (c, d)) in combi {
-        let slope1 = getSlope(dots[a], dots[b])
-        let slope2 = getSlope(dots[c], dots[d])
+    for ((a, b), (c, d)) in cases {
+        // 기울기 공식
+        // (a.y - b.y) / (a.x - b.x) == (c.y - d.y) / (c.x - d.x)
+        // 양변에 분모인 (a.x - b.x)(c.x - d.x) 를 곱하면
+        // 결국 (a.y - b.y) * (c.x - d.x) == (c.y - d.y) * (a.x - b.x)
+        //         y1      *     x2      ==     y2      *     x1
+        let y1 = dots[a][1] - dots[b][1]
+        let x2 = dots[c][0] - dots[d][0]
+        let y2 = dots[c][1] - dots[d][1]
+        let x1 = dots[a][0] - dots[b][0]
         
-        if slope1 == slope2 {
+        if y1 * x2 == y2 * x1 {
             return 1
         }
     }
+    
     return 0
 }
