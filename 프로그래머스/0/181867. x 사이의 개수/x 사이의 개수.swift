2d1f549522e4ffd90d7ac3722b4ac67bb85ffc5b
이융의 <121233350+iyungui@users.noreply.github.com>
2025-03-29 14:23:57 +0900

    [level 0] Title: x 사이의 개수, Time: 41.84 ms, Memory: 19 MB -BaekjoonHub

--- 프로그래머스/0/181867. x 사이의 개수/x 사이의 개수.swift
+++ 프로그래머스/0/181867. x 사이의 개수/x 사이의 개수.swift
@@ -1,6 +1,6 @@
 import Foundation
 
 func solution(_ myString:String) -> [Int] {
-    return myString.split(separator: "x", omittingEmptySubsequences: false)
-                  .map { String($0).count }
+    let arr = myString.components(separatedBy: "x")
+    return arr.map { $0.count }
 }
