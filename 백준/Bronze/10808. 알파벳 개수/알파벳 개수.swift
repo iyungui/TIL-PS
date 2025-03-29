fdfd9f4e8c4ec9633b7a0967e106d6a48f953fca
이융의 <121233350+iyungui@users.noreply.github.com>
2025-03-29 18:55:30 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 8 ms, Memory: 79508 KB -BaekjoonHub

--- 백준/Bronze/10808. 알파벳 개수/알파벳 개수.swift
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.swift
@@ -1,11 +1,9 @@
-
 import Foundation
 
 let s = readLine()!
 var cnt = [Int](repeating: 0, count: 26)
 
 for char in s {
-    let index = Int(char.asciiValue! - Character("a").asciiValue!)
-    cnt[index] += 1
+    cnt[Int(char.asciiValue!) - 97] += 1
 }
 print(cnt.map { String($0) }.joined(separator: " "))
