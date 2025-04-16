33076984e1b7ac7ced1609349780b75bce5f3e59
Yung-ui <121233350+iyungui@users.noreply.github.com>
2025-04-16 12:26:04 +0900

    [Bronze II] Title: 트럭 주차, Time: 8 ms, Memory: 79508 KB -BaekjoonHub

--- 백준/Bronze/2979. 트럭 주차/트럭 주차.swift
+++ 백준/Bronze/2979. 트럭 주차/트럭 주차.swift
@@ -1,32 +1,21 @@
 import Foundation
 
-// 주차 요금 A, B, C
-let temp = readLine()!.components(separatedBy: " ").map { Int($0)! }
-let a = temp[0], b = temp[1], c = temp[2]
-
-var arr2: [[Int]] = []
-
+let arr = readLine()!.components(separatedBy: " ").map { Int($0)! }
+let a = arr[0], b = arr[1], c = arr[2]
+var cnt: Array<Int> = Array(repeating: 0, count: 100)
 for _ in 0..<3 {
-    let temp: String = readLine()!
-    arr2.append(temp.components(separatedBy: " ").map { Int($0)! })
-}
-
-var cnt = Array(repeating: 0, count: 100)
-
-arr2.forEach { num in
-    for i in num[0]..<num[1] {
+    let temp = readLine()!.components(separatedBy: " ").map { Int($0)! }
+    for i in temp[0]..<temp[1] {
         cnt[i] += 1
     }
 }
-var result: Int = 0
+var res: Int = 0
 for i in cnt {
-    if i == 1 {
-        result += (a * 1)
-    } else if i == 2 {
-        result += (b * 2)
-    } else if i == 3 {
-        result += (c * 3)
+    switch i {
+    case 1: res += i * a
+    case 2: res += i * b
+    case 3: res += i * c
+    default: continue
     }
 }
-
-print(result)
+print(res)
