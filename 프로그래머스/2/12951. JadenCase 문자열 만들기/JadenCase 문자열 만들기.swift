c34fae0e228e4a1de9525028ab44a00792d81f5a
Yung-ui <121233350+iyungui@users.noreply.github.com>
2025-04-17 14:35:48 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.15 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.swift
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.swift
@@ -1,7 +1,6 @@
 func solution(_ s:String) -> String {
     s
     .components(separatedBy: " ")
-    .filter { $0 != " " }
     .map { (word: String) -> String in
         return word.prefix(1).uppercased() + word.dropFirst().lowercased()
     }   // [String]
