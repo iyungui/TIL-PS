ddc083c3e3dd34a96b999b856e1ec5a96eac8ce0
Yung-ui <121233350+iyungui@users.noreply.github.com>
2025-04-17 14:10:47 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 278.67 ms, Memory: 23.8 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.swift
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.swift
@@ -1,22 +1,13 @@
 import Foundation
 
 func solution(_ s:String) -> [Int] {
-    // 이진 변환의 횟수
-    var cnt = 0
-    // 변환 과정에서 제거된 모든 0의 개수
-    var cnt2 = 0
-    
-    var str: String = s
-    
-    while str != "1" {
-        // 0 제거하기
-        let c = Array(str).filter { $0 != "0" }.count
-        cnt2 += (str.count - c)
-
-        // c 를 이진법으로 표현한 문자열로 바꾸기
-        str = String(c, radix: 2)
-        print(str)
-        cnt += 1
+    var x: String = s
+    var cnt1 = 0, cnt2 = 0
+    while x != "1" {
+        let removeZeroCnt = x.replacingOccurrences(of: "0", with: "").count
+        cnt2 += x.count - removeZeroCnt
+        x = String(removeZeroCnt, radix: 2)
+        cnt1 += 1
     }
-    return [cnt, cnt2]
+    return [cnt1, cnt2]
 }
