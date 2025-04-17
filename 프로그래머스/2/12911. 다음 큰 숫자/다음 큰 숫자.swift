cc246245a56a511961dbf9cd46f28dadba7aa08e
Yung-ui <121233350+iyungui@users.noreply.github.com>
2025-04-17 15:04:20 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.01 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.swift
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.swift
@@ -1,18 +1,15 @@
 import Foundation
 
+
 func solution(_ n:Int) -> Int {
-    var n = n
-    let cnt = String(n, radix: 2).filter { $0 == "1" }.count
-    var cnt2 = 0
-    var str: String
-    guard n != 1000000 else { return n + 1 }
+    var answer : Int = n + 1
+
     while true {
-        n += 1
-        str = String(n, radix: 2)
-        cnt2 = str.filter { $0 == "1" }.count
-        if cnt == cnt2 {
-            return Int(str, radix: 2)!
+        if n.nonzeroBitCount == answer.nonzeroBitCount {
+            break;
         }
+        answer += 1
     }
-    return 0
+
+    return answer
 }
