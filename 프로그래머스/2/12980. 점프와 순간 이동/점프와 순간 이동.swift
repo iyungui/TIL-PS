5f143f2f75c00bbf6dad3cd38e6a221e6c64134e
Yung-ui <121233350+iyungui@users.noreply.github.com>
2025-04-17 18:17:51 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.01 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.swift
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.swift
@@ -1,18 +1,7 @@
 import Foundation
 
 func solution(_ n:Int) -> Int {
-/*
-    N이 짝수일 경우, N/2 위치에서 순간이동 하면 N에 도달할 수 있음
-N이 홀수일 경우, (N-1)/2 위치에서 순간이동한 후 1칸 점프하거나, (N+1)/2 위치에서 순간이동 후 1칸 뒤로 가야 함 (뒤로 가는 건 불가능하므로 앞으로 1칸 점프)    
-*/
-    var n = n
-    var jump = 0
-    while n > 0 {
-        if n % 2 == 0 {
-            n /= 2  // 순간이동
-        } else {
-            n -= 1; jump += 1   // 점프
-        }
-    }
-    return jump
+    if n == 1 { return 1 }
+    if n % 2 == 0 { return solution(n / 2) }
+    return solution(n / 2) + 1
 }
