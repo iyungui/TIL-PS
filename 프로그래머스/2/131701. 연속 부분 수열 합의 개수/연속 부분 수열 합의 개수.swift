ddf32a6869ffe370267285d17028ad6880620854
Yung-ui <121233350+iyungui@users.noreply.github.com>
2025-04-23 01:51:27 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 148.29 ms, Memory: 27.3 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.swift
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.swift
@@ -1,20 +1,23 @@
-func solution(_ elements: [Int]) -> Int {
+import Foundation
+
+func solution(_ elements:[Int]) -> Int {
     let n = elements.count
-    var sums = Set<Int>()
+    var psum = Array(repeating: 0, count: n * 2 + 1)
+    var res = Set<Int>()
     
-    // 원형 수열을 처리하기 위해 배열을 두 번 연결
-    let circular = elements + elements
+    // make prefix sum
+    for i in 0..<2*n {
+        // psum[0] = 0, psum[1] = elements[0], psum[2] = psum[1] + elements[1]..
+        psum[i + 1] = psum[i] + elements[i % n]
+    }
     
-    // 길이가 1부터 n까지인 모든 연속 부분 수열의 합을 구함
+    // calculate prefix sum
     for length in 1...n {
         for start in 0..<n {
-            var sum = 0
-            for i in 0..<length {
-                sum += circular[start + i]
-            }
-            sums.insert(sum)
+            let sum = psum[start + length] - psum[start]
+            res.insert(sum)
         }
     }
     
-    return sums.count
+    return res.count
 }
