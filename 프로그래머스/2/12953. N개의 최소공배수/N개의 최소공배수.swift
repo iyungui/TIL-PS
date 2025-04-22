979fa93f2c2ddf09d0c5e10ed884eff5235b2819
Yung-ui <121233350+iyungui@users.noreply.github.com>
2025-04-22 21:10:46 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.10 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.swift
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.swift
@@ -1,15 +1,19 @@
 func solution(_ arr:[Int]) -> Int {
-    arr.reduce(1) { result, num in
-        if result == 1 { return num }    // 첫번째 원소인 경우
-        return lcm(result, num)
+    var temp = arr[0]
+    
+    for num in arr {
+        print(num, temp)
+        temp = lcm(temp, num)
     }
+    
+    return temp
+}
+
+func lcm(_ a: Int, _ b: Int) -> Int {
+    (a * b) / gcd(a, b)
 }
 
 func gcd(_ a: Int, _ b: Int) -> Int {
     if b == 0 { return a }
     return gcd(b, a % b)
-}
-
-func lcm(_ a: Int, _ b: Int) -> Int {
-    return (a * b) / gcd(a, b)
 }
