7df8de479287b76db0a27cd516e3f5a1f4af1ca2
Yung-ui <121233350+iyungui@users.noreply.github.com>
2025-04-23 22:59:01 +0900

    [level 2] Title: 할인 행사, Time: 473.38 ms, Memory: 21.4 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.swift
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.swift
@@ -1,30 +1,34 @@
 import Foundation
 
 func solution(_ want:[String], _ number:[Int], _ discount:[String]) -> Int {
-    var dict: [String: Int] = [:]
-    for (value, count) in zip(want, number) {
-        dict[value] = count
+    // zip을 사용하여 want와 number를 딕셔너리 wantDict에 저장
+    var wantDict: [String: Int] = [:]
+    for (value, count) in zip (want, number) {
+        wantDict[value] = count
     }
     
     var res = 0
     
-    for i in 0...(discount.count - 10) {
-        
+    // discounts 목록에서, 가능한 모든 10일 기간 (윈도우)에 대해 반복.
+    for start in 0...discount.count - 10 {
+    
+        // 각 윈도우 마다, window 딕셔너리 새로 생성
         var windowDict: [String: Int] = [:]
         
-        for j in i..<(i + 10) {
-            windowDict[discount[j], default: 0] += 1
+        // 해당 start 로부터 10일 동안 discount의 제품과 수량을 window 딕셔너리에 기록
+        for i in start..<(start + 10) {
+            windowDict[discount[i], default: 0] += 1
         }
         
-        var isMatch: Bool = true
-        
-        for (value, count) in dict {
+        var isMatch = true
+        // 원하는 모든 제품이 충분한 수량으로 할인 되는지 확인
+        for (value, count) in wantDict {
             if windowDict[value, default: 0] < count {
                 isMatch = false
                 break
             }
         }
-        
+        // 모든 제품이 충분히 할인된다면 res 1 증가
         if isMatch { res += 1 }
     }
     return res
