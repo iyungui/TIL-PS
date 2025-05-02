7664a38393b63313269f5052bfcbbbb37cd6f3c8
Yung-ui <121233350+iyungui@users.noreply.github.com>
2025-05-02 18:43:11 +0900

    [level 2] Title: 기능개발, Time: 0.04 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.swift
+++ 프로그래머스/2/42586. 기능개발/기능개발.swift
@@ -1,34 +1,37 @@
 import Foundation
 
-func solution(_ progresses:[Int], _ speeds:[Int]) -> [Int] {
-    var dates: [Int] = []
-    for (pro, s) in zip(progresses, speeds) {
-        var pro = pro, cnt = 0
-        while pro < 100 {
-            pro += s
-            cnt += 1
-        }
-        dates.append(cnt)
+func solution(_ progresses: [Int], _ speeds: [Int]) -> [Int] {
+    // 각 작업이 완료되는데 필요한 일수 계산
+    var daysRequired: [Int] = []
+    
+    for i in 0..<progresses.count {
+        let remainingProgress = 100 - progresses[i]
+        // 나누어 떨어지지 않으면 하루 더 필요 (올림 효과)
+        let days = remainingProgress / speeds[i] + (remainingProgress % speeds[i] > 0 ? 1 : 0)
+        daysRequired.append(days)
     }
-    print("dates: \(dates)")
-    var stk: [Int] = []
-    var res: [Int] = []
-    for i in dates {
-        if stk.isEmpty { stk.append(i) }
-        else {
-            if stk.first! < i {
-                print("\(stk.first)가 \(i)보다 더 작은 경우:", terminator: "")
-                res.append(stk.count)
-                stk = []
-                stk.append(i)
-                print(stk)
-            } else if stk.first! >= i {
-                print("\(stk.first)가 \(i)보다 더 크거나 같은 경우:", terminator: "")
-                stk.append(i)
-                print(stk)
-            }
+    
+    var result: [Int] = []
+    var index = 0
+    
+    // 모든 작업을 처리할 때까지 반복
+    while index < daysRequired.count {
+        let currentDay = daysRequired[index]
+        var count = 1
+        
+        // 현재 작업 이후의 작업들 중 같이 배포될 수 있는 작업 카운트
+        var nextIndex = index + 1
+        while nextIndex < daysRequired.count && daysRequired[nextIndex] <= currentDay {
+            count += 1
+            nextIndex += 1
         }
+        
+        // 결과 배열에 현재 배포에 포함된 기능 수 추가
+        result.append(count)
+        
+        // 인덱스 업데이트
+        index = nextIndex
     }
-    if !stk.isEmpty { res.append(stk.count) }
-    return res
+    
+    return result
 }
