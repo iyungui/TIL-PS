5cb1a20c4d9c11b827adf70a164a7f6d4b6d0aed
Yung-ui <121233350+iyungui@users.noreply.github.com>
2025-05-07 17:51:40 +0900

    [level 2] Title: 튜플, Time: 284.82 ms, Memory: 20.8 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.swift
+++ 프로그래머스/2/64065. 튜플/튜플.swift
@@ -1,18 +1,11 @@
 import Foundation
 
-func solution(_ s:String) -> [Int] {
-    let a = s.components(separatedBy: "}").filter { !$0.isEmpty }
-    var dict: [Int : Int] = [:]
-    for c in a {
-        var dropedStr = c.dropFirst(2).components(separatedBy: ",")
-        for ch in dropedStr {
-            dict[Int(ch)!, default: 0] += 1
-        }
+func solution(_ s: String) -> [Int] {
+    var components = [Int: Int]()
+    
+    s.split { !$0.isNumber }.map { Int(String($0))! }.forEach {
+        components[$0, default: 0] += 1
     }
-    let sortedDict = dict.sorted{ $0.value > $1.value }
-    var res: [Int] = []
-    for (key, value) in sortedDict {
-        res.append(key)
-    }
-    return res
+    
+    return components.sorted { $0.value > $1.value }.map { $0.key }
 }
