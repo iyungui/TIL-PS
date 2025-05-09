8abe3eea1cb8cee1fc7244021babea3f9aac1617
Yung-ui <121233350+iyungui@users.noreply.github.com>
2025-05-09 23:13:54 +0900

    [Silver III] Title: 수열, Time: 140 ms, Memory: 85252 KB -BaekjoonHub

---
+++ 백준/Silver/2559. 수열/수열.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+let a = readLine()!.components(separatedBy: " ").map { Int($0)! }
+
+var n: Int = a[0], k: Int = a[1]
+
+let arr = readLine()!.components(separatedBy: " ").map { Int($0)! }
+
+var psum = Array(repeating: 0, count: 100001)
+
+for i in 1...n {
+    psum[i] = psum[i - 1] + arr[i - 1]
+}
+
+var res = psum[k] - psum[0]
+
+for i in k...n {
+    res = max(res, psum[i] - psum[i - k])
+}
+
+print(res)
