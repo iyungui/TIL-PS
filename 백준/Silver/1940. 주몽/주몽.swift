1ad8e48a63f5225fd84283cd67d39bb5d08d29fc
Yung-ui <121233350+iyungui@users.noreply.github.com>
2025-05-18 17:17:38 +0900

    [Silver IV] Title: 주몽, Time: 32 ms, Memory: 80052 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.swift
+++ 백준/Silver/1940. 주몽/주몽.swift
@@ -1,21 +1,13 @@
 import Foundation
-
 let n = Int(readLine()!)!
 let m = Int(readLine()!)!
-var nums = readLine()!.components(separatedBy: " ").map { Int($0)! }
-print(solve())
-
-
-func solve() -> Int {
-    if m > 100000 { return 0 }
-    nums.sort()
-    var cnt = 0
-    var (l, r) = (0, n - 1)
-    while l < r {
-        let sum = nums[l] + nums[r]
-        if sum == m { cnt += 1 }
-        if sum <= m { l += 1 }
-        else { r -= 1 }
-    }
-    return cnt
+let nums = readLine()!.components(separatedBy: " ").map { Int($0)! }.sorted()
+var cnt = 0
+var (l, r) = (0, n - 1)
+while l < r {
+    let sum = nums[l] + nums[r]
+    if sum == m { cnt += 1 }
+    if sum <= m { l += 1 }
+    else { r -= 1 }
 }
+print(cnt)
