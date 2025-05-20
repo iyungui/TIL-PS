0f8254d78d317d4549fdf1702d9c2e17e849e3cf
Yung-ui <121233350+iyungui@users.noreply.github.com>
2025-05-20 15:10:40 +0900

    [level 2] Title: 할인 행사, Time: 451.20 ms, Memory: 21.2 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.swift
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.swift
@@ -1,24 +1,24 @@
 import Foundation
 
 func solution(_ want:[String], _ number:[Int], _ discount:[String]) -> Int {
-    var wantDict: [String: Int] = [:]
-    for (value, count) in zip(want, number) {
-        wantDict[value] = count
+    var dict: [String: Int] = [:]
+    for (item, num) in zip(want, number) {
+        dict[item, default: 0] = num
     }
-    var res = 0
+    var ret = 0
     for start in 0...discount.count - 10 {
         var window: [String: Int] = [:]
         for i in start..<(start + 10) {
             window[discount[i], default: 0] += 1
         }
         var flag = true
-        for (value, count) in wantDict {
+        for (value, count) in dict {
             if window[value, default: 0] < count {
                 flag = false
                 break
             }
         }
-        if flag { res += 1 }
-    }
-    return res
+        if flag { ret += 1 }
+    } 
+    return ret
 }
