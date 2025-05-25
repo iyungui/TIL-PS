761c3dc488be835bd839003f1f24e4603e2f8416
Yung-ui <121233350+iyungui@users.noreply.github.com>
2025-05-25 14:14:01 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 12 ms, Memory: 79512 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.swift
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.swift
@@ -1,39 +1,42 @@
 import Foundation
 
-let N = Int(readLine()!)!
+let n = Int(readLine()!)!
 let nums = readLine()!.components(separatedBy: " ").map { Int($0)! }
-var oper = readLine()!.components(separatedBy: " ").map { Int($0)! }
-var minResult = Int.max
-var maxResult = Int.min
 
-func dfs(idx: Int, ret: Int) {
-    if idx == N {
-        minResult = min(minResult, ret)
-        maxResult = max(maxResult, ret)
+// +, -, *, /
+var oper = readLine()!.components(separatedBy: " ").map { Int($0)! }
+
+
+var minR = Int.max, maxR = Int.min
+
+func go(idx: Int, ret: Int) {
+    if idx == n {
+        minR = min(minR, ret)
+        maxR = max(maxR, ret)
         return
     }
     
     for i in 0..<4 {
         if oper[i] > 0 {
-            oper[i] -= 1    // 연산자 사용
+            oper[i] -= 1
             var nextRet = ret
             
             switch i {
             case 0: nextRet += nums[idx]
             case 1: nextRet -= nums[idx]
             case 2: nextRet *= nums[idx]
-            case 3: nextRet = nextRet < 0 && nums[idx] >= 0 ?  -(-nextRet / nums[idx]) : nextRet / nums[idx]
+            case 3: nextRet = nextRet < 0 && nums[idx] >= 0 ? -(-nextRet / nums[idx]) : nextRet / nums[idx]
             default: break
             }
             
-            dfs(idx: idx + 1, ret: nextRet)
+            go(idx: idx + 1, ret: nextRet)
             
             oper[i] += 1
         }
     }
-    return
 }
 
-dfs(idx: 1, ret: nums[0])
-print(maxResult)
-print(minResult)
+go(idx: 1, ret: nums[0])
+
+print(maxR)
+print(minR)
