305df4790511891951e82551c55087a07d3653b2
Yung-ui <121233350+iyungui@users.noreply.github.com>
2025-05-25 15:51:54 +0900

    [Silver III] Title: 퇴사, Time: 8 ms, Memory: 79508 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.swift
+++ 백준/Silver/14501. 퇴사/퇴사.swift
@@ -1,29 +1,28 @@
 import Foundation
 
 let n = Int(readLine()!)!
-
-// tuple
-var arr: [(time: Int, price: Int)] = []
+var arr = [(time: Int, price: Int)]()
 
 for _ in 0..<n {
-    let line = readLine()!.components(separatedBy: " ").map { Int($0)! }
-    arr.append((line[0], line[1]))
-}
-
-func solve(_ day: Int) -> Int {
-    // 기저 사례: day가 n을 넘어가는 경우, 0
-    if day >= n { return 0 }
-    
-    // 현재 상담을 하지 않는 경우(다음 날 상담)
-    let a = solve(day + 1)
-    
-    // 현재 day에 상담하는 경우
-    var b = 0
-    if day + arr[day].time <= n {   // n일까지는 회사에 있으므로 n 포함
-        b = arr[day].price + solve(day + arr[day].time)
-    }
-    
-    return max(a, b)
+    let a = readLine()!.components(separatedBy: " ").map { Int($0)! }
+    arr.append((time: a[0], price: a[1]))
 }
 
 print(solve(0))
+
+func solve(_ day: Int) -> Int {
+    // base case: 퇴사일
+    if day >= n { return 0 }
+    
+    // 상담을 하지 않는 경우 (다음 날로 이동)
+    let skip: Int = solve(day + 1)
+    
+    // 상담을 하는 경우
+    var take: Int = 0
+    let endDate = day + arr[day].time
+    if endDate <= n {
+        take = arr[day].price + solve(endDate)
+    }
+    
+    return max(skip, take)
+}
