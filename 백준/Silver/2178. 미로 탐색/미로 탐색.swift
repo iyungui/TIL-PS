06f00a2aa326cb5049e7abeb479360fb8b71f0e0
iyungui <121233350+iyungui@users.noreply.github.com>
2025-07-28 14:10:56 +0900

    [Silver I] Title: 미로 탐색, Time: 12 ms, Memory: 80040 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.swift
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.swift
@@ -10,25 +10,23 @@ for _ in 0..<n {
 
 let dy = [-1, 0, 1, 0]
 let dx = [0, 1, 0, -1]
-var visited = Array(repeating: Array(repeating: false, count: m), count: n)
-var y = 0, x = 0
+var visited = Array(repeating: Array(repeating: 0, count: m), count: n)
 
-// (y, x, distance)
-var queue = [(Int, Int, Int)]()
+// (y, x)
+var queue = [(Int, Int)]()
 
-// 시작점 (0, 0)에서 거리는 1로 시작
-queue.append((0, 0, 1))
-visited[0][0] = true
+// 시작점 (0, 0)
+queue.append((0, 0))
+visited[0][0] = 1
 
 var front = 0
 
 while front < queue.count {
-    let (y, x, dist) = queue[front]
+    let (y, x) = queue[front]
     front += 1
 
     // 도착점에 도달하면 break
     if y == n - 1 && x == m - 1 {
-        print(dist)
         break
     }
 
@@ -37,9 +35,11 @@ while front < queue.count {
         let nx = x + dx[i]
 
         if ny < 0 || ny >= n || nx < 0 || nx >= m || a[ny][nx] == 0 { continue }
-        if visited[ny][nx] { continue }
+        if visited[ny][nx] != 0 { continue }
 
-        visited[ny][nx] = true
-        queue.append((ny, nx, dist + 1))
+        visited[ny][nx] = visited[y][x] + 1
+        queue.append((ny, nx))
     }
 }
+
+print(visited[n - 1][m - 1])
