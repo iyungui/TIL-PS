c568f811a46fe5f978358f1efe629fc56b90c4e4
iyungui <121233350+iyungui@users.noreply.github.com>
2025-09-15 11:21:10 +0900

    [Gold IV] Title: 주난의 난(難), Time: 20 ms, Memory: 81796 KB -BaekjoonHub

---
+++ 백준/Gold/14497. 주난의 난（難）/주난의 난（難）.swift
@@ -0,0 +1,65 @@
+import Foundation
+
+func solve() {
+    let dy = [-1, 0, 1, 0]
+    let dx = [0, 1, 0, -1]
+
+    // 맵의 크기
+    let input = readLine()!.split(separator: " ").map { Int($0)! }
+    let (n, m) = (input[0], input[1])
+    // 주난이의 위치(y1, x1)와 범인위치(y2, x2) 그리고 0-based Indexing
+    let input2 = readLine()!.split(separator: " ").map { Int($0)! }
+    let (y1, x1, y2, x2) = (input2[0] - 1, input2[1] - 1, input2[2] - 1, input2[3] - 1)
+    
+    // 맵
+    var arr: [[Character]] = []
+    for _ in 0..<n {
+        let line = Array(readLine()!)
+        arr.append(line)
+    }
+
+    // queue   (1차원 배열로 만들기)
+    var queue: [Int] = [1000 * y1 + x1]
+    var queueIndex = 0
+    var visited = Array(repeating: Array(repeating: 0, count: m), count: n)
+    // 주난이 처음 위치 방문 처리 
+    visited[y1][x1] = 1
+
+    // 점프 횟수
+    var jump = 0
+
+    while arr[y2][x2] != "0" {
+        jump += 1
+        var temp: [Int] = []
+        
+        while queueIndex < queue.count {
+            let cur = queue[queueIndex]
+            let y = cur / 1000
+            let x = cur % 1000
+
+            queueIndex += 1
+
+            for i in 0..<4 {
+                let ny = y + dy[i]
+                let nx = x + dx[i]
+
+                if ny < 0 || ny >= n || nx < 0 || nx >= m || visited[ny][nx] != 0 { continue }
+                visited[ny][nx] = jump
+
+                if arr[ny][nx] != "0" {
+                    arr[ny][nx] = "0"
+                    temp.append(1000 * ny + nx)
+                }
+                else {
+                    queue.append(1000 * ny + nx)
+                }
+            }
+        }
+        queue = temp
+        queueIndex = 0
+    }
+
+    print(visited[y2][x2])
+}
+
+solve()
