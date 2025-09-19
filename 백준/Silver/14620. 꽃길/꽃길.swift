2bde376334d1c2bef16213d761bbcaf59b799184
iyungui <121233350+iyungui@users.noreply.github.com>
2025-09-19 20:40:21 +0900

    [Silver II] Title: 꽃길, Time: 36 ms, Memory: 79512 KB -BaekjoonHub

--- 백준/Silver/14620. 꽃길/꽃길.swift
+++ 백준/Silver/14620. 꽃길/꽃길.swift
@@ -1,53 +1,79 @@
 import Foundation
 
 func main() {
-    let n = Int(readLine()!)!
-    var price = [[Int]]()
-    for _ in 0..<n {
-        let input = readLine()!.split(separator: " ").map { Int($0)! }
-        price.append(input)
-    }
+    let (n, price) = input()
+    let dy = [-1, 0, 1, 0]
+    let dx = [0, 1, 0, -1]
+
     var flowers = Array(repeating: Array(repeating: false, count: n), count: n)
 
     var ret = Int.max
-    func go(_ cnt: Int) {
+
+    func input() -> (Int, [[Int]]) {
+        let n = Int(readLine()!)!
+        var price = [[Int]]()
+
+        for _ in 0..<n {
+            let line = readLine()!.components(separatedBy: " ").map { Int($0)! }
+            price.append(line)
+        }
+
+        return (n, price)
+    }
+
+    func check(_ y: Int, _ x: Int) -> Bool {
+        if flowers[y][x] { return false }
+        for i in 0..<4 {
+            let ny = y + dy[i]
+            let nx = x + dx[i]
+            if ny < 0 || ny >= n || nx < 0 || nx >= n { return false }
+            if flowers[ny][nx] { return false }
+        }
+        return true 
+    }
+
+    func setFlower(_ y: Int, _ x: Int) -> Int {
+        var cost = price[y][x]
+        flowers[y][x] = true
+
+        for i in 0..<4 {
+            let ny = y + dy[i]
+            let nx = x + dx[i]
+            flowers[ny][nx] = true
+            cost += price[ny][nx]
+        }
+
+        return cost
+    }
+
+    func eraseFlower(_ y: Int, _ x: Int) {
+        flowers[y][x] = false
+
+        for i in 0..<4 {
+            let ny = y + dy[i]
+            let nx = x + dx[i]
+            flowers[ny][nx] = false
+        }
+    }
+
+    func go(_ cnt: Int, _ cost: Int) {
         if cnt == 3 {
-            ret = min(ret, calc(flowers))
+            ret = min(ret, cost)
             return
         }
 
         for i in 1..<n - 1 {
             for j in 1..<n - 1 {
-                if flowers[i][j] || flowers[i - 1][j] || flowers[i + 1][j] || flowers[i][j - 1] || flowers[i][j + 1] {
-                    continue
+                if check(i, j) {
+                    go(cnt + 1, cost + setFlower(i, j))
+                    eraseFlower(i, j)
                 }
-                flowers[i][j] = true
-                flowers[i - 1][j] = true
-                flowers[i + 1][j] = true
-                flowers[i][j - 1] = true
-                flowers[i][j + 1] = true
-                go(cnt + 1)
-                flowers[i][j] = false
-                flowers[i - 1][j] = false
-                flowers[i + 1][j] = false
-                flowers[i][j - 1] = false
-                flowers[i][j + 1] = false
             }
         }
     }
 
-    func calc(_ flowers: [[Bool]]) -> Int {
-        var sum = 0
-        for i in 0..<n {
-            for j in 0..<n {
-                if flowers[i][j] {
-                    sum += price[i][j]
-                }
-            }
-        }
-        return sum
-    }
-    go(0)
+    go(0, 0)
+
     print(ret)
 }
 
