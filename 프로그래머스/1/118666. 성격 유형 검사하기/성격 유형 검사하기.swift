88dcf6087291d4ecb12b35d4af5ecae01d65e2ee
iyungui <121233350+iyungui@users.noreply.github.com>
2025-09-21 18:26:58 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.83 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.swift
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.swift
@@ -1,26 +1,27 @@
 import Foundation
 
 func solution(_ survey:[String], _ choices:[Int]) -> String {
-    var result = ""
-    // 각 성격유형의 점수를 0으로 초기화
     var score: [Character: Int] = [
         "R": 0, "T": 0,
         "C": 0, "F": 0,
         "J": 0, "M": 0,
         "A": 0, "N": 0
     ]
-    for (sur, choice) in zip(survey, choices) {
+    
+    for (choice, sur) in zip(choices, survey) {
         if choice == 4 { continue }
-        
         if choice < 4 {
-            score[sur.first!]! += 4 - choice
+            score[sur.first!]! += (4 - choice)
         } else {
-            score[sur.last!]! += choice - 4
+            score[sur.last!]! += (choice - 4)
         }
     }
-    result += score["R"]! >= score["T"]! ? "R" : "T"
-    result += score["C"]! >= score["F"]! ? "C" : "F"
-    result += score["J"]! >= score["M"]! ? "J" : "M"
-    result += score["A"]! >= score["N"]! ? "A" : "N"
-    return result
+    var ret = ""
+    
+    ret += score["R"]! >= score["T"]! ? "R" : "T"
+    ret += score["C"]! >= score["F"]! ? "C" : "F"
+    ret += score["J"]! >= score["M"]! ? "J" : "M"
+    ret += score["A"]! >= score["N"]! ? "A" : "N"
+    
+    return ret
 }
