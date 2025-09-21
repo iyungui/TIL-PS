ce7edd080ab5d5419e40da562f93ad7f1f3d3bd4
iyungui <121233350+iyungui@users.noreply.github.com>
2025-09-21 11:12:59 +0900

    [level 3] Title: 코딩 테스트 공부, Time: 190.85 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/3/118668. 코딩 테스트 공부/코딩 테스트 공부.swift
+++ 프로그래머스/3/118668. 코딩 테스트 공부/코딩 테스트 공부.swift
@@ -1,61 +1,32 @@
 import Foundation
 
-// 초기 alp, cop 에서 목표 alp, cop까지 가는 최단거리를 구하는 문제
-// dp 문제
-func solution(_ alp: Int, _ cop: Int, _ problems: [[Int]]) -> Int {
-    // 목표 알고력과 코딩력을 구함 (모든 문제의 요구사항 중 최대값)
+// alp, cop 에서 maxAlp, maxCop 로 가는 최단 시간 구하는 문제.
+// dp로 풀 수 있음
+func solution(_ alp:Int, _ cop:Int, _ problems:[[Int]]) -> Int {
     var maxAlp = alp
     var maxCop = cop
-
-    for problem in problems {
-        let alpReq = problem[0]
-        let copReq = problem[1]
-
-        maxAlp = max(maxAlp, alpReq)
-        maxCop = max(maxCop, copReq)
+    problems.forEach {
+        maxAlp = max($0[0], maxAlp)
+        maxCop = max($0[1], maxCop)
     }
-
-    // DP 배열 초기화
-    // dp[i][j] = (알고력 i, 코딩력 j) 상태에 도달하는데 필요한 최단 시간
     var dp = Array(repeating: Array(repeating: 10000, count: maxCop + 1), count: maxAlp + 1)
-
-    // 기저 사례: 초기상태는 시간 0
     dp[alp][cop] = 0
-
-    // DP 배열 업데이트
-    for i in alp...maxAlp {
-        for j in cop...maxCop {
-            if dp[i][j] == Int.max { continue }
-
-            // 1. 알고력을 공부하는 경우 (알고력 1 증가, 시간 1 소요)
-            if i + 1 <= maxAlp {
-                dp[i + 1][j] = min(dp[i + 1][j], dp[i][j] + 1)
-            }
-
-            // 2. 코딩력을 공부하는 경우 (코딩력 1 증가, 시간 1 소요)
-            if j + 1 <= maxCop {
-                dp[i][j + 1] = min(dp[i][j + 1], dp[i][j] + 1)
-            }
-
-            // 3. 문제 풀기
-            for problem in problems {
-                let alpReq = problem[0]
-                let copReq = problem[1]
-                let alpRwd = problem[2]
-                let copRwd = problem[3]
-                let cost = problem[4]
-
-                // 현재 상태(i, j)에서 이 문제를 풀 수 있는지 확인
-                if i >= alpReq && j >= copReq {
-                    // 목표를 넘지 않도록 제한
-                    let nextAlp = min(i + alpRwd, maxAlp)
-                    let nextCop = min(j + copRwd, maxCop)
-
-                    dp[nextAlp][nextCop] = min(dp[nextAlp][nextCop], dp[i][j] + cost)
+    for curAlp in alp...maxAlp {
+        for curCop in cop...maxCop {
+            var minAlp = min(maxAlp, curAlp + 1)
+            var minCop = min(maxCop, curCop + 1)
+            
+            dp[minAlp][curCop] = min(dp[minAlp][curCop], dp[curAlp][curCop] + 1)
+            dp[curAlp][minCop] = min(dp[curAlp][minCop], dp[curAlp][curCop] + 1)
+            
+            problems.forEach {
+                minAlp = min(maxAlp, curAlp + $0[2])
+                minCop = min(maxCop, curCop + $0[3])
+                if curAlp >= $0[0] && curCop >= $0[1] {
+                    dp[minAlp][minCop] = min(dp[minAlp][minCop], dp[curAlp][curCop] + $0[4])
                 }
             }
         }
     }
-
     return dp[maxAlp][maxCop]
 }
