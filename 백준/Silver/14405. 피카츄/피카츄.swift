31e8b775ee52c97a592c30b6b7d1dfa429ded48b
iyungui <121233350+iyungui@users.noreply.github.com>
2025-09-23 13:07:19 +0900

    [Silver V] Title: 피카츄, Time: 8 ms, Memory: 79504 KB -BaekjoonHub

--- 백준/Silver/14405. 피카츄/피카츄.swift
+++ 백준/Silver/14405. 피카츄/피카츄.swift
@@ -1,36 +1,19 @@
 
 import Foundation
 
-let s = readLine()!
+let s = Array(readLine()!)
 var flag = false
 
 var i = 0
-
-
 while i < s.count {
-    let startIndex = s.index(s.startIndex, offsetBy: i)
-    
-    if i < s.count - 1 {
-        let endIndex = s.index(startIndex, offsetBy: 2)
-        let substr = String(s[startIndex..<endIndex])
-        if substr == "pi" || substr == "ka" {
-            i += 2
-            continue
-        }
+    if i <= s.count - 2 && (String(s[i..<i+2]) == "pi" || String(s[i..<i+2]) == "ka") {
+        i += 2
+    } else if i <= s.count - 3 && String(s[i..<i+3]) == "chu" {
+        i += 3
+    } else {
+        flag = true
+        break
     }
-    
-    if i < s.count - 2 {
-        let endIndex = s.index(startIndex, offsetBy: 3)
-        let substr = String(s[startIndex..<endIndex])
-        if substr == "chu" {
-            i += 3
-            continue
-        }
-    }
-    
-    flag = true
-    break
 }
 
-if flag { print("NO") }
-else { print("YES") }
+print(flag ? "NO" : "YES")
