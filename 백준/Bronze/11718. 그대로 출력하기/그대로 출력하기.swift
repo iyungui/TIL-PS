e961b18fb9bbd5ee4dac6b0fd96401293126f085
iyungui <121233350+iyungui@users.noreply.github.com>
2025-09-24 20:38:42 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 8 ms, Memory: 69092 KB -BaekjoonHub

--- 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.swift
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.swift
@@ -1,10 +1,3 @@
-import Foundation
-
-var a: String?
-a = readLine()
-if let a = a { print(a) }
-
-while a != nil {
-    a = readLine()
-    if let a = a { print(a) }
+while let a = readLine() {
+    print(a)
 }
