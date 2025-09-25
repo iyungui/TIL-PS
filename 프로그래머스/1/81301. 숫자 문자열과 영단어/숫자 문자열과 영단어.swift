1e1e7eac959245991dc36263d0e9923542a83d33
iyungui <121233350+iyungui@users.noreply.github.com>
2025-09-26 00:59:46 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.22 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.swift
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.swift
@@ -1,22 +1,11 @@
 import Foundation
 
 func solution(_ s:String) -> Int {
-    let numbers: [(str: String, num: String)] = [
-        ("zero", "0"),
-        ("one", "1"),
-        ("two", "2"),
-        ("three", "3"),
-        ("four", "4"),
-        ("five", "5"),
-        ("six", "6"),
-        ("seven", "7"),
-        ("eight", "8"),
-        ("nine", "9")
-    ]
+    let arr = ["zero","one","two","three","four","five","six","seven","eight","nine"]
     
     var ret = s
-    for n in numbers {
-        ret = ret.replacingOccurrences(of: n.str, with: n.num)
+    for i in 0..<arr.count {
+        ret = ret.replacingOccurrences(of: arr[i], with: String(i))
     }
     
     return Int(ret)!
