f9f988606ffe212b74e4b6ff500acefb87449d3d
iyungui <121233350+iyungui@users.noreply.github.com>
2025-10-01 23:38:04 +0900

    [level 2] Title: [카카오 인턴] 수식 최대화, Time: 0.71 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.swift
+++ 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.swift
@@ -1,80 +1,51 @@
 import Foundation
 
-func permute<T>(_ arr: [T]) -> [[T]] {
-    var ret: [[T]] = []
-    var cur: [T] = []
-    var used = Array(repeating: false, count: arr.count)
-    
-    func backtrack() {
-        if cur.count == arr.count {
-            ret.append(cur)
-            return
-        }
-        for i in 0..<arr.count {
-            if used[i] { continue }
-            used[i] = true
-            cur.append(arr[i])
-            backtrack()
-            cur.removeLast()
-            used[i] = false
+enum Operation: Character {
+    case multiple = "*"
+    case plus = "+"
+    case minus = "-"
+
+    func cal(_ a: Int64, _ b: Int64) -> Int64 {
+        switch self {
+            case .multiple: return a * b
+            case .plus: return a + b
+            case .minus: return a - b
+            default: return -1
         }
     }
-    
-    backtrack()
-    return ret
 }
 
-func solution(_ expression:String) -> Int64 {
-    var nums: [Int] = []
-    var ops:[Character] = []
-    var num = ""
-    
-    for ch in expression {
-        if ch.isNumber {
-            num += String(ch)
-        } else {
-            nums.append(Int(num)!)
-            ops.append(ch)
-            num = ""
-        }
-    }
-    nums.append(Int(num)!)
-    
-    let priority = permute(Array(Set(ops)))
-    
-    var ret = 0
+func go(_ numbers: [Int64], _ opers: [Character], prior: [Operation]) -> Int64 {
+    var nums = numbers, opers = opers
 
-    func calc(_ nums: [Int], _ opers: [Character], _ priority: [Character]) -> Int {
-        var nums = nums, ops = opers
-        
-        for prior in priority {
-            var i = 0
-            while i < ops.count {
-                if ops[i] == prior {
-                    nums[i] = go(prior, i, nums)
-                    nums.remove(at: i + 1)
-                    ops.remove(at: i)
-                } else {
-                    i += 1
-                }
-            }
-        }
-        return abs(nums[0])
+    for p in prior {
+        while let idx: Array<Character>.Index = opers.firstIndex(of: p.rawValue) {
+            nums[idx] = p.cal(nums[idx], nums[idx + 1])
+            nums.remove(at: idx + 1)
+            opers.remove(at: idx)
+        } 
     }
-    
-    func go(_ prior: Character, _ i: Int, _ nums: [Int]) -> Int {
-        switch prior {
-            case "*": return nums[i] * nums[i + 1]
-            case "+": return nums[i] + nums[i + 1]
-            case "-": return nums[i] - nums[i + 1]
-            default: return 0
+    return abs(nums[0])
+}
+
+func solution(_ expression: String) -> Int64 {
+    let numbers = expression.components(separatedBy: ["*", "+", "-"]).map { Int64($0)! }
+    let opers = (expression.filter { !$0.isNumber }).map { Character(String($0)) }
+    let myOper = "*-+"
+    var ret: Int64 = 0
+
+    for first in myOper {
+        for second in myOper {
+            guard first != second else { continue }
+            let third = myOper.filter { $0 != first && $0 != second }[0]
+            let cur = go(numbers, opers, prior: [
+                Operation(rawValue: first)!,
+                Operation(rawValue: second)!,
+                Operation(rawValue: third)!,
+            ])
+            ret = max(cur, ret)
         }
     }
 
-    for prior in priority {
-        let cur = calc(nums, ops, prior)
-        ret = max(cur, ret)
-    }
-    
-    return Int64(ret)
+    return ret
 }
