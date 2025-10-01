015fc2685c2fc8c9be20f446a0d87d0bdbb5033d
iyungui <121233350+iyungui@users.noreply.github.com>
2025-10-01 22:18:05 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 1.03 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.swift
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.swift
@@ -1,55 +1,44 @@
 import Foundation
 
 func solution(_ numbers: [Int], _ hand: String) -> String {
-
-    func getPos(_ num: Int) -> (Int, Int) {
-        switch num {
-            case 1: return (0, 0)
-            case 2: return (0, 1)
-            case 3: return (0, 2)
-            case 4: return (1, 0)
-            case 5: return (1, 1)
-            case 6: return (1, 2)
-            case 7: return (2, 0)
-            case 8: return (2, 1)
-            case 9: return (2, 2)
-            case 0: return (3, 1)
-            default: return (0, 0)
-        }
-    }
-
-    func getDist(_ pos1: (Int, Int), _ pos2: (Int, Int)) -> Int {
-        return abs(pos1.0 - pos2.0) + abs(pos1.1 - pos2.1)
-    }
-
-    var leftPos = (3, 0)
-    var rightPos = (3, 2)
     var ret = ""
-
-    for num in numbers {
-        if num == 1 || num == 4 || num == 7 {
-            ret += "L"; leftPos = getPos(num)
-        }
-        else if num == 3 || num == 6 || num == 9 {
-            ret += "R"; rightPos = getPos(num)
-        }
-        else {
-            let targetPos = getPos(num)
-            let leftDist = getDist(leftPos, targetPos)
-            let rightDist = getDist(rightPos, targetPos)
-
-            if leftDist < rightDist {
-                ret += "L"; leftPos = targetPos
-            }
-            else if leftDist > rightDist {
-                ret += "R"; rightPos = targetPos
-            }
-            else {
-                if hand == "left" { ret += "L"; leftPos = targetPos }
-                else { ret += "R"; rightPos = targetPos }
+    var l = -1, r = -2
+    func getDist(_ cur: Int, _ target: Int) -> Int {
+        let mp = [[1,2,3],[4,5,6],[7,8,9],[-1,0,-2]]
+        var s = (0, 0), e = (0, 0)
+        for i in 0..<4 {
+            for j in 0..<3 {
+                if mp[i][j] == cur { s = (i, j) }
+                if mp[i][j] == target { e = (i, j) }
             }
         }
+        return abs(s.0 - e.0) + abs(s.1 - e.1)
     }
 
+    for n in numbers {
+        switch n {
+            case 1, 4, 7: ret += "L"; l = n
+            case 3, 6, 9: ret += "R"; r = n
+            default:
+                let distL = getDist(l, n)
+                let distR = getDist(r, n)
+
+                if distL == distR {
+                    if hand == "left" {
+                        ret += "L"
+                        l = n
+                    } else {
+                        ret += "R"
+                        r = n
+                    }
+                }
+                else if distL < distR {
+                    ret += "L"; l = n
+                }
+                else {
+                    ret += "R"; r = n
+                }
+        }
+    }
     return ret
 }
