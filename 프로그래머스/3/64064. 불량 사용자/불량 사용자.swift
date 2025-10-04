6c97599c2b23406ac17bea6e6b96a81c129ddf75
iyungui <121233350+iyungui@users.noreply.github.com>
2025-10-04 17:58:10 +0900

    [level 3] Title: 불량 사용자, Time: 481.18 ms, Memory: 16.8 MB -BaekjoonHub

---
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.swift
@@ -0,0 +1,49 @@
+import Foundation
+
+func solution(_ user_id:[String], _ banned_id:[String]) -> Int {
+    // userId 전처리
+    var dict = [String: [Int]]()
+    for (idx, user) in user_id.enumerated() {
+        let user = Array(user)
+        let n = user.count
+        for mask in 0..<(1 << n) {
+            var a = ""
+            for i in 0..<n {
+                if mask & (1 << i) != 0 {
+                    a.append(user[i])
+                } else {
+                    a += "*"
+                }
+            }
+            dict[a, default: []].append(idx)
+        }
+    }
+    var arr = [[Int]]()
+    for ban in banned_id {
+        let idxs = dict[ban]!
+        arr.append(idxs)
+    }
+    
+    func go() -> Int {
+        var s = Set<[Int]>()
+        var visited = Array(repeating: false, count: user_id.count)
+        func backtrack(_ depth: Int, _ cur: [[Int]]) {
+            if cur.count == arr.count {
+                s.insert(cur.reduce([]) { $0 + $1 }.sorted())
+                return
+            }
+            for i in 0..<arr[depth].count {
+                let idx = arr[depth][i]
+                if visited[idx] { continue }
+                visited[idx] = true
+                backtrack(depth + 1, cur + [[idx]])
+                visited[idx] = false
+            }
+        }
+        
+        backtrack(0, [])
+        return s.count
+    }
+
+    return go()
+}
