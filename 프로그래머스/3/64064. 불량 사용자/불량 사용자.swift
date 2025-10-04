a0c6b4828ecd606f721a8565fcffd730b67149f0
iyungui <121233350+iyungui@users.noreply.github.com>
2025-10-04 18:56:14 +0900

    [level 3] Title: 불량 사용자, Time: 259.40 ms, Memory: 16.8 MB -BaekjoonHub

--- 프로그래머스/3/64064. 불량 사용자/불량 사용자.swift
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.swift
@@ -1,49 +1,47 @@
 import Foundation
 
 func solution(_ user_id:[String], _ banned_id:[String]) -> Int {
-    // userId 전처리
     var dict = [String: [Int]]()
     for (idx, user) in user_id.enumerated() {
         let user = Array(user)
         let n = user.count
+        
         for mask in 0..<(1 << n) {
             var a = ""
             for i in 0..<n {
-                if mask & (1 << i) != 0 {
-                    a.append(user[i])
-                } else {
-                    a += "*"
-                }
+                if mask & (1 << i) != 0 { a.append(user[i]) }
+                else { a += "*" }
             }
             dict[a, default: []].append(idx)
         }
     }
-    var arr = [[Int]]()
+    
+    var nums = [[Int]]()
     for ban in banned_id {
-        let idxs = dict[ban]!
-        arr.append(idxs)
+        let d = dict[ban]!
+        nums.append(d)
     }
     
     func go() -> Int {
         var s = Set<[Int]>()
-        var visited = Array(repeating: false, count: user_id.count)
+        var visited = [Bool](repeating: false, count: user_id.count)
+        
         func backtrack(_ depth: Int, _ cur: [Int]) {
-            if cur.count == arr.count {
+            if depth == nums.count {
                 s.insert(cur.sorted())
                 return
             }
-            for i in 0..<arr[depth].count {
-                let idx = arr[depth][i]
+            for i in 0..<nums[depth].count {
+                let idx = nums[depth][i]
                 if visited[idx] { continue }
                 visited[idx] = true
                 backtrack(depth + 1, cur + [idx])
                 visited[idx] = false
             }
         }
-        
         backtrack(0, [])
         return s.count
     }
-
+    
     return go()
 }
