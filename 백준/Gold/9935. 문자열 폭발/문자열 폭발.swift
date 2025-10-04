e4fe1098921e1e00c7fc70c753412d8e396eb8ce
iyungui <121233350+iyungui@users.noreply.github.com>
2025-10-04 16:05:30 +0900

    [Gold IV] Title: 문자열 폭발, Time: 120 ms, Memory: 105120 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.swift
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.swift
@@ -1,28 +1,26 @@
 import Foundation
 
 func main() {
-    let str = Array(readLine()!)
+    let str = readLine()!
     let bomb = Array(readLine()!)
-    let bombLength = bomb.count
+    let cnt = bomb.count
+
     var stk = [Character]()
-    
+
     for ch in str {
         stk.append(ch)
 
-        if stk.count >= bombLength {
+        if stk.count >= cnt {
             var flag = true
-            for i in 0..<bombLength {
-                if stk[stk.count - bombLength + i] != bomb[i] {
-                    flag = false
-                    break
-                }
+
+            for i in 0..<cnt {
+                let idx = stk.count - cnt + i
+                if stk[idx] != bomb[i] { flag = false; break }
             }
-            if flag {
-                stk.removeLast(bombLength)
-            }
+
+            if flag { stk.removeLast(cnt) }
         }
     }
-
     print(stk.isEmpty ? "FRULA" : String(stk))
 }
 
