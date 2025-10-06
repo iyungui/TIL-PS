720fccc9f31e2121e991a145bcb259ab6c2f946e
iyungui <121233350+iyungui@users.noreply.github.com>
2025-10-07 01:05:13 +0900

    [level 2] Title: [1차] 캐시, Time: 302.44 ms, Memory: 21.7 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.swift
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.swift
@@ -1,25 +1,22 @@
 func solution(_ cacheSize:Int, _ cities:[String]) -> Int {
     guard cacheSize > 0 else { return cities.count * 5 }
-    var cities = cities.map { $0.lowercased() }
     var cache = [String]()
-    var totalTime = 0
-    
+    let cities = cities.map { $0.lowercased() }
+    var total = 0
     for city in cities {
-        // cache hit
         if let index = cache.firstIndex(of: city) {
             cache.remove(at: index)
             cache.append(city)
-            totalTime += 1
+            total += 1
         }
-        // cache miss
         else {
             if cache.count >= cacheSize {
-                _ = cache.removeFirst()
+                cache.removeFirst()
             }
             cache.append(city)
-            totalTime += 5
+            total += 5
         }
     }
     
-    return totalTime
+    return total
 }
