8453033ed8af07ba578f1ff3ce1790c4745477ca
iyungui <121233350+iyungui@users.noreply.github.com>
2025-10-07 03:28:59 +0900

    [Gold V] Title: 회의실 배정, Time: 136 ms, Memory: 72236 KB -BaekjoonHub

--- 백준/Gold/1931. 회의실 배정/회의실 배정.swift
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.swift
@@ -1,27 +1,10 @@
-import Foundation
-
-func main() {
-    let n = Int(readLine()!)!
-    var meetings: [(start: Int, end: Int)] = [(Int, Int)]()
-
-    for _ in 0..<n {
-        let line = readLine()!.split(separator: " ").map { Int($0)! }
-        meetings.append((start: line[0], end: line[1]))
-    }
-    
-    meetings.sort { $0.end == $1.end ? $0.start < $1.start : $0.end < $1.end }
-
-    var lastEndTime = 0
-    var cnt = 0
-
-    for meeting in meetings {
-        if meeting.start >= lastEndTime {
-            lastEndTime = meeting.end
-            cnt += 1
-        }
-    }
-
-    print(cnt)
+let n = Int(readLine()!)!
+var arr = [(Int, Int)]()
+for _ in 0..<n {
+    let a = readLine()!.split(separator: " ").map { Int($0)! }
+    arr.append((a[0], a[1]))
 }
-
-main()
+var (end, cnt) = (0, 0)
+arr.sort { $0.1 == $1.1 ? $0.0 < $1.0 : $0.1 < $1.1 }
+for m in arr where m.0 >= end { end = m.1; cnt += 1 }
+print(cnt)
