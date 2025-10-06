8a2e2584fcbbacee21b3df9607ffb8e8f5f3b14b
iyungui <121233350+iyungui@users.noreply.github.com>
2025-10-07 03:25:15 +0900

    [Gold V] Title: 회의실 배정, Time: 140 ms, Memory: 82648 KB -BaekjoonHub

--- 백준/Gold/1931. 회의실 배정/회의실 배정.swift
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.swift
@@ -2,32 +2,26 @@ import Foundation
 
 func main() {
     let n = Int(readLine()!)!
-    var meetings = [(start: Int, end: Int)]()
+    var meetings: [(start: Int, end: Int)] = [(Int, Int)]()
+
     for _ in 0..<n {
         let line = readLine()!.split(separator: " ").map { Int($0)! }
-        let start = line[0] // 회의 시작 시간
-        let end = line[1]   // 회의 종료 시간
-        meetings.append((start, end))
-    }
-
-    // 종료 시간을 기준으로 오름차순 정렬, 종료 시간이 같다면 시작 시간을 기준으로 오름차순 정렬
-    meetings.sort { (m1, m2) -> Bool in
-        if m1.end == m2.end {
-            return m1.start < m2.start
-        } else {
-            return m1.end < m2.end
-        }
+        meetings.append((start: line[0], end: line[1]))
     }
     
-    var count = 0          // 최대 회의 개수
-    var lastEndTime = 0    // 마지막으로 선택된 회의의 종료 시간
+    meetings.sort { $0.end == $1.end ? $0.start < $1.start : $0.end < $1.end }
+
+    var lastEndTime = 0
+    var cnt = 0
+
     for meeting in meetings {
         if meeting.start >= lastEndTime {
-            count += 1
             lastEndTime = meeting.end
+            cnt += 1
         }
     }
-    print(count)
+
+    print(cnt)
 }
 
 main()
