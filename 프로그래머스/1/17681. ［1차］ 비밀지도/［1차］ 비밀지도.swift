3d69eb3fefeca45f9ed2d8fedbaba76c56df405e
iyungui <121233350+iyungui@users.noreply.github.com>
2025-10-07 00:47:10 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.31 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.swift
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.swift
@@ -1,7 +1,18 @@
 func solution(_ n:Int, _ arr1:[Int], _ arr2:[Int]) -> [String] {
-    return (0..<n).map { i in
-        let binary = String(arr1[i] | arr2[i], radix: 2)
-        let padded = String(repeating: "0", count: n - binary.count) + binary
-        return padded.reduce("") { $0 + ($1 == "1" ? "#" : " ") }
+    var ret = [String]()
+    
+    for (a, b) in zip(arr1, arr2) {
+        var aa = String(a, radix: 2)
+        var bb = String(b, radix: 2)
+        while aa.count < n { aa = "0" + aa }
+        while bb.count < n { bb = "0" + bb }
+        
+        var row = ""
+        for (aaa, bbb) in zip(aa, bb) {
+            if String(aaa) + String(bbb) == "00" { row += " " }
+            else { row += "#" }
+        }
+        ret.append(row)
     }
+    return ret
 }
