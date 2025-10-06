505b24da8035397bfb08f96bb68ea767937987e5
iyungui <121233350+iyungui@users.noreply.github.com>
2025-10-06 21:16:55 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.20 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.swift
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.swift
@@ -1,12 +1,39 @@
 import Foundation
 
 func solution(_ s:String) -> Int {
-    let arr = ["zero","one","two","three","four","five","six","seven","eight","nine"]
+    let dict: [String: Int] = [
+        "zero": 0,
+        "one": 1,
+        "two": 2,
+        "three": 3,
+        "four": 4,
+        "five": 5,
+        "six": 6,
+        "seven": 7,
+        "eight": 8,
+        "nine": 9
+    ]
     
-    var ret = s
-    for i in 0..<arr.count {
-        ret = ret.replacingOccurrences(of: arr[i], with: String(i))
+    let s = Array(s)
+    var i = 0
+    var ret = [Int]()
+    
+    while i < s.count {
+        if s[i].isNumber {
+            ret.append(Int(String(s[i]))!)
+            i += 1
+            continue
+        }
+        
+        for len in [5, 4, 3] where len + i <= s.count {
+            let word = s[i..<(i + len)].reduce("") { $0 + String($1) }
+            if let num = dict[word] {
+                ret.append(num)
+                i += len
+                break
+            }
+        }
     }
     
-    return Int(ret)!
+    return Int(ret.map { String($0) }.reduce("", +))!
 }
