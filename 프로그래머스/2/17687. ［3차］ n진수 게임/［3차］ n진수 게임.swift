ec037237be091e9076496cd9fc2c231e742672f9
iyungui <121233350+iyungui@users.noreply.github.com>
2025-10-06 11:30:55 +0900

    [level 2] Title: [3차] n진수 게임, Time: 79.59 ms, Memory: 23.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.swift
@@ -0,0 +1,15 @@
+func solution(_ n:Int, _ t:Int, _ m:Int, _ p:Int) -> String {
+    var arr: [Character] = []
+    for i in 0..<(t * m) {
+        let b = String(i, radix: n)
+        for bb in b { arr.append(bb) }
+    }
+    var ret = ""
+    var i = p - 1
+    while i < arr.count {
+        ret.append(arr[i])
+        if ret.count == t { break }
+        i += m
+    }
+    return ret.uppercased()
+}
