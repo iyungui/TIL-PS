cf10b65f5db21070d9fbfc086daeb9bfdb154972
iyungui <121233350+iyungui@users.noreply.github.com>
2025-10-07 14:43:04 +0900

    [level 2] Title: 튜플, Time: 400.76 ms, Memory: 20.8 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.swift
+++ 프로그래머스/2/64065. 튜플/튜플.swift
@@ -1,28 +1,7 @@
 import Foundation
 
 func solution(_ s:String) -> [Int] {
-    let parts = s.components(separatedBy: "},")
-    var sets = [[Int]]()
-    for part in parts {
-        let numbers = part.replacingOccurrences(of: "{", with: "")
-                          .replacingOccurrences(of: "}", with: "")
-                          .split(separator: ",").compactMap { Int($0) }
-        sets.append(numbers)
-    }
-    
-    sets.sort { $0.count < $1.count }
-    
-    var ret = [Int]()
-    var seen = Set<Int>()
-    
-    for set in sets {
-        for num in set {
-            if !seen.contains(num) {
-                ret.append(num)
-                seen.insert(num)
-            }
-        }
-    }
-    
-    return ret
+    var cnt = [Int: Int]()
+    _ = s.split { !$0.isNumber }.map { Int(String($0))! }.forEach { cnt[$0, default: 0] += 1 }
+    return cnt.sorted { $0.value > $1.value }.map { $0.key }
 }
