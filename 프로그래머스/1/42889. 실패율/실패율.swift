2b58f65c0b30d4ab529f726196efa243f025eee5
iyungui <121233350+iyungui@users.noreply.github.com>
2025-10-07 23:59:19 +0900

    [level 1] Title: 실패율, Time: 8212.20 ms, Memory: 25.4 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.swift
+++ 프로그래머스/1/42889. 실패율/실패율.swift
@@ -1,23 +1,25 @@
 import Foundation
 
 func solution(_ N:Int, _ stages:[Int]) -> [Int] {
-    var rates = [Int: Double]()
-    let stages = stages.sorted()
+    var stages = stages.sorted()
+    var ret = [(Int, Double)]()
     var start = 0
+    
     for i in 1...N {
         if let index = stages.lastIndex(of: i) {
             let cnt = index - start + 1
             let remain = stages.count - start
             let rate = Double(cnt) / Double(remain)
-            start = index + 1
-            rates[i] = rate
+            start = (index + 1)
+            ret.append((i, rate))
         }
         else {
-            rates[i] = (start <= stages.count) ? 0 : 1
+            if start <= stages.count {
+                ret.append((i, 0))
+            } else {
+                ret.append((i, 1))
+            }
         }
     }
-    
-    return rates
-            .sorted { ($0.value == $1.value) ? ($0.key < $1.key) : ($0.value > $1.value) }
-            .map { Int($0.key) }
+    return ret.sorted { $0.1 == $1.1 ? $0.0 < $1.0 : $0.1 > $1.1 }.map { $0.0 }
 }
