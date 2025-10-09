fb815dbae77174d8d7303023241177dab7808c50
iyungui <121233350+iyungui@users.noreply.github.com>
2025-10-09 15:43:37 +0900

    [level 2] Title: 후보키, Time: 5.53 ms, Memory: 16.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42890. 후보키/후보키.swift
@@ -0,0 +1,66 @@
+import Foundation
+
+func solution(_ relation:[[String]]) -> Int {
+    let n = relation.count
+    let m = relation[0].count
+    var combis = Array(repeating: [[Int]](), count: m + 1)
+    
+    for i in 1...m {    // O(2^m - 1)
+        combis[i] = combination(m, i)
+    }
+    
+    func check(_ combi: [Int]) -> Bool {
+        var picked = Array(repeating: [String](), count: n)
+
+        for c: Int in combi {
+            for i in 0..<n {
+                for j in 0..<m where j == c {
+                    picked[i].append(relation[i][j])
+                }
+            }
+        }
+
+        var sets = Set<[String]>()
+
+        for p: [String] in picked {
+            if sets.contains(p) { return false }
+            sets.insert(p)
+        }
+        return true
+    }
+    
+    var sets = Set<Set<Int>>()
+    for combi: [[Int]] in combis {
+        for cc: [Int] in combi {
+            let curSet = Set(cc)
+            var flag = true
+            for s in sets {
+                if s.isSubset(of: curSet) { flag = false; break }
+            }
+            
+            if flag && check(cc) {
+                sets.insert(curSet)
+            }
+        }
+    }
+    return sets.count
+}
+
+// 1부터 m개뽑는 모든 조합 생성 
+func combination(_ n: Int, _ r: Int) -> [[Int]] {
+    var ret = [[Int]]()
+        
+    func backtrack(_ start: Int, _ cur: [Int]) {
+        if cur.count == r {
+            ret.append(cur)
+            return
+        }
+        for i in start..<n {
+            backtrack(i + 1, cur + [i])
+        }
+    }
+    
+    backtrack(0, [])
+    return ret
+}
+    
