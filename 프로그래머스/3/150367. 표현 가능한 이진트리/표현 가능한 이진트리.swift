ab44c23ab52562399e60948b8dbc31e6a5665bcd
iyungui <121233350+iyungui@users.noreply.github.com>
2025-10-10 12:27:31 +0900

    [level 3] Title: 표현 가능한 이진트리, Time: 107.33 ms, Memory: 17.7 MB -BaekjoonHub

--- 프로그래머스/3/150367. 표현 가능한 이진트리/표현 가능한 이진트리.swift
+++ 프로그래머스/3/150367. 표현 가능한 이진트리/표현 가능한 이진트리.swift
@@ -1,31 +1,26 @@
 import Foundation
 
-func solution(_ numbers: [Int64]) -> [Int] {
-    func go(_ binary: [Character], _ left: Int, _ right: Int) -> Bool {
-        if left > right { return true }
-
-        let mid = (left + right) / 2
-
-        if binary[mid] == "0" {
-            for i in left...right {
-                if binary[i] == "1" { return false }
-            }
-            return true
-        }
-        let l = go(binary, left, mid - 1)
-        let r = go(binary, mid + 1, right)
-
-        return l && r
-    }
-
-    func solve(_ n: Int64) -> Bool {
-        var binary = String(n, radix: 2)
-        var k = 1
-        while (1 << k) - 1 < binary.count { k += 1 }
-        let padding = (1 << k) - 1 - binary.count
-        binary = String(repeating: "0", count: padding) + binary
-        return go(Array(binary), 0, binary.count - 1)
-    }
-
+func solution(_ numbers:[Int64]) -> [Int] {
     return numbers.map { solve($0) ? 1 : 0 }
+}
+
+private func solve(_ num: Int64) -> Bool {
+    var binary = String(num, radix: 2)
+    var k = 1
+    while (1 << k) - 1 < binary.count { k += 1 }
+    let padding = (1 << k) - 1 - binary.count
+    binary = String(repeating: "0", count: padding) + binary
+    return go(Array(binary), 0, binary.count - 1)
+}
+
+private func go(_ arr: [Character], _ left: Int, _ right: Int) -> Bool {
+    if left > right { return true }
+    let mid = (left + right) / 2
+    if arr[mid] == "0" {
+        for i in left...right {
+            if arr[i] == "1" { return false }
+        }
+        return true
+    } 
+    return go(arr, left, mid - 1) && go(arr, mid + 1, right)
 }
