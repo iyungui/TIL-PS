086d58fb11fb09c4d6a51a9d60f5fd7433ccca6f
iyungui <121233350+iyungui@users.noreply.github.com>
2025-10-10 19:30:44 +0900

    [level 2] Title: [3차] 압축, Time: 0.44 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.swift
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.swift
@@ -6,33 +6,20 @@ func solution(_ msg:String) -> [Int] {
         dict[ch] = i
     }
     
+    var iter = msg.makeIterator()
     var ret = [Int]()
-    
-    let msg = Array(msg).map { String($0) }
-    var index = 27
-    
-    var i = 0
-    
-    while i < msg.count {
-        var j = i + 1
-        var w = msg[i]
+    var key = ""
+    while let c = iter.next() {
+        let newKey = key + String(c)
         
-        while j <= msg.count {
-            let c = j < msg.count ? msg[j] : ""
-            let wc = w + c
-            
-            if dict[wc] != nil {
-                w = wc
-                j += 1
-            } else {
-                dict[wc] = index
-                index += 1
-                break
-            }
+        if dict[newKey] != nil {
+            key.append(c)
+        } else {
+            dict[newKey] = dict.count + 1
+            ret.append(dict[key]!)
+            key = String(c)
         }
-        ret.append(dict[w]!)
-        i += w.count
     }
-    
+    ret.append(dict[key]!)
     return ret
 }
