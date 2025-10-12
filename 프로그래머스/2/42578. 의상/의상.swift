d4c5535fde7da79df1e4953162ca080b99996557
iyungui <121233350+iyungui@users.noreply.github.com>
2025-10-12 23:06:23 +0900

    [level 2] Title: 의상, Time: 0.10 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.swift
+++ 프로그래머스/2/42578. 의상/의상.swift
@@ -1,16 +1,6 @@
 import Foundation
 
 func solution(_ clothes:[[String]]) -> Int {
-    var dict: [String: Int] = [:]
-    
-    for item in clothes {
-        let category = item[1]
-        dict[category, default: 1] += 1
-    }
-    
-    var res = 1
-    for cnt in dict.values {
-        res *= cnt
-    }
-    return res - 1
+    var dict = clothes.reduce(into: [String: Int]()) { $0[$1[1], default: 0] += 1 }
+    return dict.values.reduce(1) { $0 * ($1 + 1) } - 1
 }
