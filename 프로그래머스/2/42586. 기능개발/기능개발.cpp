90635ddefb91f75d0ab792c956a111469cf986e6
iyungui <121233350+iyungui@users.noreply.github.com>
2025-10-15 20:06:25 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 4.18 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.cpp
+++ 프로그래머스/2/42586. 기능개발/기능개발.cpp
@@ -5,21 +5,25 @@ using namespace std;
 vector<int> solution(vector<int> progresses, vector<int> speeds) {
     vector<int> answer;
     vector<int> days;
-    
     for(int i = 0; i < progresses.size(); i++) {
-        int remaining = 100 - progresses[i];
-        int day = (remaining + speeds[i] - 1) / speeds[i];
+        int remain = 100 - progresses[i];
+        int day = (remain + speeds[i] - 1) / speeds[i];
         days.push_back(day);
     }
+    
     int i = 0;
-    while(i < days.size()) {
-        int cnt = 1;
+    
+    while (i < days.size()) {
+        int count = 1;
         int deploy = days[i];
-        while (i + cnt < days.size() && days[i + cnt] <= deploy) {
-            cnt++;
+        
+        while(i + count < days.size() && days[i + count] <= deploy) {
+            count++;
         }
-        answer.push_back(cnt);
-        i += cnt;
+        
+        answer.push_back(count);
+        i += count;
     }
+    
     return answer;
 }
