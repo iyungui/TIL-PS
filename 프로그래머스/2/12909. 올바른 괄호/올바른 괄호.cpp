674f30eb0c0b7e29b721ecdd5bbf714250cce1e3
iyungui <121233350+iyungui@users.noreply.github.com>
2025-10-15 16:02:01 +0900

    [level 2] Title: 올바른 괄호, Time: 0.38 ms, Memory: 3.8 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.cpp
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.cpp
@@ -3,11 +3,10 @@
 using namespace std;
 
 bool solution(string s) {
-    stack<char> stk;
-    for (int i = 0; i < s.size(); i++) {
-        if (s[i] == '(') stk.push(s[i]);
-        else if (stk.size() && s[i] == ')') stk.pop();
-        else return false;
+    int n = 0;
+    for(int i = 0; i <s.size(); i++) {
+        if (s[i] == '(') n++;
+        else if (--n < 0) return false;
     }
-    return stk.empty();
+    return n == 0;
 }
