80cd4d75341da7fdf3c555f6333a1aa4331d81aa
iyungui <121233350+iyungui@users.noreply.github.com>
2025-10-16 13:33:22 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 3.68 MB -BaekjoonHub

---
+++ 프로그래머스/1/42862. 체육복/체육복.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int solution(int n, vector<int> lost, vector<int> reserve) {
+    vector<int> students(n + 1, 1);
+    
+    for(int l: lost) students[l]--;
+    for(int r: reserve) students[r]++;
+    
+    for(int i = 1; i <= n; i++) {
+        if(students[i] <= 0) {
+            if (i > 1 && students[i - 1] > 1) {
+                students[i - 1]--;
+                students[i]++;
+            }
+            
+            else if(i < n &&students[i + 1] > 1) {
+                students[i + 1]--;
+                students[i]++;
+            }
+        }
+    }
+    
+    int answer = 0;
+    for(int i = 1; i <= n; i++) {
+        if(students[i] > 0) answer++;
+    }
+    return answer;
+}
