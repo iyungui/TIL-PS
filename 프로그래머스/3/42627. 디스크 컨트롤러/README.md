e97627f953c9a97b84169c552533ac212820b9b3
iyungui <121233350+iyungui@users.noreply.github.com>
2025-10-16 10:06:32 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.14 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/README.md
+++ 프로그래머스/3/42627. 디스크 컨트롤러/README.md
@@ -1,10 +1,10 @@
 # [level 3] 디스크 컨트롤러 - 42627 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42627?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42627) 
 
 ### 성능 요약
 
-메모리: 4.14 MB, 시간: 0.11 ms
+메모리: 4.21 MB, 시간: 0.14 ms
 
 ### 구분
 
@@ -18,7 +18,7 @@
 
 ### 제출 일자
 
-2025년 10월 16일 00:16:46
+2025년 10월 16일 10:06:29
 
 ### 문제 설명
 
--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.cpp
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.cpp
@@ -3,32 +3,32 @@
 using namespace std;
 
 struct Job {
-    int number, req, t;
+    int num, req, t;
+    
     bool operator>(const Job& other) const {
-        if (t != other.t) return t > other.t;
-        if (req != other.req) return req > other.req;
-        return number > other.number;
+        if(t != other.t) return t > other.t;
+        if(req != other.req) return req > other.req;
+        return num > other.num;
     }
 };
 
 int solution(vector<vector<int>> jobs) {
     sort(jobs.begin(), jobs.end());
-    
     priority_queue<Job, vector<Job>, greater<Job>> pq;
-    int curTime = 0, sum = 0, idx = 0;
+    int idx = 0, time = 0, sum = 0;
     
-    while (idx < jobs.size() || !pq.empty()) {
-        while (idx < jobs.size() && jobs[idx][0] <= curTime) {
+    while (idx < jobs.size() || pq.size()) {
+        while (idx < jobs.size() && time >= jobs[idx][0]) {
             pq.push({idx, jobs[idx][0], jobs[idx][1]});
             idx++;
         }
         
-        if (pq.empty()) {
-            curTime = jobs[idx][0];
-        } else {
+        if (idx < jobs.size() && pq.empty()) time = jobs[idx][0];
+        else {
             Job cur = pq.top(); pq.pop();
-            curTime += cur.t;
-            sum += (curTime - cur.req);
+            time += cur.t;
+            
+            sum += (time - cur.req);
         }
     }
     return sum / jobs.size();
