d7f41c08619a0171e479021509a208ea0d812f4c
iyungui <121233350+iyungui@users.noreply.github.com>
2025-10-16 10:41:58 +0900

    [level 2] Title: 전화번호 목록, Time: 71.72 ms, Memory: 31.5 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
@@ -3,19 +3,10 @@
 using namespace std;
 
 bool solution(vector<string> phone_book) {
-    unordered_set<string> hash_set;
+    sort(phone_book.begin(), phone_book.end());
     
-    for(const string& phone: phone_book) {
-        hash_set.insert(phone);
+    for(int i = 0; i < phone_book.size() - 1; i++) {
+        if (phone_book[i + 1].find(phone_book[i]) == 0) return false;
     }
-    
-    for (const string& phone: phone_book) {
-        for(int i = 1; i < phone.size(); i++) {
-            if(hash_set.find(phone.substr(0, i)) != hash_set.end()) {
-                return false;
-            }
-        }
-    }
-    
     return true;
 }
