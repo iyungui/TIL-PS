4d20da6c748f7f3a4db8fec0c071cf58596d9ff1
iyungui <121233350+iyungui@users.noreply.github.com>
2025-11-09 16:07:47 +0900

    [D3] Title: 햄버거 다이어트, Time: 19 ms, Memory: 6,808 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.cpp
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.cpp
@@ -2,37 +2,37 @@
 
 using namespace std;
 
-int t;
+int TC;
 
 int N, L;   // 재료의 수, 제한 칼로리
 
 int T[24], K[24];   // 맛 점수 T, 칼로리 K
 int ret;
-
-void go(int idx, int score, int kcal) {
-    if(idx == N) {
-        ret = max(ret, score);
-        return;
-    }
-    if (kcal + K[idx] <= L) go(idx + 1, score + T[idx], kcal + K[idx]);
-    go(idx + 1, score, kcal);
-}
+int dp[21][10001];
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
 
-    cin >> t;
-    for(int i = 0; i < t; i++) {
+    cin >> TC;
+    for(int t = 0; t < TC; t++) {
         cin >> N >> L;
         memset(T, 0, sizeof(T));
         memset(K, 0, sizeof(K));
+        memset(dp, 0, sizeof(dp));
+        for(int i = 1; i <= N; i++) cin >> T[i] >> K[i];
+        
+        for(int i = 1; i <= N; i++) {
+            for(int w = 0; w <= L; w++) {
+                dp[i][w] = dp[i-1][w];
+                if(w >= K[i]) {
+                    dp[i][w] = max(dp[i-1][w], dp[i-1][w - K[i]] + T[i]);
+                }
+            }
+        }
 
-        for(int j = 0; j < N; j++) cin >> T[j] >> K[j];
-        ret = 0;
-        go(0, 0, 0);
-        cout << "#" << i + 1 << " " << ret << '\n'; 
+        cout << "#" << t + 1 << " " << dp[N][L] << '\n'; 
     }
     return 0;
 }
