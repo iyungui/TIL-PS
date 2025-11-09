0fca3e77c0f2658a8d18c6c19e94741fea4b7c67
iyungui <121233350+iyungui@users.noreply.github.com>
2025-11-09 22:14:11 +0900

    [D1] Title: 연월일 달력, Time: 4 ms, Memory: 396 KB -BaekjoonHub

--- SWEA/D1/2056. 연월일 달력/연월일 달력.cpp
+++ SWEA/D1/2056. 연월일 달력/연월일 달력.cpp
@@ -7,26 +7,18 @@ string s;
 int days[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr); cout.tie(nullptr);
     cin >> T;
     for(int t = 1; t <= T; t++) {
         cin >> s;
-        string y = s.substr(0, 4);
-        string m = s.substr(4, 2);
-        string d = s.substr(6, 2);
-
-        int mm = stoi(m);
-        int dd = stoi(d);
-
-        bool flag = false;
-
-        if(mm < 1 || mm > 12) flag = true;
-        else if(dd < 1 || dd > days[mm]) flag = true;
-
+        int y = stoi(s.substr(0, 4));
+        int m = stoi(s.substr(4, 2));
+        int d = stoi(s.substr(6, 2));
+        
+        bool vaild = (m >= 1 && m <= 12 && d >= 1 && d <= days[m]);
+        
         cout << "#" << t << " ";
-        if(flag) cout << -1 << '\n';
-        else cout << y << "/" << m << "/" << d << '\n';
+        if(vaild) printf("%04d/%02d/%02d\n", y, m, d);
+        else cout << -1 << '\n';
     }
     return 0;
 }
