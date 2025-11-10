b6c7aafc68f3a325e2112eb8ecf1067f51878c5a
iyungui <121233350+iyungui@users.noreply.github.com>
2025-11-10 17:39:14 +0900

    [D3] Title: [S/W 문제해결 응용] 2일차 - 최대 상금, Time: 57 ms, Memory: 6,268 KB -BaekjoonHub

--- SWEA/D3/1244. ［S／W 문제해결 응용］ 2일차 － 최대 상금/［S／W 문제해결 응용］ 2일차 － 최대 상금.cpp
+++ SWEA/D3/1244. ［S／W 문제해결 응용］ 2일차 － 최대 상금/［S／W 문제해결 응용］ 2일차 － 최대 상금.cpp
@@ -1,43 +1,35 @@
 #include<bits/stdc++.h>
-
 using namespace std;
 
-int T, ret, c;
-string s;
 map<pair<string, int>, int> dp;
 
-int go(string s, int cnt) {
-    if(cnt == c) {
-        return stoi(s);
-    }
-    if(dp.count({s, cnt})) return dp[{s, cnt}];
+int T, cnt;
+string s;
 
+int go(string s, int c) {
+    if(c == cnt) return stoi(s);
+    if(dp.count({s, c})) return dp[{s, c}];
     int maxVal = 0;
     for(int i = 0; i < s.size(); i++) {
         for(int j = i + 1; j < s.size(); j++) {
             if(s[i] == s[j]) continue;
             swap(s[i], s[j]);
-            int result = go(s, cnt + 1);
+            maxVal = max(maxVal, go(s, c + 1));
             swap(s[j], s[i]);
-            maxVal = max(maxVal, result);
         }
     }
-    dp[{s, cnt}] = maxVal;
-    return maxVal;
+    return dp[{s, c}] = maxVal;
 }
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr); cout.tie(nullptr);
-
+    ios_base::sync_with_stdio(false); cout.tie(nullptr); cin.tie(nullptr);
     cin >> T;
 
     for(int t = 1; t <= T; t++) {
         dp.clear();
-        cin >> s >> c;
-        ret = go(s, 0);
+        cin >> s >> cnt;
+        int ret = go(s, 0);
         cout << "#" << t << " " << ret << '\n';
     }
-
     return 0;
 }
