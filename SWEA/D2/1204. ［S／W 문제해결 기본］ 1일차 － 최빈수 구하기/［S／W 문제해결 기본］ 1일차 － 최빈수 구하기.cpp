6d0f9e75c15d15c923e2d1078a5ff3716f7b3e1a
iyungui <121233350+iyungui@users.noreply.github.com>
2025-11-10 16:13:36 +0900

    [D2] Title: [S/W 문제해결 기본] 1일차 - 최빈수 구하기, Time: 5 ms, Memory: 6,000 KB -BaekjoonHub

--- SWEA/D2/1204. ［S／W 문제해결 기본］ 1일차 － 최빈수 구하기/［S／W 문제해결 기본］ 1일차 － 최빈수 구하기.cpp
+++ SWEA/D2/1204. ［S／W 문제해결 기본］ 1일차 － 최빈수 구하기/［S／W 문제해결 기본］ 1일차 － 최빈수 구하기.cpp
@@ -5,6 +5,11 @@ using namespace std;
 int T, t, temp;
 int a[104];
 
+bool cmp(const pair<int, int>& a, const pair<int, int>& b) {
+    if(a.first == b.first) return a.second > b.second;
+    return a.first > b.first;
+}
+
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr); cout.tie(nullptr);
@@ -12,22 +17,22 @@ int main() {
     while(T--) {
         cin >> t;
         memset(a, 0, sizeof(a));
+
         for(int i = 0; i < 1000; i++) {
             cin >> temp;
             a[temp]++;
         }
         
-        int maxCnt = -1;
-        int num = -1;
+        vector<pair<int, int>> v;   // [빈도수, 점수]
 
         for(int i = 0; i <= 100; i++) {
-            if(maxCnt <= a[i]) {
-                maxCnt = a[i];
-                num = i;
-            }
+            if(a[i] > 0) v.push_back({a[i], i});
         }
 
-        cout << "#" << t << " " << num << '\n';
+        // 빈도수 내림차순, 같으면 점수 내림차순
+        sort(v.begin(), v.end(), cmp);
+
+        cout << "#" << t << " " << v[0].second << '\n';
     }
 
     return 0;
