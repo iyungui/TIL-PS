8e33b1d7c0bcf789edab135d7c099f3684d565ee
iyungui <121233350+iyungui@users.noreply.github.com>
2025-11-12 14:02:57 +0900

    [D2] Title: 숫자 배열 회전, Time: 4 ms, Memory: 396 KB -BaekjoonHub

--- SWEA/D2/1961. 숫자 배열 회전/숫자 배열 회전.cpp
+++ SWEA/D2/1961. 숫자 배열 회전/숫자 배열 회전.cpp
@@ -1,47 +1,58 @@
 #include<bits/stdc++.h>
+
 using namespace std;
 
-int TC, n;
-int a[10][10];
-int r90[10][10], r180[10][10], r270[10][10];
+int T, n;
+int a[10][10], r90[10][10], r180[10][10], r270[10][10]; 
+
+void initialize() {
+    memset(a, 0, sizeof(a));
+    memset(r90, 0, sizeof(r90));
+    memset(r180, 0, sizeof(r180));
+    memset(r270, 0, sizeof(r270));
+
+    cin >> n;
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            cin >> a[i][j];
+        }
+    }
+}
+
+void rotate() {
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            r90[j][n-1-i] = a[i][j];
+            r180[n-1-i][n-1-j] = a[i][j];
+            r270[n-1-j][i] = a[i][j];
+        }
+    }
+}
+
+void print(int t) {
+    cout << "#" << t << "\n";
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) cout << r90[i][j];
+        cout << " ";
+        for(int j = 0; j < n; j++) cout << r180[i][j];
+        cout << " ";
+        for(int j = 0; j < n; j++) cout << r270[i][j];
+        cout << '\n';
+    }
+}
 
 int main() {
-    ios_base::sync_with_stdio(false); cout.tie(nullptr); cin.tie(nullptr);
-    cin >> TC;
-    for(int t = 1; t <= TC; t++) {
-        cin >> n;
-        
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < n; j++) {
-                cin >> a[i][j];
-            }
-        }
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
 
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < n; j++) {
-                r90[j][n-1-i] = a[i][j];
-                r180[n-1-i][n-1-j] = a[i][j];
-                r270[n-1-j][i] = a[i][j];
-            }
-        }
+    cin >> T;
 
-        cout << "#" << t << '\n';
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < n; j++) {
-                cout << r90[i][j];
-            }
-            cout << " ";
-
-            for(int j = 0; j < n; j++) {
-                cout << r180[i][j];
-            }
-            cout << " ";
-
-            for(int j = 0; j < n; j++) {
-                cout << r270[i][j];
-            }
-            cout << '\n';
-        }
+    for(int t = 1; t <= T; t++) {
+        initialize();
+        rotate();
+        print(t);
     }
     return 0;
 }
