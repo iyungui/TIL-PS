711e0722325cca49a06270b4ef9589a0c134e392
iyungui <121233350+iyungui@users.noreply.github.com>
2025-11-12 15:12:06 +0900

    [D3] Title: 부분 수열의 합, Time: 6 ms, Memory: 5,924 KB -BaekjoonHub

--- SWEA/D3/2817. 부분 수열의 합/부분 수열의 합.cpp
+++ SWEA/D3/2817. 부분 수열의 합/부분 수열의 합.cpp
@@ -3,30 +3,27 @@
 using namespace std;
 
 int T, n, k;
-int dp[1004];
-int a[24];
+int a[24], dp[24][1004];
 
 int main() {
     cin >> T;
-
     for(int t = 1; t <= T; t++) {
         cin >> n >> k;
 
-        memset(dp, 0, sizeof(dp));
         memset(a, 0, sizeof(a));
+        memset(dp, 0, sizeof(dp));
 
-        for(int i = 0; i < n; i++) cin >> a[i];
+        for(int i = 1; i <= n; i++) cin >> a[i];
 
-
-        dp[0] = 1;
-
-        for(int i = 0; i < n; i++) {
-            for(int j = k; j >= a[i]; j--) {
-                dp[j] += dp[j - a[i]];
+        dp[0][0] = 1;
+        for(int i = 1; i <= n; i++) {
+            for(int j = 0; j <= k; j++) {
+                dp[i][j] = dp[i-1][j];
+                if(j >= a[i]) dp[i][j] += dp[i-1][j - a[i]];
             }
         }
-
-        cout << "#" << t << " " << dp[k] << '\n';
+         
+        cout << "#" << t << " " << dp[n][k] << '\n';
     }
 
     return 0;
