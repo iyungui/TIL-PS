a7ff5e3c8c935c0683d44549d2c4bef6e8087def
iyungui <121233350+iyungui@users.noreply.github.com>
2025-11-22 00:15:16 +0900

    [Gold IV] Title: 뮤탈리스크, Time: 0 ms, Memory: 3044 KB -BaekjoonHub

--- 백준/Gold/12869. 뮤탈리스크/뮤탈리스크.cc
+++ 백준/Gold/12869. 뮤탈리스크/뮤탈리스크.cc
@@ -1,63 +1,46 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
+
 using namespace std;
 
-int n;
-int a[3], visited[64][64][64];  // 해당 체력 방문한 적 있는가?
-
-vector<int> b = {1, 3, 9};
-vector<vector<int>> v;
-
-struct scv {
-    int x, y, z;
+int attack[6][3] = {
+    {9, 3, 1},
+    {9, 1, 3},
+    {3, 1, 9},
+    {3, 9, 1},
+    {1, 9, 3},
+    {1, 3, 9}
 };
 
-void bfs(int x, int y, int z) {
-    int cnt = 0;
-    queue<scv> q;
-    q.push({x, y, z});
-    
-    visited[x][y][z] = 1;
-    
+struct A { 
+    int a, b, c;
+};
+
+int n, mu[3];
+int visited[64][64][64];
+queue<A> q;
+int main() {
+    cin >> n;
+    for(int i = 0; i < n; i++) cin >> mu[i];
+
+    q.push({mu[0], mu[1], mu[2]});
+    visited[mu[0]][mu[1]][mu[2]] = 1;
     while(q.size()) {
-        x = q.front().x;
-        y = q.front().y;
-        z = q.front().z;
+        int x = q.front().a;
+        int y = q.front().b;
+        int z = q.front().c;
         q.pop();
         
-        if(x == 0 && y == 0 && z == 0) {
-            cout << visited[x][y][z] - 1 << '\n';
-            return;
-        }
-        
-        for(int i = 0; i < v.size(); i++) {
-            int nx = max(0, x - v[i][0]);
-            int ny = max(0, y - v[i][1]);
-            int nz = max(0, z - v[i][2]);
-            if(visited[nx][ny][nz]) continue;
+        if(visited[0][0][0]) break;
+        for(int i = 0; i < 6; i++) {
+            int nx = max(0, x - attack[i][0]);
+            int ny = max(0, y - attack[i][1]);
+            int nz = max(0, z - attack[i][2]);
             
+            if(visited[nx][ny][nz]) continue;
             visited[nx][ny][nz] = visited[x][y][z] + 1;
-            q.push({nx, ny, nz});
+            q.push({nx,ny,nz});
         }
     }
-    
-    return;
-}
-
-int main() {
-    
-    do {
-        v.push_back(b);
-    } while(next_permutation(b.begin(), b.end()));
-    
-    cin >> n;
-    
-    memset(a, 0, sizeof(a));
-    
-    for(int i = 0; i < n; i++) {
-        cin >> a[i];
-    }
-    
-    bfs(a[0], a[1], a[2]);
-    
+    cout << visited[0][0][0] - 1 << '\n';
     return 0;
 }
