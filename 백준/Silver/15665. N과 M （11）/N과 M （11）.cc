7180fe79baeb5be9ab05aacabadf437b2ee3c286
iyungui <121233350+iyungui@users.noreply.github.com>
2026-01-04 18:32:52 +0900

    [Silver II] Title: N과 M (11), Time: 104 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15665. N과 M （11）/N과 M （11）.cc
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.cc
@@ -12,20 +12,25 @@ void go(int k) {
         cout << '\n';
         return;
     }
-
-    int prev = -1;
     for(int i = 0; i < n; i++) {
-        if(prev == nums[i]) continue;
-        prev = nums[i];
         a[k] = nums[i];
         go(k+1);
     }
 }
 
+vector<bool> chk(10002);
+
 int main() {
     ios::sync_with_stdio(0); cin.tie(0);
     cin >> n >> m;
-    for(int i = 0; i < n; i++) cin >> nums[i];
+    int idx = 0;
+    while(n--) {
+        cin >> nums[idx];
+        if(chk[nums[idx]]) continue;
+        chk[nums[idx]] = 1;
+        idx++;
+    }
+    n = idx;
     sort(nums, nums+n);
     go(0);
     return 0;
