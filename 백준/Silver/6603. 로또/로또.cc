6b268d23500ee76860bf5c5bd6c61c43a8c92d11
iyungui <121233350+iyungui@users.noreply.github.com>
2026-01-04 12:50:37 +0900

    [Silver II] Title: 로또, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/6603. 로또/로또.cc
+++ 백준/Silver/6603. 로또/로또.cc
@@ -2,31 +2,21 @@
 using namespace std;
 
 int k;
-int a[15], b[6];
-
-void go(int cnt, int st) {
-    if(cnt == 6) {
-        for(int i = 0; i < 6; i++) {
-            cout << b[i] << ' ';
-        }
-        cout << '\n';
-        return;
-    }
-    for(int i = st; i < k; i++) {
-        b[cnt] = a[i];
-        go(cnt+1, i+1);
-    }
-}
+int a[15], mask[15];
 
 int main() {
     ios_base::sync_with_stdio(false); cin.tie(0);
     while(1) {
         cin >> k;
-        if(k == 0) break;
-        for(int i = 0; i < k; i++) {
-            cin >> a[i];
-        }
-        go(0, 0);
+        if(!k) break;
+        for(int i = 0; i < k; i++) cin >> a[i];
+        for(int i = 6; i < k; i++) mask[i] = 1;
+        do {
+            for(int i = 0; i < k; i++) {
+                if(!mask[i]) cout << a[i] << ' ';
+            }
+            cout << '\n';
+        } while(next_permutation(mask, mask+k));
         cout << '\n';
     }
     return 0;
