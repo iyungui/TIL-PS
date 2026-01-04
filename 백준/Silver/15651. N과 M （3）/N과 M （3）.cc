9033563458ac5f50279038ab8dacb53f48af74d9
iyungui <121233350+iyungui@users.noreply.github.com>
2026-01-04 13:02:16 +0900

    [Silver III] Title: N과 M (3), Time: 300 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.cc
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int n, m;
+int a[10];
+void go(int k, int idx) {
+    if(k == m) {
+        for(int i = 0; i < m; i++) {
+            cout << a[i] << ' ';
+        }
+        cout << '\n';
+        return;
+    }
+
+    for(int i = 1; i <= n; i++) {
+        a[k] = i;
+        go(k + 1, i);
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(0);
+    cin >> n >> m;
+    go(0, 1);
+    return 0;
+}
