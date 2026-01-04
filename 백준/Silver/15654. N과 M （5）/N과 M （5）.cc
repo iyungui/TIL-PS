6c2e6e7c8a5140af6cb93d7a05544a873fc07477
iyungui <121233350+iyungui@users.noreply.github.com>
2026-01-04 17:18:09 +0900

    [Silver III] Title: N과 M (5), Time: 28 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.cc
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int n, m;
+int a[10], nums[10], isused[10];
+
+void go(int k) {
+    if(k == m) {
+        for(int i = 0; i < m; i++) {
+            cout << a[i] << ' ';
+        }
+        cout << '\n';
+        return;
+    }
+
+    for(int i = 0; i < n; i++) {
+        if(!isused[i]) {
+            isused[i] = 1;
+            a[k] = nums[i];
+            go(k+1);
+            isused[i] = 0;
+        }
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(0);
+    cin >> n >> m;
+    for(int i = 0; i < n; i++) cin >> nums[i];
+    sort(nums, nums + n);
+    go(0);
+    return 0;
+}
