48093088cd2be278bfc0d9b34546179ef3951b31
iyungui <121233350+iyungui@users.noreply.github.com>
2026-03-02 14:15:26 +0900

    [Silver III] Title: 이친수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/2193. 이친수/이친수.cc
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+int n;
+ll dp[91][2];
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+    cin >> n;
+    dp[1][1] = 1;
+    dp[1][0] = 0;
+    for(int i = 2; i <= n; i++) {
+        dp[i][0] = dp[i-1][0] + dp[i-1][1];
+        dp[i][1] = dp[i-1][0];
+    }
+    cout << dp[n][0] + dp[n][1] << '\n';
+    return 0;
+}
