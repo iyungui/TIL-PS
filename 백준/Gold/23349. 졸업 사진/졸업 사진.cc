4f493d17a1bd1a4e30b93db964b6e8760ed30554
이융의 <121233350+iyungui@users.noreply.github.com>
2026-03-27 21:58:59 +0900

    [Gold V] Title: 졸업 사진, Time: 16 ms, Memory: 3992 KB -BaekjoonHub

--- 백준/Gold/23349. 졸업 사진/졸업 사진.cc
+++ 백준/Gold/23349. 졸업 사진/졸업 사진.cc
@@ -3,67 +3,76 @@
 using namespace std;
 
 struct Info {
-    string place;
-    int max_cnt, st, ed;
+    string p;
+    int cnt, st, ed;
 };
 
 int main() {
     ios::sync_with_stdio(0); cin.tie(0);
     int n; cin >> n;
 
-    // 중복학생 체크용
+    map<string, vector<int>> cnt;
     set<string> names;
-    // 장소별 시간 카운트 배열(1~50000)
-    map<string, vector<int>> place_data;
 
     for(int i = 0; i < n; i++) {
         string name, place;
         int st, ed;
         cin >> name >> place >> st >> ed;
 
-        // 1. 학생 중복 제출 방지
-        if(names.count(name))continue;
+        if(names.count(name)) continue;
         names.insert(name);
 
-        // 2. 장소별 시간대 카운팅(처음 등장하는 장소면 벡터 크기 초기화)
-        if(place_data.find(place) == place_data.end()) place_data[place].resize(50005, 0);
+        // 처음 나온 장소이면 벡터크기 초기화
+        if(cnt.find(place) == cnt.end()) {
+            cnt[place].resize(50004, 0);
+        };
+
         for(int t = st; t < ed; t++) {
-            place_data[place][t]++;
+            cnt[place][t]++;
         }
     }
 
     Info best = {"", -1, 0, 0};
-    // 각 장소를 순회(map은 키 기준 자동 정렬됨)
-    for(auto const& [place, counts] : place_data) {
-        int max_val = 0;
+
+    for(auto const& [place, counts] : cnt) {
+        int cur_max_cnt = 0;
+
         for(int t = 1; t <= 50000; t++) {
-            max_val = max(max_val, counts[t]);
+            cur_max_cnt = max(cur_max_cnt, counts[t]);
         }
 
-        if(max_val == 0) continue;
+        if(cur_max_cnt == 0) continue;
 
-        // 해당 장소에서 max_val이 유지되는 가장 빠르고 가장 긴 구간 찾기
         int s, e;
-
         for(int t = 1; t <= 50000; t++) {
-            if(counts[t] == max_val) {
+            if(cur_max_cnt == counts[t]) {
                 s = t;
-                while(t <= 50000 && counts[t] == max_val) {
+                while(t <= 50000 && cur_max_cnt == counts[t]) {
                     t++;
                 }
                 e = t;
-
-                // 가장 빠른 시간대를 찾아야 하므로, 첫 번째 발견된 구간만 확인
                 break;
             }
         }
 
-        // 1)인원수, 2,3,4) 사전순 장소(이미정렬됨), 가장 빠른. 긴 시간대는 이미 장소별로찾음
-        if(max_val > best.max_cnt) {
-            best = {place, max_val, s, e};
-        }
+        // 1
+        if(cur_max_cnt > best.cnt) best = {place, cur_max_cnt, s, e};
     }
 
-    cout << best.place << " " << best.st << " " << best.ed << '\n';
+    cout << best.p << " " << best.st << " " << best.ed << '\n';
     return 0;
 }
+/*
+같은 이름 학생 -> 첫번째 제출만 인정 => set<string>
+장소(10가지)
+시간대 st, ed(50000)
+
+우선순위
+1.제출횟수cnt
+2.가장앞에오는시간대
+3.가장빠른
+4.가장 긴시간
+
+장소별로 시간대 카운트(1) => 장소별로 maxCnt시작부터 maxCnt끝까지의 구간 => 그 구간 중 우선순위대로
+int cnt[11][50004]
+*/
