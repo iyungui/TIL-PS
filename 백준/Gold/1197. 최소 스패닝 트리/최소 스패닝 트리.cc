e9e7f8327bbf7409fef308c43d27a4983b084bc1
이융의 <121233350+iyungui@users.noreply.github.com>
2026-04-04 02:51:44 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 36 ms, Memory: 3356 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.cc
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.cc
@@ -1,13 +1,13 @@
 #include<bits/stdc++.h>
-
 using namespace std;
-/*
-1.간선을 크기 오름차순 정렬 하고 제일 낮은 비용 간선 선택
-2. 현재선택한 간선이 u,v연결 간선이라고할때. 만약 u, v 같은 그룹이면. continue. 다른 그룹이면 같은그룹으로 만들고. 현재 선택한 간선을 mst에 추가
-3. mst에 v-1개 간선 추가라면 종료. 그렇지않다면 비용이 작은간선 선택후 2번과정반복
-*/
 
+/*
+1. 간선을 크기 오름차순으로 정렬하고 제일 낮은 비용 간선을 선택
+2. 현재 선택한 간선이 u, v 연결하는 간선이면. 만약 u,v가 같은그룹이면 continue. 다른그룹이면 union. 현재 선택한 간선을 mst에 추가
+3. mst에 v-1개 간선추가라면 종료. 그렇지 않다면 비용이 작은간선 선택 후 2번과정반복
+*/
 vector<int> p(10004, -1);
+tuple<int, int, int> edge[100004];
 
 int find(int x) {
     if(p[x] < 0) return x;
@@ -23,12 +23,10 @@ bool is_diff_group(int u, int v) {
     else p[u] = v;
     return 1;
 }
-int v, e;
-tuple<int, int, int> edge[100005];
 
 int main() {
     ios::sync_with_stdio(0); cin.tie(0);
-    cin >> v >> e;
+    int v, e; cin >> v >> e;
     for(int i = 0; i < e; i++) {
         int a, b, cost; cin >> a >> b >> cost;
         edge[i] = {cost, a, b};
@@ -41,8 +39,7 @@ int main() {
         if(!is_diff_group(a, b)) continue;
         ans += cost;
         cnt++;
-        if(cnt == v - 1) break;
-    }
-    cout << ans;
+        if(cnt == v-1) break;
+    }cout << ans << '\n';
     return 0;
 }
