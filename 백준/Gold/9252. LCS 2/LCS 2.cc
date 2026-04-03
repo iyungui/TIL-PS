0af309a8a9f27e6d3bf0ea6a4b922ed2a756498e
이융의 <121233350+iyungui@users.noreply.github.com>
2026-04-03 20:21:10 +0900

    [Gold III] Title: LCS 2, Time: 4 ms, Memory: 5964 KB -BaekjoonHub

--- 백준/Gold/9252. LCS 2/LCS 2.cc
+++ 백준/Gold/9252. LCS 2/LCS 2.cc
@@ -1,50 +1,40 @@
-/*
-첫번째 문자열의 앞에서부터 i개까지 봤을때 LCS
-두번째 문자열의 앞에서부터 j개까지 봤을때 LCS
-
-d[i][j] = d[i-1][j-1]+1 (x[i] == y[j])
-d[i][j] = max(d[i-1][j], d[i][j-1]) (x[i]!=y[j])
-
-d[i][0] = 0
-d[0][j] = 0
-*/
-
 #include<bits/stdc++.h>
+
 using namespace std;
 
-string x, y;
 int d[1004][1004];
+
 int main() {
     ios::sync_with_stdio(0); cin.tie(0);
+    string x, y; 
     cin >> x >> y;
-
     int a = x.size();
     int b = y.size();
 
     for(int i = 1; i <= a; i++) {
         for(int j = 1; j <= b; j++) {
-            if(x[i-1] == y[j-1]) d[i][j] = d[i-1][j-1] + 1;
-            else d[i][j] = max(d[i-1][j], d[i][j-1]);
+            if(x[i-1] == y[j-1]) {
+                d[i][j] = d[i-1][j-1] + 1;
+            }
+            else {
+                d[i][j] = max(d[i-1][j], d[i][j-1]);
+            }
         }
     }
-
     int i = a;
     int j = b;
+
     string ret = "";
     while(1) {
         if(i == 0 || j == 0) break;
+
         if(x[i-1] == y[j-1]) {
             ret = x[i-1] + ret;
-            i--;
-            j--;
+            i--; j--;
         }
         else {
-            if(d[i-1][j] >= d[i][j-1]) {
-                i--;    // up
-            }
-            else {
-                j--;    // left
-            }
+            if(d[i-1][j] >= d[i][j-1]) i--;
+            else j--;
         }
     }
 
