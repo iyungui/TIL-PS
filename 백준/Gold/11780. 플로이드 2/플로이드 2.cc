8cbea585d3ec18b5d8f84346cf7c9d9b31901585
이융의 <121233350+iyungui@users.noreply.github.com>
2026-04-04 02:26:19 +0900

    [Gold II] Title: 플로이드 2, Time: 28 ms, Memory: 2104 KB -BaekjoonHub

--- 백준/Gold/11780. 플로이드 2/플로이드 2.cc
+++ 백준/Gold/11780. 플로이드 2/플로이드 2.cc
@@ -1,58 +1,52 @@
 #include<bits/stdc++.h>
-
 using namespace std;
+
 int dist[104][104];
 int nxt[104][104];
-const int INF = 0x3f3f3f3f;
-
 int main() {
     ios::sync_with_stdio(0); cin.tie(0);
-    int n, m;
-    cin >> n >> m;
-    // init
+    int n, m; cin >> n >> m;
+    
+    // 자기 자신은 0, 나머지는 INF
     for(int i = 1; i <= n; i++) {
         for(int j = 1; j <= n; j++) {
-            if(i==j) dist[i][j] = 0;
-            else dist[i][j] = INF;
+            if(i == j) dist[i][j] = 0;
+            else dist[i][j] = 0x3f3f3f3f;
         }
     }
-    while(m--){
-        int a, b, c;
-        cin >> a >> b >> c;
-        // 여러 개 노선 중 최단거리만 저장
-        dist[a][b] = min(dist[a][b], c);
-        nxt[a][b] = b;  // a에서 b로 가는 최단거리는 b를 거쳐야한다
-    }
 
-    // O(V^3)
-    // k -> i -> j순서로.
-    for(int k = 1; k <= n; k++) {   // 거쳐가는 노드 k
-        for(int i = 1; i <= n; i++) {   // 시작 노드 i
-            for(int j = 1; j <= n; j++) {   // 도착 노드 J
-                // i -> j로 바로 가는 것보다
-                // k를 거쳐가야 하는 것이 더 빠른 경우에 갱신
+    // 버스 정보 입력받기
+    for(int i = 0; i < m; i++) {
+        int a, b, c; cin >> a >> b >> c;
+        // 여러 개 노선 중 최단 거리만 저장
+        dist[a][b] = min(dist[a][b], c);
+        nxt[a][b] = b;
+    }
+    
+    // O(v^3)
+    // k -> i 순서로
+    for(int k = 1; k <= n; k++) {
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= n; j++) {
                 if(dist[i][j] > dist[i][k] + dist[k][j]) {
                     dist[i][j] = dist[i][k] + dist[k][j];
-                    nxt[i][j] = nxt[i][k];  // i에서 j까지 최단거리로 가려면 i에서 k로 가야한다
-                }    
+                    nxt[i][j] = nxt[i][k];
+                }
             }
-        }    
+        }
     }
 
     for(int i = 1; i <= n; i++) {
         for(int j = 1; j <= n; j++) {
-            // 갈 수 없다면 0 출력
-            if(dist[i][j] == INF) cout << 0 << " ";
+            if(dist[i][j] == 0x3f3f3f3f) cout << 0 << " ";
             else cout << dist[i][j] << " ";
         }
         cout << '\n';
     }
 
-    // nxt 테이블 출력
     for(int i = 1; i <= n; i++) {
         for(int j = 1; j <= n; j++) {
-            if(dist[i][j] == 0 || dist[i][j] == INF) {
-                // 자기자신으로가는 경로 혹은 갈 수 없는 경로는 0 출력
+            if(dist[i][j] == 0 || dist[i][j] == 0x3f3f3f3f) {
                 cout << "0\n";
                 continue;
             }
@@ -63,13 +57,11 @@ int main() {
                 st = nxt[st][j];
             }
             path.push_back(j);
+
             cout << path.size() << ' ';
             for(auto x : path) cout << x << ' ';
             cout << '\n';
         }
     }
-
     return 0;
 }
-
-
