04569b445cca7674b1ca59995561bb7f8942cc39
이융의 <121233350+iyungui@users.noreply.github.com>
2026-04-04 13:27:28 +0900

    [level 3] Title: 섬 연결하기, Time: 0.02 ms, Memory: 4.11 MB -BaekjoonHub

---
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+int p[104];
+int find(int x) {
+    if(p[x] < 0) return x;
+    return p[x] = find(p[x]);
+}
+bool is_diff_group(int u, int v) {
+    u = find(u); v = find(v);
+    if(u == v) return 0;
+    if(p[u] == p[v]) p[u]--;
+    if(p[u] < p[v]) p[v] = u;
+    else p[u] = v;
+    return 1;
+}
+int solution(int n, vector<vector<int>> costs) {
+    int answer = 0;
+    memset(p, -1, sizeof(p));
+    
+    vector<tuple<int, int, int>> edge;
+    int e = costs.size();
+    for(int i = 0; i < e; i++) {
+        edge.push_back({costs[i][2], costs[i][0], costs[i][1]});
+    }
+    sort(edge.begin(), edge.end());
+    int cnt = 0;
+    for(int i = 0; i < e; i++) {
+        auto [c, a, b] = edge[i];
+        if(!is_diff_group(a, b)) continue;
+        answer += c;
+        cnt++;
+        if(cnt == n - 1) break;
+    }
+    return answer;
+}
