b86de30b9e784618940e67ec603f83f21f845f5d
이융의 <121233350+iyungui@users.noreply.github.com>
2026-04-04 09:41:49 +0900

    [Gold I] Title: 외판원 순회, Time: 20 ms, Memory: 6120 KB -BaekjoonHub

--- 백준/Gold/2098. 외판원 순회/외판원 순회.cc
+++ 백준/Gold/2098. 외판원 순회/외판원 순회.cc
@@ -1,44 +1,42 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int d[16][1 << 16]; // d[i][visited]: 0번째 도시에서 출발하여 현재까지 visited방문하였고 현재는 i번째를 방문하는 중, 나머지 모든 도시를 다 순회한 뒤 0번째로 다시 돌아오기 까지의 최소비용
-int dist[16][16];
+// 현재 i번째 방문, 지금까지 visited 방문한 상태.에서 나머지 모든 도시 방문하면서 다시 시작점까지 돌아오는 가장 적은 비용 
+int d[16][1 << 16];
 int n;
 const int INF = 1e9;
-
-int go(int cur, int visited) {
-    // 모든 도시를 다 방문한 경우
+int w[20][20];
+int dfs(int cur, int visited) {
+    // 다 방문한 경우
     if(visited == (1 << n) - 1) {
-        return dist[cur][0] ? dist[cur][0] : INF;
+        // 다시 시작점으로 돌아오는 길이 있으면 1
+        // 길이 없으면 0이므로, INF 반환.
+        return w[cur][0] ? w[cur][0] : INF;
     }
-    // 이미 계산된 경로인 경우
+    // 이미 계산된 경로인경우
     int &ret = d[cur][visited];
-    if(ret != -1) return ret;
-
-    // 최대값으로 초기화하고 다음 도시 방문 시작
-    ret = INF;
-    // cur에서 갈 수 있는 모든 도시를 탐색
+    if(~ret) return ret;
+    ret = INF;  // 방문하지 않은 상태 -> 최댓값부터.
+    // cur에서 나머지 도시 중 이동
     for(int i = 0; i < n; i++) {
-        // 이미 방문한 도시인 경우
+        // 이미 방문했거나 길이 없는 경우는 가지 않음
         if(visited & (1 << i)) continue;
-        // 길이 없는 경우
-        if(dist[cur][i] == 0) continue;
-        ret = min(ret, dist[cur][i] + go(i, visited | (1 << i)));
+        if(w[cur][i] == 0) continue;
+
+        // cur에서 i번째 도시로 방문하는 경우와 비교
+        ret = min(ret, w[cur][i] + dfs(i, visited | (1 << i)));
     }
     return ret;
 }
-
 int main() {
     ios::sync_with_stdio(0); cin.tie(0);
     cin >> n;
-    memset(d, -1, sizeof(d));
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < n; j++) {
-            cin >> dist[i][j];
+            cin >> w[i][j];
         }
     }
-    // 0번 도시부터 방문 -> 나머지 모든 도시 방문 -> 다시 0번 도시까지 오는데 최소비용
-    cout << go(0, 1) << '\n'; 
-
+    memset(d, -1, sizeof(d));
+    cout << dfs(0, 1) << '\n';
     return 0;
 }
