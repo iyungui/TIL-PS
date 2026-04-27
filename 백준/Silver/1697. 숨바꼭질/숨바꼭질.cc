4dcc1731f35a7c1207619570711f16f9eaf7f3b6
이융의 <121233350+iyungui@users.noreply.github.com>
2026-04-27 15:08:16 +0900

    [Silver I] Title: 숨바꼭질, Time: 0 ms, Memory: 2544 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
@@ -2,25 +2,22 @@
 
 using namespace std;
 
-int n, k, ret;
+int n, k;
 int pos, t;
-bool visited[100004];
-queue<pair<int, int>> q;    // position, time
+int dist[100004];
+queue<int> q;    // position
 
 int bfs() {
     while(!q.empty()) {
-        tie(pos, t) = q.front(); q.pop();
-        vector<int> next_position = {pos-1, pos+1, pos*2};
-
-        for(int nxt : next_position) {
+        int pos = q.front(); q.pop();
+        for(int nxt : {pos-1, pos+1, pos*2}) {
             // 다음위치가 동생 위치인 경우
-            if(nxt == k) return t + 1;
+            if(nxt == k) return dist[pos] + 1;
 
-            // 수빈이의 다음 위치가 범위 안에 있고,
-            // 미방문 위치인 경우
-            if(nxt >= 0 && nxt <= 100000 && !visited[nxt]) {
-                visited[nxt] = 1;
-                q.push({nxt, t + 1});
+            // 수빈이의 다음 위치가 범위 안에 있고, 미방문 위치인 경우
+            if(nxt >= 0 && nxt <= 100000 && dist[nxt] == -1) {
+                dist[nxt] = dist[pos] + 1;
+                q.push(nxt);
             }
         }
     }
@@ -31,16 +28,13 @@ int main() {
     ios::sync_with_stdio(0); cin.tie(0);
     cin >> n >> k;
     // 동생이 수빈과 동일한 위치 혹은 뒤에 있는 경우. 
-    if(n >= k) {
-        cout << n - k << '\n';
-        return 0;
-    }
+    if(n >= k) { cout << n - k << '\n'; return 0; }
 
-    // 수빈이의 위치 n에 0초에 도달
-    q.push({n, 0});
-    visited[n] = 1;
-    ret = bfs();
-
-    cout << ret << '\n';
+    // 수빈이의 위치 n
+    memset(dist, -1, sizeof(dist));
+    dist[n] = 0;
+    q.push(n);
+    
+    cout << bfs() << '\n';
     return 0;
 }
