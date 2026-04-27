e9db50050f6ec3467e8e7d157f030091a7b5791d
이융의 <121233350+iyungui@users.noreply.github.com>
2026-04-27 14:31:05 +0900

    [Gold V] Title: 토마토, Time: 84 ms, Memory: 14176 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.cc
+++ 백준/Gold/7576. 토마토/토마토.cc
@@ -1,59 +1,64 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 
 using namespace std;
 
 int n, m;
-int tomato[1004][1004];
-queue<pair<int, int>> q;
-int dist[1004][1004];
+int a[1004][1004];
+int visited[1004][1004];
+int y, x, ny, nx;
+int cnt;
+int dy[] = {-1,0,1,0};
+int dx[] = {0,1,0,-1};
 
-const int dy[] = {-1, 0, 1, 0};
-const int dx[] = {0, 1, 0, -1};
-
-int main() {
-    cin >> m >> n;
-
+bool chk() {
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < m; j++) {
-            cin >> tomato[i][j];
+            // 익지 않은 토마토가 있을 경우 true
+            if(a[i][j] == 0) return 1;
+        }
+    }
+    return 0;
+}
 
-            if(tomato[i][j] == 1) {
-                q.push({i, j});
-                dist[i][j] = 0;
-            } else if (tomato[i][j] == 0) {
-                dist[i][j] = -1;
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+    cin >> m >> n;
+
+    queue<pair<int, int>> q;
+    
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            cin >> a[i][j];
+            // 익은 토마토들을 큐에 넣기
+            if(a[i][j] == 1){
+                 q.push({i, j});
+                 visited[i][j] = 1;
             }
         }
     }
 
-    while(q.size()) {
-        auto cur = q.front(); q.pop();
-        int y = cur.first; 
-        int x = cur.second;
+    while(!q.empty()) {
+        tie(y,x) = q.front(); q.pop();
+        cnt = max(cnt, visited[y][x] - 1);
         for(int i = 0; i < 4; i++) {
-            int ny = y + dy[i];
-            int nx = x + dx[i];
+            ny = y + dy[i];
+            nx = x + dx[i];
 
-            if(ny < 0 || ny >= n || nx < 0 || nx >= m) continue;
-            if(tomato[ny][nx] == -1) continue; // 토마토 없는 칸
-            if(dist[ny][nx] != -1) continue; // 이미 익은 토마토
-            dist[ny][nx] = dist[y][x] + 1;
+            if(ny < 0 || ny >= n || nx < 0 || nx >= m || visited[ny][nx]) continue;
+            // 토마토가 없는 경우 탐색x
+            if(a[ny][nx] == -1) continue;
+            
+            a[ny][nx] = 1;  // 익은 토마토로 만들기
+            visited[ny][nx] = visited[y][x] + 1;
             q.push({ny, nx});
         }
     }
-
-    int ans = 0;
-
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < m; j++) {
-            if(tomato[i][j] == 0 && dist[i][j] == -1) {
-                cout << -1;
-                return 0;
-            }
-            ans = max(ans, dist[i][j]);
-        }
+    
+    if(chk()) {
+        cout << -1 << '\n';
     }
-
-    cout << ans;
+    else {
+        cout << cnt << '\n';
+    }
     return 0;
 }
