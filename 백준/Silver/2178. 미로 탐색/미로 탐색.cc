09cfc98eaf45761e87a8c73f8fee597f9995f74d
이융의 <121233350+iyungui@users.noreply.github.com>
2026-04-27 14:11:01 +0900

    [Silver I] Title: 미로 탐색, Time: 0 ms, Memory: 2240 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.cc
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.cc
@@ -1,57 +1,44 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 
 using namespace std;
 
-#define MAX_NUM 104
+int n, m;
+int a[104][104];
+int visited[104][104];
+int y, x, ny, nx;
 
-string a;
-int arr[MAX_NUM][MAX_NUM];
-int visited[MAX_NUM][MAX_NUM];
-int x, y, n, m;
+int dy[] = {-1,0,1,0};
+int dx[] = {0,1,0,-1};
 
-int dy[] = {-1, 0, 1, 0};
-int dx[] = {0, 1, 0, -1};
-
-bool InRange(int y, int x) {
-    return y >= 0 && y < n && x >= 0 && x < m;
-}
-
-void bfs(int y, int x) {
-    queue<pair<int, int>> q;
-    visited[y][x] = 1;
-    q.push({y, x});
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> n >> m;
+    for(int i = 0; i < n; i++) {
+        string s;
+        cin >> s;
+        for(int j = 0; j < m; j++) {
+            a[i][j] = (int)s[j] - '0';
+        }
+    }
     
-    while((int)q.size()) {
-        int ny, nx;
+    queue<pair<int, int>> q;
+    q.push({0, 0});
+    visited[0][0] = 1;
+
+    while(!q.empty()) {
         tie(y, x) = q.front(); q.pop();
+        
         for(int i = 0; i < 4; i++) {
             ny = y + dy[i];
             nx = x + dx[i];
-            
-            if(InRange(ny, nx) && arr[ny][nx] && visited[ny][nx] == 0) {
-                visited[ny][nx] = visited[y][x] + 1;
-                q.push({ny, nx});
-            }
-        }
-    }
-}
 
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL); cout.tie(NULL);
-        
-    cin >> n >> m;
-    
-    for(int i = 0; i < n; i++) {
-        cin >> a;
-        for(int j = 0; j < m; j++) {
-            arr[i][j] = (int)a[j] - '0';
-        }
+            if(ny < 0 || ny >= n || nx < 0 || nx >= m || visited[ny][nx]) continue;
+            if(a[ny][nx] == 0) continue;
+            visited[ny][nx] = visited[y][x] + 1;
+            q.push({ny, nx});
+        } 
     }
-    
-    bfs(x, y);
-    
-    cout << visited[n - 1][m - 1] << '\n';
-    
+    cout << visited[n-1][m-1] << '\n';
     return 0;
 }
