6499533040199b6bc940b2fbfeb18fc67be3afe7
이융의 <121233350+iyungui@users.noreply.github.com>
2026-04-29 16:26:43 +0900

    [level 1] Title: 모의고사, Time: 0.10 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.cpp
+++ 프로그래머스/1/42840. 모의고사/모의고사.cpp
@@ -2,24 +2,28 @@
 
 using namespace std;
 
+int a[] = {1,2,3,4,5};
+int b[] = {2,1,2,3,2,4,2,5};
+int c[] = {3,3,1,1,2,2,4,4,5,5};
+
 vector<int> solution(vector<int> answers) {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL); cout.tie(NULL);
+    vector<int> scores(3);
     
-    vector<int> a = {1, 2, 3, 4, 5}; // 1번 수포자
-    vector<int> b = {2, 1, 2, 3, 2, 4, 2, 5}; // 2번 수포자
-    vector<int> c = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}; // 3번 수포자
-    
-    vector<int> score(3);
-    for(int i = 0; i < answers.size(); i++) {
-        if(a[i % a.size()] == answers[i]) score[0]++;
-        if(b[i % b.size()] == answers[i]) score[1]++;
-        if(c[i % c.size()] == answers[i]) score[2]++;
+    for(int i = 0; i < (int)answers.size(); i++) {
+        if(a[i%5] == answers[i]) scores[0]++;
+        if(b[i%8] == answers[i]) scores[1]++;
+        if(c[i%10] == answers[i]) scores[2]++;
     }
-    int max_score = *max_element(score.begin(), score.end());
-    vector<int> answer;
+    
+    // 가장 높은 점수를 받은 사람 찾기
+    // 가장 높은 점수 mx
+    int mx = *max_element(scores.begin(), scores.end());
+    vector<int> ret;
     for(int i = 0; i < 3; i++) {
-        if(score[i] == max_score) answer.push_back(i + 1);
+        if(scores[i] == mx) ret.push_back(i + 1);
     }
-    return answer;
+    
+    // 오름차순 정렬
+    sort(ret.begin(), ret.end());
+    return ret;
 }
