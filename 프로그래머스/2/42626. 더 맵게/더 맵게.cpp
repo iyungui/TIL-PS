c86789b29a1b8143d35bba411db06b723bc8afa3
이융의 <121233350+iyungui@users.noreply.github.com>
2026-04-29 15:02:33 +0900

    [level 2] Title: 더 맵게, Time: 200.40 ms, Memory: 40.8 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.cpp
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.cpp
@@ -3,17 +3,20 @@
 using namespace std;
 
 int solution(vector<int> scoville, int K) {
-    priority_queue<int, vector<int>, greater<int>> pq (scoville.begin(), scoville.end());
-    int answer = 0;
+    priority_queue<int, vector<int>, greater<int>> pq(scoville.begin(), scoville.end());
     
-    while (pq.size() >= 2) {
-        if (pq.top() >= K) { return answer; }
-        answer++;
-        int a = pq.top(); pq.pop();
-        int b = pq.top(); pq.pop();
-        int c = a + (b * 2);
-        pq.push(c);
+    int cnt = 0;
+    while(1) {
+        if(pq.size() && pq.top() >= K) return cnt;
+        if((int)pq.size() >= 2) {
+            int a = pq.top(); pq.pop();
+            int b = pq.top(); pq.pop();
+            int c = a + (b * 2);
+            pq.push(c);
+        } 
+        else break;
+        cnt++;
     }
-    if (pq.size() == 1 && pq.top() >= K) { return answer; }
+    
     return -1;
 }
