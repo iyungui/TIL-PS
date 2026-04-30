ef266cc64b051caa2efdce5bde0df40ad42200f6
이융의 <121233350+iyungui@users.noreply.github.com>
2026-04-30 15:47:50 +0900

    [level 2] Title: 구명보트, Time: 1.90 ms, Memory: 4.78 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.cpp
+++ 프로그래머스/2/42885. 구명보트/구명보트.cpp
@@ -1,24 +1,21 @@
-#include <string>
-#include <vector>
-#include <algorithm>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int solution(vector<int> people, int limit) {
+    int answer = 0;
+    int n = people.size();
     sort(people.begin(), people.end());
-    
-    int cnt = 0;
-    int left = 0;
-    int right = people.size() - 1;
-    
-    while(left <= right) {
-        if(people[left] + people[right] <= limit) {
-            left++; right--;
+    int l = 0, r = n - 1;
+    while(l <= r) {
+        int sum = people[l] + people[r];
+        if(sum <= limit) {
+            l++; r--; 
+            answer++;   // 보트 추가
         }
         else {
-            right--;
+            r--; answer++;
         }
-        cnt++;
     }
-    return cnt;
+    return answer;
 }
