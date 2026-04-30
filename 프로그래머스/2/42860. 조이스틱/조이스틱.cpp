111a9c5a640d5c4d3f26ca0c1de9863b3eab06f8
이융의 <121233350+iyungui@users.noreply.github.com>
2026-04-30 15:21:40 +0900

    [level 2] Title: 조이스틱, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.cpp
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.cpp
@@ -3,32 +3,20 @@
 using namespace std;
 
 int solution(string name) {
-    int answer = 0;
     int n = name.size();
-    int moves = n - 1;
+    int answer = 0;
+    int moved = n - 1;
     for(int i = 0; i < n; i++) {
-        int up = (int)(name[i] - 'A');
-        int down = (int)('Z' - name[i]) + 1;
-        int up_down = min(up, down);
-        answer += up_down;
+        int up = (int)name[i] - 'A';
+        int down = ('Z' - (int)name[i]) + 1;
+        answer += min(up, down);
         
-        int next = i + 1;
-        while(next < n && name[next] == 'A') next++;
+        int nxt = i + 1;
+        while(nxt < n && name[nxt] == 'A') nxt++;
         
-        int front_and_back = i*2 + (n-next);
-        int back_and_front = (n-next) * 2 + i;
-        
-        moves = min({moves, front_and_back, back_and_front});
+        int right_first = i * 2 + (n - nxt);
+        int left_first = (n - nxt) * 2 + i;
+        moved = min({moved, right_first, left_first});    
     }
-    return answer + moves;
+    return answer + moved;
 }
-
-/*
-J A N
-
-i = J
-next = N
-
-i -> next (n - 1)
-
-*/
