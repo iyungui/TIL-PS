fb81920da34d242f2ff596d34cb7832e661cb989
이융의 <121233350+iyungui@users.noreply.github.com>
2026-05-07 22:19:29 +0900

    [Passed] 가능한 수열 중 최솟값 구하기 (8ms, 0MB)

--- codetree/trail4/가능한 수열 중 최솟값 구하기/find-min-of-possible-series.cpp
+++ codetree/trail4/가능한 수열 중 최솟값 구하기/find-min-of-possible-series.cpp
@@ -2,44 +2,29 @@
 using namespace std;
 
 int N;
-vector<int> b;
+string ret;
 
-bool is_equal(int e1, int s1, int e2, int s2) {
-    int len = e1-s1;
-    for(int i = 0; i <= len; i++) {
-        if(b[s1+i] != b[s2+i]) return 0;    // 하나라도 다른 경우
+bool chk(string& s) {
+    int m = s.size();
+    for(int i = 1; i <= m/2; i++) {
+        string r = s.substr(m-i, i);
+        string l = s.substr(m - (i*2), i);
+
+        if(l == r) return 0;
     }
-    return 1;
-}
-
-bool chk() {
-    int len = 1;
-    while(1) {
-        int e1 = b.size()-1;
-        int s1 = e1-len+1;
-        int e2 = s1-1;
-        int s2 = e2-len+1;
-
-        if(s2 < 0) break;
-        
-        if(is_equal(e1,s1,e2,s2)) return 0;
-
-        len++;
-    }
-
     return 1;
 }
 
 void go(int k) {
     if(k == N) {
-        for(int i = 0; i < N; i++) cout << b[i];
+        cout << ret << '\n';
         exit(0);
     }
 
-    for(int i = 4; i <= 6; i++) {
-        b.push_back(i);
-        if(chk()) go(k+1);
-        b.pop_back();
+    for(char c = '4'; c <= '6'; c++) {
+        ret.push_back(c);
+        if(chk(ret)) go(k+1);
+        ret.pop_back();
     }
 }
 
