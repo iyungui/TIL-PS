0d3c45b282f9ca7293747f9cb59e9c9ac920f398
이융의 <121233350+iyungui@users.noreply.github.com>
2026-05-09 17:31:47 +0900

    [Passed] 수들 중 최솟값 최대화하기 (27ms, 0MB)

--- codetree/trail4/수들 중 최솟값 최대화하기/maximin-of-numbers.cpp
+++ codetree/trail4/수들 중 최솟값 최대화하기/maximin-of-numbers.cpp
@@ -5,26 +5,15 @@ int visited[12];
 int grid[12][12];
 int ret;
 int n;
-int chosen[12];
-
-int get_min_val() {
-    int res = 10004;
-    for(int r = 0; r < n; r++) {
-        res = min(res, chosen[r]);
-    }
-    return res;
-}
-
-void go(int row) {
+void go(int row, int min_val) {
     if(row == n) {
-        ret = max(ret, get_min_val());
+        ret = max(ret, min_val);
         return;
     }
     for(int col = 0; col < n; col++) {
         if(visited[col]) continue;
         visited[col] = true;
-        chosen[row] = grid[row][col];
-        go(row+1);
+        go(row+1, min(min_val, grid[row][col]));
         visited[col] = false;
     }
 }
@@ -34,7 +23,7 @@ int main() {
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < n; j++) cin >> grid[i][j];
     }
-    go(0);
+    go(0, 10000);
     cout << ret << '\n';
     return 0;
 }
