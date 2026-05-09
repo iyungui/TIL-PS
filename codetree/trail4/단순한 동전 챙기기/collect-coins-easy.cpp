58091581ce60b8a12830f0f1bd115b67dced93cf
이융의 <121233350+iyungui@users.noreply.github.com>
2026-05-09 09:32:18 +0900

    [Passed] 단순한 동전 챙기기 (8ms, 0MB)

--- codetree/trail4/단순한 동전 챙기기/collect-coins-easy.cpp
+++ codetree/trail4/단순한 동전 챙기기/collect-coins-easy.cpp
@@ -8,39 +8,32 @@ pair<int, int> coins[10];
 pair<int, int> s, e;
 int n;
 bool has_coin[10];
-int chosen[10];
 int ret = 1e9;
 
 int get_dist(pair<int, int> a, pair<int, int> b) {
     return abs(a.first - b.first) + abs(a.second - b.second);
 }
 
-int get_total_dist(int m) {
-    int total = get_dist(s, coins[chosen[0]]);    // 시작점부터 첫번째 동전까지의 거리
-    for(int i = 1; i < m; i++) {
-        total += get_dist(coins[chosen[i-1]], coins[chosen[i]]);
-    }
-    total += get_dist(coins[chosen[m-1]], e);
-    return total;
-}
-
+// 현재 보고있는 동전 번호 cur
 // 마지막으로 선택한 동전의 번호 last, 
-// 지금까지 모은 동전의 개수 cnt
-void go(int last, int cnt) {
+// 지금까지 모은 동전의 개수 cnt,
+// 현재까지의 최소이동거리 dist
+void go(int cur, int last, int cnt, int dist) {
     // 3개의 동전을 모았다면 그 때마다 최솟값 갱신
-    if(cnt >= 3) ret = min(ret, get_total_dist(cnt));
+    if(cnt >= 3) ret = min(ret, dist + get_dist(coins[last], e));
 
     // 마지막 동전까지 다 탐색한 경우
-    if(last == 9) return;
+    if(cur > 9) return;
 
     if(9-last < 3-cnt && cnt < 3) return;
 
-    int nxt = last+1;
-    if(has_coin[nxt]) {
-        chosen[cnt] = nxt;
-        go(nxt, cnt+1);
+    if(has_coin[cur]) {
+        int nxt_dist;
+        if(cnt == 0) nxt_dist = get_dist(s, coins[cur]);
+        else nxt_dist = get_dist(coins[last], coins[cur]);
+        go(cur+1, cur, cnt+1, dist + nxt_dist);
     }
-    go(nxt, cnt);
+    go(cur+1, last, cnt, dist);
 }
 
 int main() {
@@ -58,7 +51,7 @@ int main() {
         }
     }
 
-    go(0, 0);
+    go(1, 0, 0, 0);
     if(ret == 1e9) cout << -1 << '\n';
     else cout << ret << '\n';
     return 0;
