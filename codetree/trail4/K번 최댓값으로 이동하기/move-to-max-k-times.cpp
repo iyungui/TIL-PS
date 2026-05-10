980e96038e250694ac4564e0027cfa2bd0e0ad71
이융의 <121233350+iyungui@users.noreply.github.com>
2026-05-10 21:51:54 +0900

    [Passed] K번 최댓값으로 이동하기 (8ms, 0MB)

---
+++ codetree/trail4/K번 최댓값으로 이동하기/move-to-max-k-times.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// 1. x보다 작은 칸
+// 2. 그 중 최댓값
+// 3. 그 중 행 번호가 작은 곳
+// 4. 열 번호가 작은 곳
+
+// 시작위치에서 출발하여 bfs (최댓값mx도 같이 업데이트)
+// 도달한 칸들(visited가 1) 중, if(mx == board[i][j])인 것들의 위치(i,j)를 모두 후보에 넣기
+//      --- 만약, visited가 1인 곳이 하나도 없다면, break.
+// i, j 순으로 정렬 후, 첫번째 원소가 다음시작위치.(k--)
+
+int n, k;
+int board[104][104];
+bool visited[104][104];
+int r, c;
+int dy[] = {-1,0,1,0};
+int dx[] = {0,1,0,-1};
+
+int main() {
+    cin >> n >> k;
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            cin >> board[i][j];
+        }
+    }
+    cin >> r >> c;
+    r--; c--;
+    while(k--) {
+        memset(visited, 0, sizeof(visited));
+        queue<pair<int, int>> q;
+        q.push({r, c});
+        visited[r][c] = 1;
+        int val = board[r][c];
+        int mx = 0; // 이번에 방문한 칸들 중 최댓값
+        while(q.size()) {
+            auto [y, x] = q.front(); q.pop();
+            for(int i = 0; i < 4; i++) {
+                int ny = y + dy[i];
+                int nx = x + dx[i];
+
+                if(ny < 0 || ny >= n || nx < 0 || nx >= n) continue;
+                if(visited[ny][nx]) continue;
+                if(val > board[ny][nx]) {
+                    visited[ny][nx] = 1;
+                    mx = max(mx, board[ny][nx]);
+                    q.push({ny, nx});
+                }
+            }
+        }
+        vector<pair<int, int>> candidates;
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                if(visited[i][j]&& board[i][j] == mx) {
+                    candidates.push_back({i, j});
+                } 
+            }
+        }
+        if(candidates.empty()) break;   // 더 이상 새로 이동할 위치가 없는 경우
+
+        sort(candidates.begin(), candidates.end());
+        
+        r = candidates[0].first;
+        c = candidates[0].second;
+    }
+
+    cout << r + 1 << " " << c + 1 << '\n';
+    return 0;
+}
