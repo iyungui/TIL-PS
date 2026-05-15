a5d234b713d50e550a1ab6362043b84fd0a9ec53
이융의 <121233350+iyungui@users.noreply.github.com>
2026-05-15 19:14:47 +0900

    [Passed] 동전 거슬러주기 (8ms, 0MB)

--- codetree/trail4/동전 거슬러주기/coin-change.cpp
+++ codetree/trail4/동전 거슬러주기/coin-change.cpp
@@ -2,24 +2,26 @@
 using namespace std;
 
 int n, m;
-int coins[104];
-// i원을 만들기위한 최소 동전의 수
-int d[10004];
+int d[10004];   // d[i]: i원을 거슬러주기 위해 필요한 최소 동전의 수
 
 int main() {
     cin >> n >> m;
+    vector<int> coins(n);
     for(int i = 0; i < n; i++) cin >> coins[i];
-    memset(d, 0x3f3f3f3f, sizeof(d));
+
+    // 최솟값을 구해야하므로 최댓값부터
+    for(int i = 0; i <= m; i++) d[i] = INT_MAX;
     d[0] = 0;
+
     for(int i = 1; i <= m; i++) {
         for(int j = 0; j < n; j++) {
-            if(i >= coins[j]) {
-                if(d[i-coins[j]] == 0x3f3f3f3f) continue;
-                d[i] = min(d[i], d[i - coins[j]] + 1);
+            if(i - coins[j] >= 0) {
+                if(d[i-coins[j]] == INT_MAX) continue;  // i-coins[j]원을 거슬러줄 수 없는 경우 
+                d[i] = min(d[i], d[i-coins[j]] + 1);
             }
         }
     }
-    if(d[m] == 0x3f3f3f3f) cout << -1 << '\n';
-    else cout << d[m] << '\n'; 
+    if(d[m] == INT_MAX) cout << -1 << '\n';
+    else cout << d[m] << '\n';
     return 0;
 }
