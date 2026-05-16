c577130f96d883a6941e730dd06fadc3d2c7a54c
이융의 <121233350+iyungui@users.noreply.github.com>
2026-05-16 15:16:17 +0900

    [Passed] 막대기 나누기 (8ms, 0MB)

--- codetree/trail4/막대기 나누기/rod-cutting.cpp
+++ codetree/trail4/막대기 나누기/rod-cutting.cpp
@@ -1,37 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// 막대기를 적절히 나누어, 최대 수익을 구하라
-/*
-len[4]
+int price[104], dp[104];
 
-중복 ok.
-지금까지 선택한 길이의 합이 같다면, 수익이 큰 게 좋다
+// 현재 막대기의 길이(n)
+int solve(int n) {
+    // 길이가 0 이하인 막대기의 수익은 0
+    if(n <= 0) return 0;
 
-dp[104] = dp[i]: 길이가 i인 막대기의 최대 수익
-dp[0] = 0
-나머지 dp = 주어지는 수익 값
-for(int i = 1; i <= n; i++) {
-    for(int j = 1; j <= i; j++) {
-        dp[i] = max(dp[i], dp[i-j] + price[j])
-    }
-}
-길이 4이고, 1만큼 잘랐으면,
-dp[4-1] = dp[3] 에 대한 이익 + price[1]
-*/
-int main() {
-    int n; cin >> n;
-    vector<int> price(n+1, 0);
-    for(int i = 1; i <= n; i++) cin >> price[i];
-
-    vector<int> dp(n+1, 0);
+    int &ret = dp[n];
+    if(~ret) return ret;
 
     for(int i = 1; i <= n; i++) {
-        for(int j = 1; j <= i; j++) {
-            dp[i] = max(dp[i], dp[i-j] + price[j]);
-        }
+        ret = max(ret, price[i] + solve(n-i));
     }
 
-    cout << dp[n] << '\n';
+    return ret;
+}
+
+int main() {
+    int n; cin >> n;
+    for(int i = 1; i <= n; i++) cin >> price[i];
+
+    memset(dp, -1, sizeof(dp));
+    dp[0] = 0;
+    int ret = solve(n);
+    cout << ret << '\n';
     return 0;
 }
