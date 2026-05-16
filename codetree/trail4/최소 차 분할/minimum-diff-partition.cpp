7308c87482b1d17879fcd26fb0c971626389577c
이융의 <121233350+iyungui@users.noreply.github.com>
2026-05-16 16:45:04 +0900

    [Passed] 최소 차 분할 (8ms, 0MB)

--- codetree/trail4/최소 차 분할/minimum-diff-partition.cpp
+++ codetree/trail4/최소 차 분할/minimum-diff-partition.cpp
@@ -1,41 +1,52 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// i번째 수까지 고려, 합이 j인 부분집합이 존재한다면 1, 아니라면 0
+// (case1) i번째 수를 포함하여 합이 j인 경우
+// -> i-1번째 수까지 고려했을때, 합이 j-nums[i] 였어야 함.
+//  (단 j-nums[i] >= 0 이어야 하고, 이전상태도 가능했어야함(dp[i-1][j-nums[i]])
+
+// (case2) i번째 수를 포함하지 않고 합이 j인 경우
+// -> i-1번째 수까지 고려했을때, 합이 j 였어야 함
+
+// 점화식
+// dp[i][j] = true (j-nums[i] >= 0 and dp[i-1][j-nums[i]] = true) or (dp[i-1][j] = true)
+
 int main() {
     int n; cin >> n;
-    int total = 0;
-    vector<int> nums(n);
-    for(int i = 0; i < n; i++) {
+    vector<int> nums(n+1);
+    int total = 0;  // 전체 집합의 합
+    for(int i = 1; i <= n; i++) {
         cin >> nums[i];
         total += nums[i];
     }
 
-    // 전체 합의 절반
-    int target = total / 2;
-    // 합이 j가 되는 부분 집합이 존재하는가?
-    vector<bool> dp(target+1, false);
-    dp[0] = true;
+    vector<vector<int>> dp(n+1, vector<int>(total+1, false));
+    // 0번째 수까지 고려, 합이 0인 부분집합은 존재함
+    dp[0][0] = true;
 
-    // 0-1 배낭 방식(중복x)으로 dp 테이블 채우기
-    for(int i = 0; i < n; i++) {
-        int x = nums[i];
-        for(int j = target; j >= x; j--) {
-            // j-x를 만들 수 있는 부분집합이 있다면
-            // j도 만들 수 있다
-            if(dp[j - x]) {
-                dp[j] = true;
-            }
+    // dp테이블 채우기
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j < total; j++) {
+            // (case1) i번째 수를 포함하여 합이 j인 경우
+            if(j-nums[i] >= 0 && dp[i-1][j-nums[i]]) dp[i][j] = true;
+
+            // (case2) i번째 수를 포함하지 않고 합이 j인 경우
+            if(dp[i-1][j]) dp[i][j] = true;
+        }
+    } 
+
+    // 테이블을 다 채우고,
+    // dp[n][j] 가 true라면 (j는 1부터 total-1 까지) 
+    // 그룹 A의 합은 j이고
+    // 그룹 B의 합은 total - j 임.
+    int min_diff = total;
+    for(int A = 1; A < total; A++) {
+        if(dp[n][A]) {
+            int B = total - A;
+            min_diff = min(min_diff, abs(B-A));
         }
     }
-    int max_sum_A = 0;
-    for(int i = target; i >= 0; i--) {
-        if(dp[i]) {
-            max_sum_A = i;
-            break;
-        }
-    }
-    int max_sum_B = total - max_sum_A;
-
-    cout << max_sum_B - max_sum_A << '\n';
+    cout << min_diff << '\n';
     return 0;
 }
