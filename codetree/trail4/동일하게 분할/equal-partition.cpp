5cd5b1e5508fe061c98bfe051178cdfee1fc1709
이융의 <121233350+iyungui@users.noreply.github.com>
2026-05-16 18:12:42 +0900

    [Passed] 동일하게 분할 (12ms, 0MB)

--- codetree/trail4/동일하게 분할/equal-partition.cpp
+++ codetree/trail4/동일하게 분할/equal-partition.cpp
@@ -8,12 +8,11 @@ i번째 수까지 고려, A-B 가 j 라면 true, 아니라면 false
 i번째 수를 A에 넣는 경우
 dp[i][j] = true (단 dp[i-1][j-nums[i]] 가 true)
 
-i번째 수를 넣지않는경우(B에 넣는경우)
-dp[i][j] = true (단 dp[i-1][j+nums[i]] 가 true)
+i번째 수를 넣지않는경우
+dp[i][j] = true (단 dp[i-1][j]] 가 true)
 
 */
 int main() {
-
     cin >> n;
     vector<int> nums(n+1);
     int m = 0;
@@ -22,16 +21,16 @@ int main() {
         m += nums[i];
     }
 
-    vector<vector<bool>> dp(n+1, vector<bool>(2*m+1, false));
-    dp[0][m] = true;
+    vector<vector<bool>> dp(n+1, vector<bool>(m+1, false));
+    dp[0][0] = true;
     for(int i = 1; i <= n; i++) {
-        for(int j = 0; j <= 2*m; j++) {
+        for(int j = 0; j <= m; j++) {
             if(j-nums[i] >= 0 && dp[i-1][j-nums[i]]) dp[i][j] = true;
-            if(j+nums[i] <= 2*m && dp[i-1][j+nums[i]]) dp[i][j] = true;
+            if(dp[i-1][j]) dp[i][j] = true;
         }
     }
 
-    if(dp[n][0 + m]) cout << "Yes\n";
+    if(m % 2 == 0 && dp[n][m/2]) cout << "Yes\n";
     else cout << "No\n";
     return 0;
 }
