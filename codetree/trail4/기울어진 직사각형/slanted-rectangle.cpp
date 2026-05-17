4d03a1dccbb33aea705923254d4108f79d1f3a8d
이융의 <121233350+iyungui@users.noreply.github.com>
2026-05-17 18:54:06 +0900

    [Passed] 기울어진 직사각형 (9ms, 0MB)

--- codetree/trail4/기울어진 직사각형/slanted-rectangle.cpp
+++ codetree/trail4/기울어진 직사각형/slanted-rectangle.cpp
@@ -1,63 +1,47 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n;
-int grid[24][24];
-
+int n, grid[22][22];
 int dy[] = {-1, -1, 1, 1};
 int dx[] = {1, -1, -1, 1};
-// 1번방향으로 이동한 횟수 = 3번방향으로 이동한 횟수
-// 2번 .. = 4번 ..
-// => 1번 방향으로 몇 칸, 2번 방향으로 몇 칸
 
-// [a, b, a, b]
-int get_sum(int y, int x, int a, int b) {
-    int st = grid[y][x];
-    int sum = st;
+int get_sum(int y, int x, int w, int h) {
+    int move_cnt[] = {w, h, w, h};
+    int sum = 0;
 
     for(int i = 0; i < 4; i++) {
-        int cnt;
-        if(i % 2 == 0) cnt = a;
-        else cnt = b;
+        for(int j = 0; j < move_cnt[i]; j++) {
+            y += dy[i];
+            x += dx[i];
 
-        int tmp = 0;
-        while(tmp < cnt) {
-            int ny = y + dy[i];
-            int nx = x + dx[i];
+            if(y < 0 || y >= n || x < 0 || x >= n) return 0;
 
-            if(ny < 0 || ny >= n || nx < 0 || nx >= n) break;
-
-            y = ny;
-            x = nx;
             sum += grid[y][x];
-
-            tmp++;
         }
-        if(tmp < cnt) return 0;
     }
 
-    return sum - st;
+    return sum;
 }
 
 int main() {
     cin >> n;
     for(int i = 0; i < n; i++) {
-        for(int j = 0; j < n; j++) {
-            cin >> grid[i][j];
-        }
+        for(int j = 0; j < n; j++) cin >> grid[i][j];
     }
 
-    int ret = 0;
+    int ans = 0;
 
-    for(int i = 2; i < n; i++) {
-        for(int j = 1; j < n; j++) {
-            for(int a = 1; a < n - 1; a++) {
-                for(int b = 1; b < n - 1; b++) {
-                    ret = max(ret, get_sum(i, j, a, b));
+    // 시작점(i, j)
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            for(int w = 1; w < n; w++) {
+                for(int h = 1; h < n; h++) {
+                    ans = max(ans, get_sum(i, j, w, h));
                 }
             }
         }
     }
-    cout << ret << '\n';
+
+    cout << ans << '\n';
     return 0;
 }
