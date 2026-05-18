5d59467c70380d07bd5cb593e67b9884d40ece2e
이융의 <121233350+iyungui@users.noreply.github.com>
2026-05-18 09:37:56 +0900

    [Passed] 가장 먼저 나오는 숫자 (180ms, 1MB)

--- codetree/trail5/가장 먼저 나오는 숫자/first-appear-number.cpp
+++ codetree/trail5/가장 먼저 나오는 숫자/first-appear-number.cpp
@@ -7,17 +7,13 @@ int main() {
     cin >> n >> m;
     vector<int> nums(n);
     for(int i = 0; i < n; i++) cin >> nums[i];
-    sort(nums.begin(), nums.end());
+    
     while(m--) {
-        
         int k; cin >> k;
         auto it = lower_bound(nums.begin(), nums.end(), k);
-        if(it == nums.end()) cout << -1 << '\n';
-        else {
-            int idx = it - nums.begin();
-            if(nums[idx] == k) cout << idx + 1 << '\n';
-            else cout << -1 << '\n';
-        }
+
+        if(it == nums.end() || *it != k) cout << -1 << '\n';
+        else cout << it - nums.begin() + 1 << '\n';
     }
     return 0;
 }
