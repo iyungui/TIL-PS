b1f8f23f28e407d845e46415ca6858e7f0b50297
이융의 <121233350+iyungui@users.noreply.github.com>
2026-05-18 17:12:24 +0900

    [level 2] Title: 소수 찾기, Time: 0.56 ms, Memory: 4 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.cpp
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.cpp
@@ -1,30 +1,31 @@
 #include <bits/stdc++.h>
 
+// numbers의 길이가 7이하
+// 순열 => 해당 조합이 소수라면 cnt++
+
 using namespace std;
 
-bool isused[10];
+int isused[10];
 unordered_set<int> st;
 
-bool is_prime(int n) {
-    if(n < 2) return 0;
-    for(int i = 2; i * i <= n; i++) {
-        if(n % i == 0) return 0;
+bool isPrime(int x) {
+    if(x < 2) return 0;
+    for(int i = 2; i*i <= x; i++) {
+        if(x % i == 0) return 0;
     }
     return 1;
 }
 
 void dfs(string cur, string& numbers) {
     if(cur.size()) {
-        int n = stoi(cur);
-        if(is_prime(n)) {
-            st.insert(n);
-        }
+        int num = stoi(cur);
+        if(isPrime(num)) st.insert(num);
     }
     for(int i = 0; i < (int)numbers.size(); i++) {
         if(isused[i]) continue;
-        isused[i] = 1;
+        isused[i] = true;
         dfs(cur + numbers[i], numbers);
-        isused[i] = 0;
+        isused[i] = false;
     }
 }
 
