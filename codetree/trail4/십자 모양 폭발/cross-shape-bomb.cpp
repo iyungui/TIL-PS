687563ee664a7d699c1a16d317cab78aed9ba343
이융의 <121233350+iyungui@users.noreply.github.com>
2026-05-19 17:24:47 +0900

    [Passed] 십자 모양 폭발 (12ms, 1MB)

--- codetree/trail4/십자 모양 폭발/cross-shape-bomb.cpp
+++ codetree/trail4/십자 모양 폭발/cross-shape-bomb.cpp
@@ -1,49 +1,45 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int board[204][204];
 int n;
-const int dy[] = {-1, 0, 1, 0};
-const int dx[] = {0, 1, 0, -1};
-
-bool InRange(int y, int x) {
-    return y >= 0 && y < n && x >= 0 && x < n;
-}
-
-void gravtify(int col) {
+int board[204][204];
+int dy[] = {-1,0,1,0};
+int dx[] = {0,1,0,-1};
+void gravity(int c) {
     vector<int> tmp;
-    for(int i = 0; i < n; i++) {
-        if(board[i][col] != 0) tmp.push_back(board[i][col]);
+    for(int r = 0; r < n; r++) {
+        if(board[r][c] != 0) tmp.push_back(board[r][c]);
     }
-    // 격자의 맨 아래 행부터 위로 올라가면서 tmp의 뒷부분부터 채우기
-    int tmp_idx = tmp.size() - 1;
-    for(int i = n-1; i >= 0; i--) {
-        if(tmp_idx >= 0) board[i][col] = tmp[tmp_idx--];
-        else board[i][col] = 0;
+    int idx = (int)tmp.size() - 1;
+    for(int r = n-1; r >= 0; r--) {
+        if(idx >= 0) board[r][c] = tmp[idx--];
+        else board[r][c] = 0;
     }
 }
 
-void boom(int y, int x) {
+void bomb(int y, int x) {
     int k = board[y][x];
-    set<int> col;
-    col.insert(x);
+    
+    set<int> cols;
+    cols.insert(x);
     board[y][x] = 0;
 
     for(int i = 0; i < 4; i++) {
         int ny = y;
         int nx = x;
-        for(int j = 0; j < k-1; j++) {
+
+        // 해당 방향으로 k-1번 반복
+        for(int j = 1; j < k; j++) {
             ny += dy[i];
             nx += dx[i];
-            
-            if(InRange(ny, nx)) {
-                board[ny][nx] = 0;
-                col.insert(nx);
-            }
-            else break; // 격자를 벗어나면 즉시 다른 방향으로 시도
+
+            if(ny < 0 || ny >= n || nx < 0 || nx >= n) break;
+            board[ny][nx] = 0;
+            cols.insert(nx);
         }
     }
-    for(int c : col) gravtify(c);
+
+    for(int c : cols) gravity(c);
 }
 
 void printResult() {
@@ -62,11 +58,8 @@ int main() {
             cin >> board[i][j];
         }
     }
-
-    int r, c;
-    cin >> r >> c;
-    boom(r-1, c-1);
-
+    int r, c; cin >> r >> c;
+    bomb(r-1, c-1);
     printResult();
     return 0;
 }
