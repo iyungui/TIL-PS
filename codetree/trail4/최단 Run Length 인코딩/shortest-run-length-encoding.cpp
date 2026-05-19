86e2d6078e89fe360d46c79476b122f536b1a90a
이융의 <121233350+iyungui@users.noreply.github.com>
2026-05-19 15:37:00 +0900

    [Passed] 최단 Run Length 인코딩 (2ms, 0MB)

--- codetree/trail4/최단 Run Length 인코딩/shortest-run-length-encoding.cpp
+++ codetree/trail4/최단 Run Length 인코딩/shortest-run-length-encoding.cpp
@@ -2,46 +2,41 @@
 
 using namespace std;
 
-// 오른쪽으로 k번 shift
 string shift(string s, int k) {
     int n = s.size();
-    k %= n;
     return s.substr(n-k) + s.substr(0, n-k);
 }
 
-// run-length encoding
-string run_encoding(string& s) {
-    int cnt = 1;
+string encoding(string s) {
     string ret = "";
+    int num = 1;
     int n = s.size();
-    if(n == 0) return ret;
-
     for(int i = 1; i < n; i++) {
         if(s[i] != s[i-1]) {
             ret.push_back(s[i-1]);
-            ret += to_string(cnt);
-            cnt = 1;
+            ret += to_string(num);
+            num = 1;
         }
-        else cnt++;
+        else num++;
     }
     ret.push_back(s[n-1]);
-    ret += to_string(cnt);
+    ret += to_string(num);
 
     return ret;
 }
 
 int main() {
     string A; cin >> A;
-
-    // Please write your code here.
     int n = A.size();
-    int min_len = n * 2 + 10;
+    int min_length = n * 2 + 10;
+    
     for(int i = 0; i < n; i++) {
         string shifted = shift(A, i);
-        string encoded = run_encoding(shifted);
-        min_len = min(min_len, (int)encoded.size());
+        string encoded = encoding(shifted);
+        min_length = min(min_length, (int)encoded.size());
     }
 
-    cout << min_len << '\n';
+    cout << min_length << '\n';
+    
     return 0;
 }
