c4eb199a2e1551b56dff45cf0b136088a21f3e91
이융의 <121233350+iyungui@users.noreply.github.com>
2026-05-20 12:45:44 +0900

    [Passed] 단 한 번의 2048 시도 (2ms, 0MB)

--- codetree/trail4/단 한 번의 2048 시도/one-trial-of-2048-game.cpp
+++ codetree/trail4/단 한 번의 2048 시도/one-trial-of-2048-game.cpp
@@ -3,42 +3,32 @@ using namespace std;
 
 const int n = 4;
 int board[5][5];
-char dir;
 
-// 바닥으로 밀기
 void push_down() {
-    // 숫자만 모으기 -> 짝지어 합치기 -> 바닥부터 배치
-    // 모든 열에 대해
-    for(int c = 0; c < n; c++) {
+    for(int j = 0; j < n; j++) {
         vector<int> nums;
-
-        // (1) 아래에서부터 위로 올라가면서 숫자만 모으기
-        for(int r = n-1; r >= 0; r--) {
-            if(board[r][c] != 0) nums.push_back(board[r][c]);
+        for(int i = n-1; i >= 0; i--) {
+            if(board[i][j] != 0) nums.push_back(board[i][j]);
         }
-
-        // 수집한 숫자들을 아래에서부터 두 개씩 비교하며 합치기
-        vector<int> merged;
         int m = nums.size();
-        for(int r = 0; r < m; r++) {
-            // 다음 숫자가 존재하고, 현재 숫자와 같다면 merged에 푸시
-            if(r+1 < m && nums[r] == nums[r+1]) {
-                merged.push_back(nums[r] * 2);
-                // 다음숫자는 이미 합쳐졌으므로 건너뛰기
-                r++;
+        vector<int> merged;
+        for(int i = 0; i < m; i++) {
+            if(i+1 < m && nums[i] == nums[i+1]) {
+                merged.push_back(nums[i] * 2);
+                i++;
             }
-            // 다르거나 마지막 숫자라면 그대로 유지
-            else merged.push_back(nums[r]);
+            else merged.push_back(nums[i]);
         }
-
-        // merged를 board의 바닥부터 채우기. (나머지는 0으로 처리)
         int idx = 0;
         m = merged.size();
-        for(int r = n-1; r >= 0; r--) {
-            if(idx < m) board[r][c] = merged[idx++];
-            else board[r][c] = 0;
+        for(int i = n-1; i >= 0; i--) {
+            if(idx < m) board[i][j] = merged[idx++];
+            else board[i][j] = 0;
         }
     }
+    // 1. 숫자만 모으기
+    // 2. 합치기
+    // 3. 밑에서부터 배치
 }
 
 void rotate() {
@@ -53,29 +43,33 @@ void rotate() {
             board[i][j] = tmp[i][j];
         }
     }
-    return;
 }
 
-void go(int d) {
-    if(d == 'D') push_down();
-
-    else if(d == 'R') {
-        // 시계방향 -> push down -> 원복
-        rotate();
-        push_down();
-        rotate(); rotate(); rotate();
-    }
-
-    else if(d == 'L') {
+void go(char c) {
+    if(c == 'L') {
         rotate(); rotate(); rotate();
         push_down();
         rotate();
     }
-
-    else if(d == 'U') {
+    else if(c == 'R') {
+        rotate();
+        push_down();
+        rotate(); rotate(); rotate();
+    }
+    else if(c == 'U') {
         rotate(); rotate();
         push_down();
         rotate(); rotate();
+    }
+    else if(c == 'D') push_down();
+}
+
+void printResult() {
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            cout << board[i][j] << " ";
+        }
+        cout << '\n';
     }
 }
 
@@ -85,15 +79,10 @@ int main() {
             cin >> board[i][j];
         }
     }
-    cin >> dir;
-    
-    go(dir);
+    char d; cin >> d;
 
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < n; j++) {
-            cout << board[i][j] << " ";
-        }
-        cout << '\n';
-    }
+    go(d);
+
+    printResult();
     return 0;
 }
