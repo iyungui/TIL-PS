7043f560afd4c58eb7162c21f58ec49b4ebe3ae0
이융의 <121233350+iyungui@users.noreply.github.com>
2026-09-22 13:21:44 +0900

    [Passed] 알파벳과 사칙연산 (2ms, 0MB)

--- codetree/trail4/알파벳과 사칙연산/calculations-with-alphabet.cpp
+++ codetree/trail4/알파벳과 사칙연산/calculations-with-alphabet.cpp
@@ -3,58 +3,67 @@
 using namespace std;
 
 string expression;
-vector<char> ops;
-vector<int> chs;
-bool used[6];
 int nums[6];
-int ret = -1e9;
+bool used[6];
+vector<char> ops;
+vector<int> ids;
+int ret = -2e9;
 
 int calc(int a, int b, char op) {
-    if(op == '*') return a * b;
     if(op == '+') return a + b;
-    return a - b;
+    if(op == '-') return a - b;
+    return a * b;
 }
 
+// 해당 조합에서 식 계산
 int solve() {
-    int sum = nums[chs[0]];
-    int n = ops.size();
-    for(int i = 0; i < n; i++) {
-        sum = calc(sum, nums[chs[i+1]], ops[i]);
+    int sum = nums[ids[0]]; // nums[0] 이 아니라, ids[0]임에 주의
+
+    int len = ops.size();
+
+    for(int i = 0; i < len; i++) {
+        sum = calc(sum, nums[ids[i+1]], ops[i]);
     }
+
     return sum;
 }
 
-void go(int idx) {
-    if(idx == 6) {
+// 4^6
+void combi(int idx) {
+    if(idx == 6) {  // 모든 알파벳의 숫자를 다 결정한 경우
         ret = max(ret, solve());
         return;
     }
+
+    // 나오지 않은 알파벳은 바로 다음 combi(idx+1) 호출
     if(!used[idx]) {
-        go(idx + 1);
+        combi(idx+1);
         return;
     }
+
+    // idx 번째 알파벳에 i(1이상 4이하의 정수 중 하나)를 넣는다. 다음 combi(idx+1)를 호출한다
     for(int i = 1; i <= 4; i++) {
         nums[idx] = i;
-        go(idx + 1);
+        combi(idx+1);
     }
 }
 
 int main() {
     cin >> expression;
 
-    // Please write your code here.
     for(char c : expression) {
-        if(c >= 'a' && c <= 'z') {
+        if(c >= 'a' && c <= 'f') {
             int idx = c - 'a';
             used[idx] = true;
-            chs.push_back(idx);
+            ids.push_back(idx);
         }
-        else ops.push_back(c);
+        else {
+            ops.push_back(c);
+        }
     }
 
-    go(0);
+    combi(0);
 
     cout << ret << '\n';
-
     return 0;
 }
