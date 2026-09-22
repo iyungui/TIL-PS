d7b1bcba2712cc166ce0220e2537814561354e02
이융의 <121233350+iyungui@users.noreply.github.com>
2026-09-22 14:41:38 +0900

    [Passed] N개 중에 M개 뽑기 (2ms, 0MB)

--- codetree/trail4/N개 중에 M개 뽑기/n-choose-m.cpp
+++ codetree/trail4/N개 중에 M개 뽑기/n-choose-m.cpp
@@ -1,23 +1,32 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
 int N, M;
 int a[14];
 
-void go(int st, int cnt) {
+void go(int k, int cnt) {
     if(cnt == M) {
         for(int i = 0; i < M; i++) cout << a[i] << " ";
         cout << '\n';
         return;
     }
-    for(int i = st; i <= N; i++) {
+
+    int available = N-k+1;  // 뽑을 수 있는 숫자들(k포함)
+    int remain = M-cnt; // 뽑아야할 개수
+    if(available < remain) return;
+
+    for(int i = k; i <= N; i++) {
         a[cnt] = i;
-        go(i + 1, cnt + 1);
+        go(i+1, cnt+1);
     }
 }
 
 int main() {
     cin >> N >> M;
-    go(1,0);
+
+    // Please write your code here.
+    go(1, 0);
+
     return 0;
 }
