6e341726a34a9214ae45961efdd2271b8bdc2f44
이융의 <121233350+iyungui@users.noreply.github.com>
2026-09-23 15:42:55 +0900

    [Passed] 두 수의 합 (47ms, 2MB)

--- codetree/trail5/두 수의 합/sum-of-two-num.cpp
+++ codetree/trail5/두 수의 합/sum-of-two-num.cpp
@@ -1,28 +1,17 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
-
-int a[100004], cnt;
-unordered_map<int, int> mp;
+int n, k, ret;
+unordered_map<int,int> mp;
 int main() {
-    int n, k; cin >> n >> k;
-    for(int i = 0; i < n; i++) cin >> a[i];
-    
-    for(int i = 0; i < n; i++) {
-        int target = k - a[i];
-        if(mp.find(target) != mp.end()) cnt += mp[target];
-        mp[a[i]]++;
+    cin >> n >> k;
+    for (int i = 0; i < n; i++) {
+        int x; cin >> x;
+        int t = k-x;
+        if(mp.find(t) != mp.end()) ret += mp[t];
+        mp[x]++;
     }
-
-    cout << cnt << '\n';
-
-    /*
-    a
-    4 6 5 3 7
-    target
-    5 3 4 6 2
-    cnt
-    0 0 1 2 2
-    */
+    cout << ret << '\n';
     return 0;
 }
