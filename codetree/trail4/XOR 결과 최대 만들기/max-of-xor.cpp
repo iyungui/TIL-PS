5bc8319ba2b604b91bc87c8011f1d946137ce88b
이융의 <121233350+iyungui@users.noreply.github.com>
2026-09-23 15:12:18 +0900

    [Passed] XOR 결과 최대 만들기 (2ms, 0MB)

--- codetree/trail4/XOR 결과 최대 만들기/max-of-xor.cpp
+++ codetree/trail4/XOR 결과 최대 만들기/max-of-xor.cpp
@@ -1,29 +1,31 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
-int n, m, ret;
-int a[24];
+int n, m;
+int A[20];
+int ret;
 
-// 몇번째로 뽑는 숫자인지(cnt), 몇번째인덱스부터 뽑아야 하는지(cur_idx), 현재까지의 xor 값(val)
-void findMaxXor(int cnt, int cur_idx, int cur_val) {
-    if(cnt == m) {  // m개를 다 선택한 경우 최댓값 갱신
-        ret = max(ret, cur_val);
+void go(int cnt, int k, int val) {
+    if(cnt == m) {
+        ret = max(ret, val);
         return;
     }
+    if((n-k+1) < (m-cnt)) return;
 
-    // 남은 원소의 개수가 부족하면 종료 (가지치기)
-    if(cur_idx >= n || (n-cur_idx) < (m-cnt)) return;
-
-    // cur_idx번째 숫자를 선택하는 경우
-    findMaxXor(cnt+1, cur_idx+1, cur_val ^ a[cur_idx]);
-    // 선택하지 않는 경우
-    findMaxXor(cnt, cur_idx+1, cur_val);
+    for(int i = k; i < n; i++) {
+        go(cnt+1, i+1, val ^ A[i]);
+    }
 }
 
 int main() {
     cin >> n >> m;
-    for(int i = 0; i < n; i++) cin >> a[i];
-    findMaxXor(0, 0, 0);
+    for (int i = 0; i < n; i++) {
+        cin >> A[i];
+    }
+
+    // Please write your code here.
+    go(0, 0, 0);
     cout << ret << '\n';
     return 0;
 }
