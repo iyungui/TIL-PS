25ab9651784f8b59af110c655c0c52cee406ee8e
이융의 <121233350+iyungui@users.noreply.github.com>
2026-09-23 15:29:31 +0900

    [Passed] 수 등장 횟수 (140ms, 2MB)

--- codetree/trail5/수 등장 횟수/number-frequency.cpp
+++ codetree/trail5/수 등장 횟수/number-frequency.cpp
@@ -1,17 +1,26 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
-int n, m;
+const int MAX_N = 100000;
+
+int n, m, q;
+int arr[MAX_N];
 unordered_map<int, int> mp;
 int main() {
-    cin >> n >> m; 
-    for(int i = 0; i < n; i++) {
-        int num; cin >> num;
-        mp[num]++;
+    cin >> n >> m;
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+        mp[arr[i]]++;
     }
-    while(m--) {
-        int num; cin >> num;
-        cout << mp[num] << " ";
+
+    for (int i = 0; i < m; i++) {
+        cin >> q;
+        cout << mp[q] << " ";
     }
+
+    // Please write your code here.
+
     return 0;
 }
